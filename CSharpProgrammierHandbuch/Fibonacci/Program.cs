e2b028364878a1f330d5e8d76842b9f0190e1026
Joe Care <Joe_Care_99@yahoo.com>
2020-04-10 16:47:45 +0200

    Fibonacci

---
+++ CSharpProgrammierHandbuch/Fibonacci/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+
+            Console.WriteLine("Fibonacci Numbers 1-15:");
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i + 1}: {FibonacciNumber(i)}");
+            }
+        }
+
+        static int FibonacciNumber(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            return a;
+        }
+    }
+}
