253ec774210c696c3897e8289fd96bbcdd90b4d5
Joe Care <Joe_Care_99@yahoo.com>
2020-04-10 16:45:53 +0200

    CGui

---
+++ CSharpProgrammierHandbuch/CGui/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGui
+{
+    class Application 
+    {
+        static async void Run(string[] args)
+        {
+        }
+    }
+}
