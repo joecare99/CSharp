531e7f3777686147c4d44747c8271afade2f21f7
Joe Care <Joe_Care_99@yahoo.com>
2020-04-11 19:24:07 +0200

    minor Changes

--- CSharpBible/Calc32/NonVisual/CalculatorClass.cs
+++ CSharpBible/Calc32/NonVisual/CalculatorClass.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
 
 namespace CSharpBible.Calc32.NonVisual
 {
-
     public class CalculatorClass : Component
     {
         enum eOpMode
