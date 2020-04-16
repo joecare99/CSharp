9a4dd636a31feb4f566cfaf74f074c1356fee833
Joe Care <Joe_Care_99@yahoo.com>
2020-04-16 08:52:35 +0200

    New Application

--- CSharpBible/ConsoleLib/ConsoleLib/CommonControls/Application.cs
+++ CSharpBible/ConsoleLib/ConsoleLib/CommonControls/Application.cs
@@ -1,13 +1,52 @@
-﻿using ConsoleLib.CommonControls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.Drawing;
+using static ConsoleLib.NativeMethods;
 
 namespace ConsoleLib.CommonControls
 {
-    class Application : Panel
+    public class Application : Panel
     {
+        public Point MousePos { get; private set; }
+
+        public Application()
+        {
+            ExtendedConsole.MouseEvent += HandleMouseEvent;
+            ExtendedConsole.KeyEvent += HandleKeyEvent;
+            ExtendedConsole.WindowBufferSizeEvent += HandleWinBufEvent;
+            Boarder = new Char[] { };
+        }
+
+        private void HandleWinBufEvent(object sender, WINDOW_BUFFER_SIZE_RECORD e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleKeyEvent(object sender, KEY_EVENT_RECORD e)
+        {
+            // Determine the Control to send the Event to
+            if (e.bKeyDown)
+            { }
+            else
+            { };     
+
+        }
+
+        private void HandleMouseEvent(object sender, MOUSE_EVENT_RECORD e)
+        {
+            if (e.dwEventFlags == EventFlags.MOUSE_MOVED)
+            {
+                Point lastMousePos = MousePos;
+                MousePos = e.dwMousePosition.AsPoint;
+                // Invoke Mouse Leave
+                // Invoke Mouse Enter
+            }
+        }
+
+        public virtual void Initialize()
+        {
+
+        }
+
+
     }
 }
