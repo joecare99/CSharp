959a2821417705b416b5f6838ffc98bb80491829
Joe Care <Joe_care_99@yahoo.com>
2022-10-07 00:05:39 +0200

    ActionTest

--- CSharpBible/ActionTest/Visual/FrmActionMain.cs
+++ CSharpBible/ActionTest/Visual/FrmActionMain.cs
@@ -1,27 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ***********************************************************************
+// Assembly         : ActionTest
+// Author           : Mir
+// Created          : 12-19-2021
+//
+// Last Modified By : Mir
+// Last Modified On : 02-02-2020
+// ***********************************************************************
+// <copyright file="FrmActionMain.cs" company="HP Inc.">
+//     Copyright © HP Inc. 2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
 using System.Windows.Forms;
 
 namespace CSharpBible.ActionTest.Visual
 {
+    /// <summary>
+    /// Class FrmActionMain.
+    /// Implements the <see cref="Form" />
+    /// </summary>
+    /// <seealso cref="Form" />
     public partial class FrmActionMain : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrmActionMain"/> class.
+        /// </summary>
         public FrmActionMain()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles the Click event of the btnClose control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void btnClose_Click(object sender, EventArgs e)
         {
             Close();
         }
 
+        /// <summary>
+        /// Handles the CurrentChanged event of the bindingSource1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void bindingSource1_CurrentChanged(object sender, EventArgs e)
         {
 
