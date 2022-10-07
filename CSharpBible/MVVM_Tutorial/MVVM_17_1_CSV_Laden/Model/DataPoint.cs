5952a259027ecd13d0f24be03e493366691ab5dc
Joe Care <Joe_care_99@yahoo.com>
2022-10-07 10:55:56 +0200

    MVVM - Tutorial Lession 17: CSV - Laden

---
+++ CSharpBible/MVVM_Tutorial/MVVM_17_1_CSV_Laden/Model/DataPoint.cs
@@ -0,0 +1,45 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_17_1_CSV_Laden
+// Author           : Mir
+// Created          : 07-03-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 07-04-2022
+// ***********************************************************************
+// <copyright file="DataPoint.cs" company="MVVM_17_1_CSV_Laden">
+//     Copyright (c) HP Inc.. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Globalization;
+
+namespace MVVM_17_1_CSV_Laden.Model
+{
+    /// <summary>
+    /// Class DataPoint.
+    /// </summary>
+    public class DataPoint
+    {
+        /// <summary>
+        /// Gets or sets the time stamp.
+        /// </summary>
+        /// <value>The time stamp.</value>
+        public string TimeStamp { get; set; }
+        /// <summary>
+        /// Gets or sets the x.
+        /// </summary>
+        /// <value>The x.</value>
+        public double X { get; set; }
+        /// <summary>
+        /// Gets or sets the y.
+        /// </summary>
+        /// <value>The y.</value>
+        public double Y { get; set; }
+        /// <summary>
+        /// Gets the dt.
+        /// </summary>
+        /// <value>The dt.</value>
+        public DateTime dt => DateTime.TryParse(TimeStamp, CultureInfo.CurrentUICulture.DateTimeFormat, DateTimeStyles.AssumeLocal, out DateTime d) ? d : DateTime.MinValue;
+    }
+}
---
+++ CSharpBible/MVVM_Tutorial/MVVM_17_1_CSV_Laden/ViewModel/DataPointsViewModel.cs
@@ -0,0 +1,87 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_17_1_CSV_Laden
+// Author           : Mir
+// Created          : 07-03-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-13-2022
+// ***********************************************************************
+// <copyright file="DataPointsViewModel.cs" company="MVVM_17_1_CSV_Laden">
+//     Copyright (c) HP Inc.. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM.ViewModel;
+using MVVM_17_1_CSV_Laden.Model;
+
+namespace MVVM_17_1_CSV_Laden.ViewModel
+{
+    /// <summary>
+    /// Class DataPointsViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public class DataPointsViewModel : BaseViewModel
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataPointsViewModel"/> class.
+        /// </summary>
+        public DataPointsViewModel()
+        {
+            LoadCsV = new DelegateCommand((o) => ExecLoadCsV());
+            DataPoints = new ObservableCollection<DataPoint>();
+            IsLoading = false;
+        }
+
+        /// <summary>
+        /// Gets or sets the data points.
+        /// </summary>
+        /// <value>The data points.</value>
+        public ObservableCollection<DataPoint> DataPoints  { get; set; }
+
+        /// <summary>
+        /// Executes the load cs v.
+        /// </summary>
+        private async void  ExecLoadCsV()
+        {
+            IsLoading = true;
+            using (var service = new CsvService("RBG_XIst_YIst.csv"))
+            {
+                var result = service.ReadCSV();
+                await foreach (var item in result)
+                    DataPoints.Add(item);
+            }
+            IsLoading = false;
+        }
+
+        /// <summary>
+        /// The is loading
+        /// </summary>
+        private bool _isLoading;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is loading.
+        /// </summary>
+        /// <value><c>true</c> if this instance is loading; otherwise, <c>false</c>.</value>
+        public bool IsLoading {
+            get { return _isLoading; }
+            set { if (_isLoading != value) return; _isLoading = value; RaisePropertyChanged(); }
+        }
+
+        /// <summary>
+        /// Gets or sets the load cs v.
+        /// </summary>
+        /// <value>The load cs v.</value>
+        public DelegateCommand LoadCsV { get; set; } = new DelegateCommand(
+            (o) => { },
+            (o)=>false
+            );
+    }
+}
