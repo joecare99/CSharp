d2859f4941cb63975dce8b7abb74be7f1deee658
Joe Care <Joe_care_99@yahoo.com>
2022-10-08 11:16:44 +0200

    Core & Tests

---
+++ JC-AMS/Tests/Core/TDebugHelpersTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JCAMS.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCAMS.Core.Tests
+{
+    [TestClass()]
+    public class TDebugHelpersTests
+    {
+#if NET6_0_OR_GREATER
+        private readonly string cExp= @".Invoke.ExecuteInternal.ExecuteWithAbortSafety.<ExecuteInternal>b__0.InvokeAsSynchronousTask.Invoke.Invoke.InvokeMethod";
+#else
+        private readonly string cExp= @".Execute.Invoke.ExecuteInternal.ExecuteWithAbortSafety.InvokeAsSynchronousTask.Invoke.UnsafeInvokeInternal.InvokeMethod";
+#endif
+        [TestMethod()]
+        public void GetMethodStackTest()
+        {
+            Assert.AreEqual(cExp,TDebugHelpers.GetMethodStack());
+        }
+    }
+}
---
+++ JC-AMS/Tests/Core/TDotNetUtilTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JCAMS.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCAMS.Core.Tests
+{
+    [TestClass()]
+    public class TDotNetUtilTests
+    {
+        [TestMethod()]
+        public void CheckDotNETVersionTest()
+        {
+            Assert.IsTrue(TDotNetUtil.CheckDotNETVersion(3.5, 1));
+            Assert.IsTrue(TDotNetUtil.CheckDotNETVersion(4.0, 0));
+            Assert.IsFalse(TDotNetUtil.CheckDotNETVersion(4.0, 1));
+        }
+
+        [TestMethod()]
+        public void GetDotNETVersionTest()
+        {
+            Assert.IsTrue(TDotNetUtil.GetDotNETVersion(out double fw,out int sp));
+            Assert.IsTrue(fw >= 4.0);
+            Assert.IsTrue(sp >= 0);
+        }
+    }
+}
