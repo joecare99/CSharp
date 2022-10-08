02186679bd566866897ed05d9aec42c4ddd98d36
Joe Care <Joe_care_99@yahoo.com>
2022-10-08 12:05:14 +0200

    Version-Handling Property-Handling

---
+++ JC-AMS/Core/Core/DataOperations/SPropertyHandling.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace JCAMS.Core.DataOperations
+{
+    public static class SPropertyHandling
+    {
+        public static bool SetProperty<T>(this T value,ref T store,Action<T,T,string> action = null, [CallerMemberName] string name = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(store, value)) return false;  
+            T oldval = store;
+            store = value;
+            action?.Invoke(oldval,store,name);
+            return true;
+        } 
+    }
+}
