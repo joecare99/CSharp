6cd44843015ae2e99c1286b320c11de1fdd10ff2
Joe Care <Joe_care_99@yahoo.com>
2022-10-16 19:45:06 +0200

    ItemsControlTut3

---
+++ CSharpBible/MVVM_Tutorial/ItemsControlTut3/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿// ***********************************************************************
+// Assembly         : ItemsControlTut3_net
+// Author           : Mir
+// Created          : 08-14-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-14-2022
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="ItemsControlTut3_net">
+//     Copyright (c) JC-Soft. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ItemsControlTut3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
