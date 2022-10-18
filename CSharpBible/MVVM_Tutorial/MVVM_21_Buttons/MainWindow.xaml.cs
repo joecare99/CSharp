4b2f5a04f8191519cb6d670c4ee7945c3cfbd96d
Joe Care <Joe_care_99@yahoo.com>
2022-10-18 17:59:15 +0200

    MVMVVM - Tutorial Lession 21: Buttons

---
+++ CSharpBible/MVVM_Tutorial/MVVM_21_Buttons/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_21_Buttons
+// Author           : Mir
+// Created          : 08-12-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-14-2022
+// ***********************************************************************
+// <copyright file="MainWindow.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Windows;
+
+namespace MVVM_21_Buttons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
