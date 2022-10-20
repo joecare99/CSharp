a8daa05dffa9f5dc5d77978af0e709800562bfc7
Joe Care <Joe_care_99@yahoo.com>
2022-10-20 06:28:37 +0200

    SyncAsyncParallel

---
+++ CSharpBible/MVVM_Tutorial/SyncAsyncParallel/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿// ***********************************************************************
+// Assembly         : SyncAsyncParallel
+// Author           : Mir
+// Created          : 12-26-2021
+//
+// Last Modified By : Mir
+// Last Modified On : 12-26-2021
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="Hewlett-Packard Company">
+//     Copyright © Hewlett-Packard Company 2021
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SyncAsyncParallel
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CSharpBible/MVVM_Tutorial/SyncAsyncParallel/Properties/Settings.Designer.cs
@@ -0,0 +1,42 @@
+﻿// ***********************************************************************
+// Assembly         : SyncAsyncParallel
+// Author           : Mir
+// Created          : 12-26-2021
+//
+// Last Modified By : Mir
+// Last Modified On : 07-01-2022
+// ***********************************************************************
+// <copyright file="Settings.Designer.cs" company="Hewlett-Packard Company">
+//     Copyright © Hewlett-Packard Company 2021
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace SyncAsyncParallel.Properties {
+
+
+    /// <summary>
+    /// Class Settings. This class cannot be inherited.
+    /// Implements the <see cref="System.Configuration.ApplicationSettingsBase" />
+    /// </summary>
+    /// <seealso cref="System.Configuration.ApplicationSettingsBase" />
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+
+        /// <summary>
+        /// The default instance
+        /// </summary>
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+
+        /// <summary>
+        /// Gets the default.
+        /// </summary>
+        /// <value>The default.</value>
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
+        }
+    }
+}
