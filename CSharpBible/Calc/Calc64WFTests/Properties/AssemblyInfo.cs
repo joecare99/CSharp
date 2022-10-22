4200eef2819f67976e1ed3dd4fac0e1b7ca7d507
Joe Care <Joe_care_99@yahoo.com>
2022-10-22 12:30:38 +0200

    Calc Tests & Docu

--- CSharpBible/Calc/Calc64WFTests/Properties/AssemblyInfo.cs
+++ CSharpBible/Calc/Calc64WFTests/Properties/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Reflection;
+﻿// ***********************************************************************
+// Assembly         : Calc64WFTests
+// Author           : Mir
+// Created          : 08-31-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-31-2022
+// ***********************************************************************
+// <copyright file="AssemblyInfo.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +21,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Calc64WFTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("JC-Soft")]
 [assembly: AssemblyProduct("Calc64WFTests")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2022")]
+[assembly: AssemblyCopyright("Copyright © JC-Soft 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
