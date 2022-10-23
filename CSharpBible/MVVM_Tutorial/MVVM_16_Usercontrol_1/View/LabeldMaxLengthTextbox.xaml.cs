cbaf18b54049992274c58eed008f1dbcafba42df
Joe Care <Joe_care_99@yahoo.com>
2022-10-23 19:55:54 +0200

    UserControlView 1 better, but still not working

--- CSharpBible/MVVM_Tutorial/MVVM_16_Usercontrol_1/View/LabeldMaxLengthTextbox.xaml.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_16_Usercontrol_1/View/LabeldMaxLengthTextbox.xaml.cs
@@ -20,14 +20,53 @@ namespace MVVM_16_Usercontrol_1.View
     /// </summary>
     public partial class LabeldMaxLengthTextbox : UserControl
     {
+        public static readonly DependencyProperty _CaptionProperty =
+            DependencyProperty.Register(nameof(Caption), typeof(object), typeof(LabeldMaxLengthTextbox),
+                new FrameworkPropertyMetadata((object)null, (PropertyChangedCallback)OnCaptionChanged));
+  
+        public static readonly DependencyProperty _TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(object), typeof(LabeldMaxLengthTextbox),
+                new FrameworkPropertyMetadata((object)null, (PropertyChangedCallback)OnTextChanged));
+        private string _text;
+
+        private static void OnCaptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+            => ((LabeldMaxLengthTextbox)d).OnCaptionChanged(e.OldValue, e.NewValue); // Übergang in die Instanz
+
+        private static void OnTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+            => ((LabeldMaxLengthTextbox)d).OnTextChanged(e.OldValue, e.NewValue); // Übergang in die Instanz
+
+        private void OnCaptionChanged(object old, object newValue)
+        {
+            if (newValue is string @s)
+                lblCaption.Content = $"{@s}";
+        }
+
+        private void OnTextChanged(object old, object newValue)
+        {
+            if (newValue is string @s && @s != _Text)
+                _Text=@s;
+        }
+
         public LabeldMaxLengthTextbox()
         {
             InitializeComponent();
-            this.DataContext = this;
         }
 
-        public string Caption { get; set; } = "";
-        public string Text { get; set; } = "";
+        public object Caption { get; set; } = "";
+        public object Text { 
+            get; 
+            set; }
+        public string _Text
+        {
+            get => _text;
+            set { _text = value;
+                if (Text is Binding b)
+                    ;
+                else SetValue(_TextProperty, value);
+            }
+        }
+
         public int MaxLength { get; set; } = 50;
+
     }
 }
