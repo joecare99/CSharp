31616a0045664347ef98bdee3db19e19885b0d8d
Joe Care <Joe_care_99@yahoo.com>
2023-02-11 10:19:06 +0100

    Update Packages

---
+++ CSharpBible/DB/FoxCon/Program.cs
@@ -0,0 +1,14 @@
+﻿using FoxCon.Model;
+using System;
+
+namespace FoxCon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FoxConMain.Login_is_on_home_page();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
