c748d2ff8d4c13a0967c58aae894596d0e4439a0
Joe Care <Joe_care_99@yahoo.com>
2023-04-24 23:20:50 +0200

    Various: Minor Changes

--- CSharpBible/BindingGroupExp/View/Validation/ValidateDateAndPrice.cs
+++ CSharpBible/BindingGroupExp/View/Validation/ValidateDateAndPrice.cs
@@ -10,35 +10,31 @@ namespace BindingGroupExp.View.Validation
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            BindingGroup bg = value as BindingGroup;
-
-            // Get the source object.
-            MainWindowViewModel item = bg.Items[0] as MainWindowViewModel;
-
-            object doubleValue;
-            object dateTimeValue;
-
-            // Get the proposed values for Price and OfferExpires.
-            bool priceResult = bg.TryGetValue(item, "Price", out doubleValue);
-            bool dateResult = bg.TryGetValue(item, "OfferExpires", out dateTimeValue);
-
-            if (!priceResult || !dateResult)
+                // Get the source object.
+            if (value is BindingGroup bg && bg.Items[0] is MainWindowViewModel item)
             {
-                return new ValidationResult(false, "Properties not found");
-            }
 
-            decimal price = (decimal)doubleValue;
-            DateTime offerExpires = (DateTime)dateTimeValue;
-
-            // Check that an item over $100 is available for at least 7 days.
-            if (price > 100)
-            {
-                if (offerExpires < DateTime.Today + new TimeSpan(7, 0, 0, 0))
+                // Get the proposed values for Price and OfferExpires.
+                bool priceResult = bg.TryGetValue(item, "Price", out object doubleValue);
+                bool dateResult = bg.TryGetValue(item, "OfferExpires", out object dateTimeValue);
+                if (!priceResult || !dateResult)
                 {
-                    return new ValidationResult(false, "Items over $100 must be available for at least 7 days.");
+                    return new ValidationResult(false, "Properties not found");
                 }
-            }
 
+                decimal price = (decimal)doubleValue;
+                DateTime offerExpires = (DateTime)dateTimeValue;
+
+                // Check that an item over $100 is available for at least 7 days.
+                if (price > 100)
+                {
+                    if (offerExpires < DateTime.Today + new TimeSpan(7, 0, 0, 0))
+                    {
+                        return new ValidationResult(false, "Items over $100 must be available for at least 7 days.");
+                    }
+                }
+
+            }
             return ValidationResult.ValidResult;
         }
     }
