48484e554c9ee3184d8611dbb9359bd960a2ce47
Joe Care <Joe_care_99@yahoo.com>
2023-04-24 23:17:14 +0200

    MathLib & Tests

--- CSharpBible/Libraries/MathLIbrary/TwoDim/Math2d.cs
+++ CSharpBible/Libraries/MathLIbrary/TwoDim/Math2d.cs
@@ -92,7 +92,7 @@ namespace MathLibrary.TwoDim {
 			/// </summary>
 			/// <param name="obj">The object to compare with the current object.</param>
 			/// <returns><c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.</returns>
-			public override bool Equals(object obj) => obj is Vector vector && _x == vector._x && _y == vector._y;
+			public override bool Equals(object? obj) => obj is Vector vector && _x == vector._x && _y == vector._y;
 
 			/// <summary>
 			/// Returns a hash code for this instance.
