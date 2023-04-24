f4c6ea1e6583b75429f647ed0200c2a3ad2af02f
Joe Care <Joe_care_99@yahoo.com>
2023-04-24 23:04:16 +0200

    Minor Change

--- CSharpBible/DB/DBTest1/Model/BasicExample.cs
+++ CSharpBible/DB/DBTest1/Model/BasicExample.cs
@@ -11,37 +11,35 @@ namespace DBTest1.Model
 {
     public class BasicExample
     {
-        public static async Task DoExampleAsync(params object[]? args)
+        public static async Task DoExampleAsync(params object?[] args)
         {
             var connString = "Server={0};User ID={1};Password={2};Database={3}";
 
-            using (var conn = new MySqlConnection(String.Format(connString, args)))
+            using var conn = new MySqlConnection(String.Format(connString, args));
+            conn.StateChange += Conn_StateChange;
+            try
             {
-                conn.StateChange += Conn_StateChange;
-                try
-                {
-                    await conn.OpenAsync();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Exception: {ex.Message}");
-                    throw;
-                }
-                // Insert some data
-                using (var cmd = new MySqlCommand())
-                {
-                    cmd.Connection = conn;
-                    cmd.CommandText = "INSERT INTO data (some_field) VALUES (@p)";
-                    cmd.Parameters.AddWithValue("p", "Hello world");
-                    await cmd.ExecuteNonQueryAsync();
-                }
-
-                // Retrieve all rows
-                using (var cmd = new MySqlCommand("SELECT some_field FROM data", conn))
-                using (var reader = await cmd.ExecuteReaderAsync())
-                    while (await reader.ReadAsync())
-                        Console.WriteLine(reader.GetString(0));
+                await conn.OpenAsync();
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                throw;
+            }
+            // Insert some data
+            using (var cmd = new MySqlCommand())
+            {
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT INTO data (some_field) VALUES (@p)";
+                cmd.Parameters.AddWithValue("p", "Hello world");
+                await cmd.ExecuteNonQueryAsync();
+            }
+
+            // Retrieve all rows
+            using (var cmd = new MySqlCommand("SELECT some_field FROM data", conn))
+            using (var reader = await cmd.ExecuteReaderAsync())
+                while (await reader.ReadAsync())
+                    Console.WriteLine(reader.GetString(0));
         }
 
         private static void Conn_StateChange(object sender, System.Data.StateChangeEventArgs e)
