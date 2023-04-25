6df80dde33f27d99b982fa20c2c0646c3ffef908
Joe Care <Joe_care_99@yahoo.com>
2023-04-25 11:46:30 +0200

    MVVM_16: UserControls

--- CSharpBible/MVVM_Tutorial/MVVM_16_Usercontrol_2/View/TestControl.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_16_Usercontrol_2/View/TestControl.cs
@@ -13,13 +13,13 @@ namespace MVVM_16_Usercontrol_2.View
     {
         public static readonly DependencyProperty _Text2Property = 
 			DependencyProperty.Register(nameof(Text2), typeof(object), typeof(TestControl), 
-				new FrameworkPropertyMetadata((object)null, (PropertyChangedCallback)OnText2Changed));
+				new FrameworkPropertyMetadata((object?)null, (PropertyChangedCallback)OnText2Changed));
 
 		private static void OnText2Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
 			=> ((TestControl)d).OnText2Changed(e.OldValue, e.NewValue); // Übergang in die Instanz
 
 
-        private void OnText2Changed(object old, object newValue)
+        private void OnText2Changed(object _, object newValue)
         {
             if (newValue is string @s)
                 Content = $"Text2: {@s}";
@@ -30,7 +30,7 @@ namespace MVVM_16_Usercontrol_2.View
             get => GetValue(_Text2Property);
             set
             {
-                object oldValue = GetValue(_Text2Property);
+                _ = GetValue(_Text2Property);
                 SetValue(_Text2Property, value); // speichert den Wert - überschreibt ggf. die Bindung zum "Binding"-Object 
 //                OnText2Changed(oldValue, value);
             }
