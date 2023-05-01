71a7732650b840f1b45d8e74e96d019bd6df4e97
Joe Care <Joe_care_99@yahoo.com>
2023-05-01 23:48:01 +0200

    Change Namespace

--- CSharpBible/MVVM_Tutorial/SyncAsyncParallel/Properties/Resources.Designer.cs
+++ CSharpBible/MVVM_Tutorial/SyncAsyncParallel/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SyncAsyncParallel_net.Properties {
+namespace SyncAsyncParallel.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SyncAsyncParallel_net.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SyncAsyncParallel_net.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SyncAsyncParallel.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
