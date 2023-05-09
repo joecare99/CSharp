861c7467f05a7eeb222a7d041b3ac4fdd51a0a7a
Joe Care <Joe_care_99@yahoo.com>
2023-05-09 22:06:00 +0200

    MVVM 6 renamed to MVVM 06

---
+++ CSharpBible/MVVM_Tutorial/MVVM_06_Converters_2/View/CurrencyView2.xaml.cs
@@ -0,0 +1,31 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_6_Converters_2
+// Author           : Mir
+// Created          : 07-03-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 07-04-2022
+// ***********************************************************************
+// <copyright file="CurrencyView2.xaml.cs" company="MVVM_6_Converters_2">
+//     Copyright (c) JC-Soft. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Windows.Controls;
+
+namespace MVVM_06_Converters_2.View
+{
+    /// <summary>
+    /// Interaktionslogik für CurrencyView.xaml
+    /// </summary>
+    public partial class CurrencyView2 : Page
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CurrencyView2"/> class.
+        /// </summary>
+        public CurrencyView2()
+        {
+            InitializeComponent();
+        }
+    }
+}
