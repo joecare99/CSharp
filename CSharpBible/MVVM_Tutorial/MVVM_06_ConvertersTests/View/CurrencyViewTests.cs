1d5d3d6e5bf4810621a9801ca29d53fc3e4c869d
Joe Care <Joe_care_99@yahoo.com>
2023-05-11 17:02:27 +0200

    MVVM_06_ConvertersTests

---
+++ CSharpBible/MVVM_Tutorial/MVVM_06_ConvertersTests/View/CurrencyViewTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MVVM_06_Converters.View.Tests
+{
+    [TestClass]
+    public class CurrencyViewTests
+    {
+        [TestMethod()]
+        public void CurrencyViewTest()
+        {
+            CurrencyView? testView = null;
+            var t = new Thread(() => testView = new());
+            t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            t.Start();
+            t.Join(); //Wait for the thread to end
+            Assert.IsNotNull(testView);
+            Assert.IsInstanceOfType(testView, typeof(CurrencyView));
+        }
+    }
+}
