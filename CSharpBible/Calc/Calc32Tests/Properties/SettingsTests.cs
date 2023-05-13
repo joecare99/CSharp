1b7c6ddff6e39ab276acd08c6e48d008fd783ed4
Joe Care <Joe_care_99@yahoo.com>
2023-05-13 11:08:11 +0200

    Settings

---
+++ CSharpBible/Calc/Calc32Tests/Properties/SettingsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calc32.Properties.Tests
+{
+    [TestClass]
+    public class SettingsTests
+    {
+        [TestMethod]
+        public void SettingsTest()
+        {
+            Assert.IsNotNull(Settings.Default);
+        }
+    }
+}
