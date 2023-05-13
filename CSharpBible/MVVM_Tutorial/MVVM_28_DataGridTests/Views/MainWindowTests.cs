b637b603a1eae2f0c76891c6b3d77e8f1445af35
Joe Care <Joe_care_99@yahoo.com>
2023-05-13 09:59:26 +0200

    named Variable

--- CSharpBible/MVVM_Tutorial/MVVM_28_DataGridTests/Views/MainWindowTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_28_DataGridTests/Views/MainWindowTests.cs
@@ -9,13 +9,13 @@ namespace MVVM_28_DataGrid.Views.Tests
         [TestMethod()]
         public void MainWindowTest()
         {
-            MainWindow? mw=null;
-            var t = new Thread(()=> mw = new());
+            MainWindow? testView=null;
+            var t = new Thread(()=> testView = new());
             t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
             t.Start();
             t.Join(); //Wait for the thread to end
-            Assert.IsNotNull(mw);
-            Assert.IsInstanceOfType(mw, typeof(MainWindow));    
+            Assert.IsNotNull(testView);
+            Assert.IsInstanceOfType(testView, typeof(MainWindow));    
         }
     }
 }
