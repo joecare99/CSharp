9060c953779027cf0ee5b7664f33a42891a34ef0
Joe Care <Joe_care_99@yahoo.com>
2023-05-13 10:03:31 +0200

    MVVM_06_Converters_4 Proj & Tests

---
+++ CSharpBible/MVVM_Tutorial/MVVM_06_Converters_4Tests/View/VehicleViewTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_06_Converters_4Tests.View
+{
+    internal class VehicleViewTests
+    {
+    }
+}
