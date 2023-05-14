770a86c41a299b1aa9d522d1283a1808d15af825
Joe Care <Joe_care_99@yahoo.com>
2023-05-14 13:14:33 +0200

    Listbinding

--- CSharpBible/MVVM_Tutorial/ListBinding/ViewModel/PersonViewViewModel.cs
+++ CSharpBible/MVVM_Tutorial/ListBinding/ViewModel/PersonViewViewModel.cs
@@ -14,11 +14,7 @@
 using ListBinding.Model;
 using MVVM.ViewModel;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListBinding.ViewModel
 {
