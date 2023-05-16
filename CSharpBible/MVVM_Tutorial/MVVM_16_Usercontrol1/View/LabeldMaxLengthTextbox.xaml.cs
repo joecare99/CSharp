a2c3d8eb39f583ba1a5c8533d3eb0d9d6ab48bf4
Joe Care <Joe_care_99@yahoo.com>
2023-05-16 17:46:08 +0200

    MVVM 16: UserControls (1)

---
+++ CSharpBible/MVVM_Tutorial/MVVM_16_Usercontrol1/View/LabeldMaxLengthTextbox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace MVVM_16_Usercontrol_1.View
+{
+    /// <summary>
+    /// Interaktionslogik für LabeldMaxLengthTextbox.xaml
+    /// </summary>
+    public partial class LabeldMaxLengthTextbox : UserControl
+    {
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register(nameof(LabeldMaxLengthTextbox.Caption), typeof(string), typeof(LabeldMaxLengthTextbox),
+                new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.None));
+  
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(LabeldMaxLengthTextbox.Text), typeof(string), typeof(LabeldMaxLengthTextbox),
+                new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(LabeldMaxLengthTextbox.Command), typeof(IRelayCommand), typeof(LabeldMaxLengthTextbox),
+                new FrameworkPropertyMetadata(default(IRelayCommand), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public LabeldMaxLengthTextbox()
+        {
+            InitializeComponent();
+        }
+
+        public object Caption { get; set ; } = "";
+        
+        public string Text { 
+            get => (string)GetValue(TextProperty); 
+            set => SetValue(TextProperty,value); 
+        }
+
+        public IRelayCommand Command
+        {
+            get => (IRelayCommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public int MaxLength { get; set; } = 50;
+
+    }
+}
