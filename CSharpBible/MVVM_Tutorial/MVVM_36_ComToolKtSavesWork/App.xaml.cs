90ab411e55b2011e9b18a86fc9b10a4c7f3d49c2
Joe Care <Joe_care_99@yahoo.com>
2023-05-19 22:34:39 +0200

    MVVM_36_ComToolKtSavesWork (new)

---
+++ CSharpBible/MVVM_Tutorial/MVVM_36_ComToolKtSavesWork/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_36_ComToolKtSavesWork
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Windows;
+
+namespace MVVM_36_ComToolKtSavesWork
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
+namespace MVVM_36_ComToolKtSavesWork.Models { }
+namespace MVVM_36_ComToolKtSavesWork.ValueConverter { }
+namespace MVVM_36_ComToolKtSavesWork.Services { }
