0390b101cb3afc2bb4dc3dc0f4ef7f6b1a8a3ced
Joe Care <Joe_care_99@yahoo.com>
2023-05-21 21:35:02 +0200

    MVVM 36: Tests

---
+++ CSharpBible/MVVM_Tutorial/MVVM_36_ComToolKtSavesWorkTests/ViewModels/TestUserModel.cs
@@ -0,0 +1,31 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_36_ComToolKtSavesWork_netTests
+// Author           : Mir
+// Created          : 05-14-2023
+//
+// Last Modified By : Mir
+// Last Modified On : 05-14-2023
+// ***********************************************************************
+// <copyright file="CommunityToolkit2ViewModelTests.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2023
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using MVVM_36_ComToolKtSavesWork.Models;
+using System;
+using System.ComponentModel;
+
+namespace MVVM_36_ComToolKtSavesWork.ViewModels.Tests
+{
+    public class TestUserModel : ICommunityToolkit2Model
+    {
+        private IGetResult _getResult;
+
+        public TestUserModel(IGetResult getResult) {
+            _getResult = getResult;
+        }
+        public DateTime Now => (DateTime)_getResult.Get(new object[] { })!;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
