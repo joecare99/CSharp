d776ff02c6cf7823364f64949bd17e3a043f80e4
Joe Care <Joe_care_99@yahoo.com>
2023-05-21 07:36:30 +0200

    MVVM 36: Testing

---
+++ CSharpBible/MVVM_Tutorial/MVVM_36_ComToolKtSavesWorkTests/ViewModels/DebugLog.cs
@@ -0,0 +1,34 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_36_ComToolKtSavesWork_netTests
+// Author           : Mir
+// Created          : 05-14-2023
+//
+// Last Modified By : Mir
+// Last Modified On : 05-14-2023
+// ***********************************************************************
+// <copyright file="CommunityToolkit2ViewModelTests.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2023
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+
+namespace MVVM_36_ComToolKtSavesWork.ViewModels.Tests
+{
+    public class DebugLog : IDebugLog
+    {
+        private string _debugLog="";
+
+        string IDebugLog.DebugLog => _debugLog;
+
+        public void ClearLog()
+        {
+            _debugLog="";
+        }
+
+        public void DoLog(string message)
+        {
+            _debugLog+=$"{message}{Environment.NewLine}";
+        }
+    }
+}
---
+++ CSharpBible/MVVM_Tutorial/MVVM_36_ComToolKtSavesWorkTests/ViewModels/GetResult.cs
@@ -0,0 +1,38 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_36_ComToolKtSavesWork_netTests
+// Author           : Mir
+// Created          : 05-14-2023
+//
+// Last Modified By : Mir
+// Last Modified On : 05-14-2023
+// ***********************************************************************
+// <copyright file="CommunityToolkit2ViewModelTests.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2023
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace MVVM_36_ComToolKtSavesWork.ViewModels.Tests
+{
+    public class GetResult : IGetResult
+    {
+        private Dictionary<string,Func<object[],object?>> _Dic=new();
+
+        public object? Get(object[] objects, [CallerMemberName] string proc = "")
+        {
+            if (_Dic.TryGetValue(proc, out var f))
+                return f(objects);
+            else
+                return null;
+        }
+
+        public void Register(string proc, Func<object[], object?> fesultFct)
+        {
+            _Dic.Add(proc, fesultFct);
+        }
+
+    }
+}
