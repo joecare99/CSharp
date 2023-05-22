20e471d7e26cc0f085e2e648c37917e83f1690a9
Joe Care <Joe_care_99@yahoo.com>
2023-05-22 19:52:38 +0200

    MVVM 33a CT

---
+++ CSharpBible/MVVM_Tutorial/MVVM_33a_CTEvents_To_Commands/ValueConverters/DateTimeValueConverter.cs
@@ -0,0 +1,63 @@
+// ***********************************************************************
+// Assembly         : MVVM_00_Template
+// Author           : Mir
+// Created          : 07-03-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 07-04-2022
+// ***********************************************************************
+// <copyright file="CurrencyValueConverter.cs" company="JC-Soft">
+//     Copyright (c) JC-Soft. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MVVM_33a_CTEvents_To_Commands.ValueConverter
+{
+    /// <summary>
+    /// Class CurrencyValueConverter.
+    /// Implements the <see cref="IValueConverter" />
+    /// </summary>
+    /// <seealso cref="IValueConverter" />
+    public class DateTimeValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns <see langword="null" />, the valid null value is used.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dt)
+                if (parameter is string spar)
+                    return dt.ToString(spar);
+                else
+                    return dt.ToString(culture);
+            else
+                return value?.ToString() ?? "";
+
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>A converted value. If the method returns <see langword="null" />, the valid null value is used.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string sval && DateTime.TryParse(sval.Trim(),culture, DateTimeStyles.AssumeLocal, out var dt))
+                return dt;
+            else
+                return DateTime.MinValue;
+        }
+    }
+}
