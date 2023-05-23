469991bc9e5989f33744e98f805a6b62b711a86f
Joe Care <Joe_care_99@yahoo.com>
2023-05-23 07:38:40 +0200

    MVVM 37: TreeView (new)

---
+++ CSharpBible/MVVM_Tutorial/MVVM_37_TreeView/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_37_TreeView
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Windows;
+
+namespace MVVM_37_TreeView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
+namespace MVVM_37_TreeView.Models { }
+namespace MVVM_37_TreeView.ValueConverter { }
+namespace MVVM_37_TreeView.Services { }
---
+++ CSharpBible/MVVM_Tutorial/MVVM_37_TreeView/Views/TemplateView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MVVM_37_TreeView.Views
+{
+    /// <summary>
+    /// Interaktionslogik für TemplateView.xaml
+    /// </summary>
+    public partial class TemplateView : Page
+    {
+        public TemplateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
