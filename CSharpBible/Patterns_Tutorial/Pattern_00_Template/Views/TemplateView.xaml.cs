084d0dca7a0e6181eb09e6316c4330c053a94648
Joe Care <Joe_care_99@yahoo.com>
2023-05-29 00:49:35 +0200

    Patterns Tutorial: Preparation

---
+++ CSharpBible/Patterns_Tutorial/Pattern_00_Template/Views/TemplateView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Pattern_00_Template.Views
+{
+    /// <summary>
+    /// Interaktionslogik für TemplateView.xaml
+    /// </summary>
+    public partial class TemplateView : Page
+    {
+        public TemplateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CSharpBible/Patterns_Tutorial/ReadMe.md
@@ -0,0 +1,5 @@
+# Pattern Tutorial
+Heavily inspired by [42_Entwickler](https://www.youtube.com/@42Entwickler)
+
+## Lession 1: The Singleton
+
