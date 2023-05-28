759b1b9d788fb277b670a9b2862b0098f3e5d20a
Joe Care <Joe_care_99@yahoo.com>
2023-05-29 00:59:59 +0200

    MVVM Tutorial: #31a Validation3 Tests

---
+++ CSharpBible/MVVM_Tutorial/MVVM_31a_CTValidation3Tests/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVM.ViewModel;
+using System.ComponentModel;
+
+namespace MVVM_31a_CTValidation3.ViewModels.Tests
+{
+    [TestClass()]
+    public class MainWindowViewModelTests
+    {
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+        MainWindowViewModel testModel;
+#pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+
+        [TestInitialize]
+        public void Init()
+        {
+            testModel = new();
+        }
+
+        [TestMethod()]
+        public void SetupTest()
+        {
+            Assert.IsNotNull(testModel);
+            Assert.IsInstanceOfType(testModel, typeof(MainWindowViewModel));
+            Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
+            Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
+        }
+    }
+}
