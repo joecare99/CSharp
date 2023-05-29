ceb7ea8fc6df75f98edc1a4b87c1e62e2d04e5dc
Joe Care <Joe_care_99@yahoo.com>
2023-05-29 12:40:09 +0200

    Libraries: MVVM BaseLib: ValidationHelper & ValidationHelperExt

--- CSharpBible/Libraries/MVVM_BaseLib/Helper/MVVM/ValidationHelper.cs
+++ CSharpBible/Libraries/MVVM_BaseLib/Helper/MVVM/ValidationHelper.cs
@@ -2,30 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVVM_BaseLib.Helper.MVVM
+namespace BaseLib.Helper.MVVM
 {
     public class ValidationHelper :INotifyDataErrorInfo
     {
-        private readonly IDictionary<string, List<string>> _errorList = new Dictionary<string, List<string>>();
+        private readonly IDictionary<string, List<ValidationResult>> _errorList = new Dictionary<string, List<ValidationResult>>();
 
-        public string this[string property]=> _errorList.TryGetValue(property, out var l) ? l.First() :string.Empty;
+        public string? this[string property]=> _errorList.TryGetValue(property, out var l) ? l!.First().ErrorMessage :null;
         public bool HasErrors => _errorList.Count > 0;
 
         public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
 
         public IEnumerable GetErrors(string? propertyName)
-            => _errorList.TryGetValue(propertyName ??"", out var l) ? l : new List<string>();
+            => _errorList.TryGetValue(propertyName ??"", out var l) ? l : new List<ValidationResult>();
 
         public void AddError(string property, string message)
         {
             if (_errorList.TryGetValue(property, out var lErr))
-                lErr.Add(message);
+                lErr.Add(new ValidationResult(message, new[] {property }));
             else
-                _errorList[property] = new List<string>() { message };
+                _errorList[property] = new List<ValidationResult>() { new ValidationResult(message,new[] { property }) };
             ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(property));
         }
 
