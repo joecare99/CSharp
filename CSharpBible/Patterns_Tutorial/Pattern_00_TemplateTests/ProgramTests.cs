dcd52610ab2fbfa23d807be07fbd3591972f09bc
Joe Care <Joe_care_99@yahoo.com>
2023-05-29 17:31:05 +0200

    Pattern 00: Template Tests

---
+++ CSharpBible/Patterns_Tutorial/Pattern_00_TemplateTests/ProgramTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_00_TemplateTests
+{
+    internal class ProgramTests
+    {
+    }
+}
