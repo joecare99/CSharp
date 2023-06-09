e92fc4f23645304e55505928470ba245012ab558
Joe Care <Joe_care_99@yahoo.com>
2023-06-09 16:09:17 +0200

    Graphics: CTImgGrid using Community Toolkit

---
+++ CSharpBible/Graphics/MVVM_Converter_CTImgGrid/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MVVM.ViewModel;
+using MVVM_Converter_CTImgGrid.ViewModel;
+using System;
+using System.Windows;
+
+namespace MVVM_Converter_CTImgGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.ShowClient = _ShowClientinFrame;
+            }
+        }
+
+        private void Frame_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+//                vm.FrameDataContext = e.Source as Frame
+            }
+        }
+
+        private BaseViewModel? _ShowClientinFrame(string arg)
+        {
+            try
+            {
+                this.Client.Source = new Uri(arg);
+                return Client.DataContext as BaseViewModel;
+            }
+            catch(Exception) 
+            {
+                return null;
+            };
+        }
+    }
+}
---
+++ CSharpBible/Graphics/MVVM_Converter_CTImgGrid/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Converter_CTImgGrid.ViewModel
+{
+    public class MainWindowViewModel:BaseViewModel
+    {
+        public Func<string, BaseViewModel?>? ShowClient { get; set; }
+
+        public DelegateCommand LoadLevelCommand { get; set; } = new DelegateCommand((o) => Model.Model.LoadLevel());
+        public DelegateCommand NextLevelCommand { get; set; } = new DelegateCommand((o) => Model.Model.NextLevel());
+        public DelegateCommand PrevLevelCommand { get; set; } = new DelegateCommand((o) => Model.Model.PrevLevel());
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+
+
+    }
+}
