0c8a960935a1a39f0d9b8eb03e498da052415846
Joe Care <Joe_care_99@yahoo.com>
2023-06-12 22:34:37 +0200

    Games: New GaphicUtils

--- CSharpBible/Games/CreateCards/Program.cs
+++ CSharpBible/Games/CreateCards/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using CreateCards.Model;
+using CreateCards.Utilities;
 
 public static class Program
 {
@@ -18,6 +20,22 @@ public static class Program
                 Card card = new Card(suit, value, suit is "♣" or "♠" ? Color.Black : Color.Red);
                 Bitmap cardImage = card.GetCard(400, 600);
                 cardImage.Save(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), $"card_{suit}{value.ToString().Trim('_')}.png"));
+            }
+        foreach (string suit in new[] { "♣", "♠", "♥", "♦" })
+            foreach (CardValues value in Enum.GetValues(typeof(CardValues)))
+            {
+                Card card = new Card(suit, value, suit is "♣" or "♠" ? Color.Black : Color.Red);
+                using MemoryStream ms = new();
+                Graphics g = ms.GraphicsWriter();
+                card.DrawCard(400, 600, g);    
+                g.Dispose();
+                ms.Position = 0L;
+                var mf = new Metafile(ms);
+            //    mf.DrawToScreen();
+                ms.Position = 0L;
+                var fs = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), $"card_{suit}{value.ToString().Trim('_')}.emf"), FileMode.OpenOrCreate);    
+                ms.CopyTo(fs);
+                fs.Dispose();
             }
     }
 }
