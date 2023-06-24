969d77894ec6c9a4a8f53a9430dfd30965905394
Joe Care <Joe_care_99@yahoo.com>
2023-06-25 00:35:10 +0200

    Basic: #01 Action

---
+++ CSharpBible/Basics/Basic_Del01_Action/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Basic_Del01_Action.Models;
+using Basic_Del01_Action.Properties;
+
+namespace Basic_Del01_Action.ViewModels
+{
+    public class MainViewModel
+    {
+        Model model = new Model();
+
+        public string Greeting => Resources.ResourceManager.GetString(model.Greeting); 
+    }
+}
