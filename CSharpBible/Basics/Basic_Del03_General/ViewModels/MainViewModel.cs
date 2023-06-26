7c01dac399be442aae01323ebf60ca7c79f40904
Joe Care <Joe_care_99@yahoo.com>
2023-06-26 16:13:28 +0200

    Basic: #03 General (new)

---
+++ CSharpBible/Basics/Basic_Del03_General/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Basic_Del03_General.Models;
+using Basic_Del03_General.Properties;
+
+namespace Basic_Del03_General.ViewModels
+{
+    public class MainViewModel
+    {
+        Model model = new Model();
+
+        public string Greeting => Resources.ResourceManager.GetString(model.Greeting); 
+    }
+}
