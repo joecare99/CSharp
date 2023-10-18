332bce6ef43998ee4fe14e3d2fe049f2f32934ab
Joe Care <Joe_care_99@yahoo.com>
2023-10-18 15:49:24 +0200

    BindableCollectionTests

---
+++ CSharpBible/Libraries/MVVM_BaseLibTests/ViewModel/BindableCollectionTests.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.ViewModel.Tests
+{
+    [TestClass()]
+    public class BindableCollectionTests : BaseTestViewModel
+    {
+        private BindableCollection<string> _testClass;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _testClass = new BindableCollection<string>();
+            _testClass.CollectionChanged += OnCollectionChanged;
+            ClearLog();
+
+        }
+
+        private void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+           DoLog($"OnCollectionChanged({sender},{e.Action})");
+        }
+
+        [TestMethod()]
+        public void BindableCollectionTest()
+        {
+            Assert.IsNotNull(_testClass);
+            Assert.IsInstanceOfType(_testClass, typeof(BindableCollection<string>));
+            Assert.IsInstanceOfType(_testClass, typeof(ObservableCollection<string>));
+            Assert.IsInstanceOfType(_testClass, typeof(IObservableCollection<string>));
+            Assert.AreSame(_testClass, _testClass as IObservableCollection<string>);
+        }
+
+        [TestMethod()]
+        public void BindableCollectionTest1()
+        {
+            var testClass = new BindableCollection<string>(new[] { "1", "2", "3" });
+            Assert.IsNotNull(testClass);
+            Assert.IsInstanceOfType(testClass, typeof(BindableCollection<string>));
+            Assert.IsInstanceOfType(testClass, typeof(ObservableCollection<string>));
+            Assert.IsInstanceOfType(testClass, typeof(IObservableCollection<string>));
+        }
+
+        [TestMethod()]
+        public void NotifyOnCollectionChangedTest()
+        {
+            _testClass.Add("5");
+            Assert.AreEqual(1, _testClass.Count);
+            Assert.AreEqual("5", _testClass[0]);
+            Assert.AreEqual("OnCollectionChanged(MVVM.ViewModel.BindableCollection`1[System.String],Add)\r\n", DebugLog);
+        }
+
+        [TestMethod()]
+        public void NotifyOfPropertyChangeTest()
+        {
+            _testClass.Add("5");
+            _testClass.NotifyOfPropertyChange("Test");
+            Assert.AreEqual(1, _testClass.Count);
+            Assert.AreEqual("5", _testClass[0]);
+            Assert.AreEqual("OnCollectionChanged(MVVM.ViewModel.BindableCollection`1[System.String],Add)\r\n", DebugLog);
+        }
+
+        [TestMethod()]
+        public void SetItemTest()
+        {
+            _testClass.Add("5");
+            _testClass[0] = "7";
+            Assert.AreEqual(1, _testClass.Count);
+            Assert.AreEqual("7", _testClass[0]);
+            Assert.AreEqual("OnCollectionChanged(MVVM.ViewModel.BindableCollection`1[System.String],Add)\r\nOnCollectionChanged(MVVM.ViewModel.BindableCollection`1[System.String],Replace)\r\n", DebugLog);
+        }
+
+        [TestMethod()]
+        public void RemoveItemTest()
+        {
+            _testClass.Add("5");
+            _testClass.RemoveAt(0);
+            Assert.AreEqual(0, _testClass.Count);
+            Assert.AreEqual("OnCollectionChanged(MVVM.ViewModel.BindableCollection`1[System.String],Add)\r\nOnCollectionChanged(MVVM.ViewModel.BindableCollection`1[System.String],Remove)\r\n", DebugLog);
+        }
+
+        [TestMethod()]
+        public void AddRangeTest()
+        {
+            _testClass.AddRange(new[] { "5", "7" });
+            Assert.AreEqual(2, _testClass.Count);
+            Assert.AreEqual("5", _testClass[0]);
+            Assert.AreEqual("7", _testClass[1]);
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            _testClass.AddRange(new[] { "5", "7" });
+            _testClass.Clear();
+            Assert.AreEqual(0, _testClass.Count);
+        }
+
+        [TestMethod()]
+        public void RefreshTest()
+        {
+            _testClass.AddRange(new[] { "5", "7" });
+            _testClass.Refresh();
+            Assert.AreEqual(2, _testClass.Count);
+        }
+
+        [TestMethod()]
+        public void RemoveRangeTest()
+        {
+            _testClass.AddRange(new[] { "5", "7", "1" });
+            _testClass.RemoveRange(new[] { "5", "1", "0" });
+            Assert.AreEqual(1, _testClass.Count);
+        }
+    }
+}
