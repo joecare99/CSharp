958d6b59f82c0cbbe4c0fbfce681ba8fded92e8a
Joe Care <Joe_care_99@yahoo.com>
2023-11-06 20:40:32 +0100

    MVVM 28-1 I18N

--- CSharpBible/MVVM_Tutorial/MVVM_28_1_DataGridExt/Models/Department.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_28_1_DataGridExt/Models/Department.cs
@@ -9,9 +9,8 @@ namespace MVVM_28_1_DataGridExt.Models
 {
     public class Department : NotificationObject
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-
+        public int Id { get; set; } = -1;
+        public string? Name => Properties.Resources.ResourceManager.GetString($"dep_{Id}");
+        public string? Description => Properties.Resources.ResourceManager.GetString($"dep_{Id}_Desc");
     }
 }
