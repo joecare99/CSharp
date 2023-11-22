8d22fe1dd21fd85522057114e3add41a277707d8
Joe Care <Joe_care_99@yahoo.com>
2023-11-22 22:18:56 +0100

    Add Interfaces

---
+++ CSharpBible/Patterns_Tutorial/Pattern_02_Observer/Models/CRandom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_02_Observer.Models
+{
+    public class CRandom : IRandom
+    {
+        private Random _rnd = new Random();
+        public int Next(int maxValue)
+        {
+            return _rnd.Next(maxValue);
+        }
+
+        public void Seed(int seed)
+        {
+            _rnd = new Random(seed);
+        }
+    }
+}
