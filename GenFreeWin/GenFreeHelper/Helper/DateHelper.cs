c8f1bed9f7265ca1f7c2cd19ea2475b8d73bb6de
Joe Care <Joe_care_99@yahoo.com>
2023-11-22 21:04:37 +0100

    GenFreeWin/GenFreeHelper

---
+++ GenFreeWin/GenFreeHelper/Helper/DateHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Helper
+{
+    public static class DateHelper
+    {
+#nullable enable
+        private static Func<DateTime> _getDate = ()=>DateTime.Today;
+        public static void SetDate(Func<DateTime> f)
+        {
+            _getDate = f;
+        }
+        public static string FormatDatum(this object? anlDatum, string sHeader,int iDefault = 1)
+        {
+            string kont_0 = anlDatum.AsString();
+            if (anlDatum is DateTime dt || DateTime.TryParse(kont_0,out dt))
+            {
+                kont_0 = $"{sHeader}{dt:d}";
+            }
+            else if (iDefault == 1)
+            {
+                kont_0 = $"{sHeader}{_getDate():d}";
+            }
+
+            return kont_0;
+        }
+
+        public static string Date2DotDateStr(this string Datu)
+        {
+            Datu = $"{Strings.Mid(Datu, 7, 2)} {Strings.Mid(Datu, 5, 2)} {Datu.Left( 4)}".Trim();
+            if (Datu.Length <= 0)
+            {
+                return Datu;
+            }
+            return Datu.Replace(" ", ".");
+        }
+ 
+        public static string[] IntoString(this DateTime[] adDates, string[]? asRes = null, int offs = 0)
+        {
+            asRes ??= new string[Math.Max(0, adDates.Length + offs)];
+            for(var i = 0; i<adDates.Length;i++)
+                if (i + offs >= 0 && i + offs < asRes.Length)
+                    asRes[i+offs] = adDates[i].ToString("yyyy.MM.dd");
+            return asRes;
+        }
+    }
+}
---
+++ GenFreeWin/GenFreeHelper/Helper/Externals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Helper
+{
+    public static class Externals
+    {
+
+        [DllImport("gdi32", CharSet = CharSet.Ansi, EntryPoint = "AddFontResourceA", ExactSpelling = true, SetLastError = true)]
+        public static extern int AddFontresource([MarshalAs(UnmanagedType.VBByRefStr)] ref string lpfilename);
+
+        [Obsolete("use new DriveInfo")]
+        [DllImport("kernel32", CharSet = CharSet.Ansi, EntryPoint = "GetDriveTypeA", ExactSpelling = true, SetLastError = true)]
+        public static extern int GetDriveType([MarshalAs(UnmanagedType.VBByRefStr)] ref string nDrive);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, EntryPoint = "GetModuleFileNameA", ExactSpelling = true, SetLastError = true)]
+        public static extern int GetModuleFileName(int hModule, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpfilename, int nSize);
+
+        [DllImport("user32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern short GetWindowWord(int hwnd, int nIndex);
+    }
+}
