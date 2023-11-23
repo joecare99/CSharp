d73bb11672d349e58b1262f294e4662575e01110
Joe Care <Joe_care_99@yahoo.com>
2023-11-23 08:01:05 +0100

    IPersistence

--- GenFreeWin/GenFreeBase/Interfaces/DB/IDatabase.cs
+++ GenFreeWin/GenFreeBase/Interfaces/DB/IDatabase.cs
@@ -11,7 +11,7 @@ namespace GenFree.Interfaces.DB
         void Close();
         void Execute(string v, object? value = null);
         void Execute(string sql);
-        DataTable GetSchema(string v, params string[] strings);
+        DataTable GetSchema(string v, params string?[] strings);
         IRecordset OpenRecordset(string v, object value, object missing, object missing1);
         IRecordset OpenRecordset(string v, RecordsetTypeEnum dbOpenTable = RecordsetTypeEnum.dbOpenQuery);
     }
---
+++ GenFreeWin/GenFreeBase/Interfaces/IPersistence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenFree.Interfaces
+{
+    public interface IPersistence
+    {
+        void AppendStringsTemp(IList<string> lines, string sSection);
+        void GetEnumsMand<T>(string sSection, T[] enums) where T : Enum;
+        int GetIntInit(string sSection, long lPos);
+        int GetIntMand(string sSection, long lPos = 1);
+        void PutEnumsMand<T>(string sSection, T[] enums) where T : Enum;
+        void PutIntMand(string sSection, ValueType letzte, long lPos);
+        void PutIntsMand(string sSection, int[] aiValues);
+        void ReadEnumsInit<T>(string sSection, out T eVal) where T : Enum;
+        Color[] ReadFarbenInit(string name, int iCnt);
+        int ReadIntInit(string sSection);
+        int ReadIntMand(string sFilename);
+        int[] ReadIntsInit(string sSection, int iCnt);
+        string ReadStringInit(string sSection);
+        string ReadStringMLProg(string sSection, int iMaxLine);
+        string ReadStringProg(string sSection);
+        void ReadStringsInit(string v, string[] aus);
+        int ReadStringsInit(string sSection, IList<string> asValue);
+        void ReadSuchDat<T>(string DateiName, T[] aeValues) where T : Enum;
+        void WriteEnumInit(string sSection, object eValue);
+        void WriteIntInit(string sSection, int iValue);
+        void WriteIntMand(string sFilename, int iValue);
+        void WriteIntsProg(string sSection, int[] aiValues);
+        void WriteStringInit(string verz, string sSection);
+        void WriteStringProg(string text, string sFilename);
+        void WriteStringsInit(string sSection, string[] asData);
+        void WriteStringTemp(string text, string sSection);
+    }
+}
