b04d4afa9e8bbfe83a32662b2aa98598174dd322
Joe Care <Joe_care_99@yahoo.com>
2023-11-30 13:57:21 +0100

    New interfaces

---
+++ GenFreeWin/GenFreeBase/Interfaces/Model/IHasID.cs
@@ -0,0 +1,11 @@
+﻿//using DAO;
+namespace GenFree.Interfaces.Model
+{
+    public interface IHasID<T>
+
+    {
+        T MaxID { get; }
+        void Delete(T key);
+        bool Exists(T key);
+    }
+}
---
+++ GenFreeWin/GenFreeBase/Interfaces/Model/IUsesRecordset.cs
@@ -0,0 +1,15 @@
+﻿//using DAO;
+using GenFree.Interfaces.DB;
+using System;
+
+namespace GenFree.Interfaces.Model
+{
+    public interface IUsesRecordset<T> : IHasID<T>
+    {
+        int Count { get; }
+
+        void ForEachDo(Action<IRecordset> action);
+        IRecordset Seek(T key, out bool xBreak);
+        IRecordset Seek(T Key);
+    }
+}
