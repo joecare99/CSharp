98bfae192fd4283a44f4e8dd26ef5f6aeda77114
Joe Care <Joe_care_99@yahoo.com>
2023-11-30 13:59:03 +0100

    GenFree constants

---
+++ GenFreeWin/GenFreeData/GenFree/Constants/CMonthShort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenFree.Constants
+{
+    public static class CMonthShort
+    {
+        public const string cMon_JAN = "JAN";
+        public const string cMon_FEB = "FEB";
+        public const string cMon_MAR = "MAR";
+        public const string cMon_APR = "APR";
+        public const string cMon_MAY = "MAY";
+        public const string cMon_JUN = "JUN";
+        public const string cMon_JUL = "JUL";
+        public const string cMon_AUG = "AUG";
+        public const string cMon_SEP = "SEP";
+        public const string cMon_OCT = "OCT";
+        public const string cMon_NOV = "NOV";
+        public const string cMon_DEC = "DEC";
+
+    }
+}
