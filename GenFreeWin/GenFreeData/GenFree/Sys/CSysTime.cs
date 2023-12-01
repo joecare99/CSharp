68e0dca09bcb04029906849625529d41fa807873
Joe Care <Joe_care_99@yahoo.com>
2023-12-02 00:32:49 +0100

    Systime

---
+++ GenFreeWin/GenFreeData/GenFree/Sys/CSysTime.cs
@@ -0,0 +1,15 @@
+﻿using GenFree.Interfaces.Sys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenFree.GenFree.Sys
+{
+    internal class CSysTime :ISysTime
+    {
+        public DateTime Now => DateTime.Now;
+        public DateTime Default => default;
+    }
+}
