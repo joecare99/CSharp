456f24bda3b4ba741bb5ebb475a40d0bf35ef6e2
Joe Care <Joe_care_99@yahoo.com>
2023-12-04 03:54:11 +0100

    ParentedItemsList & tests

---
+++ GenFreeWin/VBUnObfusicatorTests/Models/ParentedItemsListTests.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VBUnObfusicator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace VBUnObfusicator.Models.Tests
+{
+    public interface ITestItf : IHasParents<ITestItf>, IEquatable<ITestItf> 
+    {
+        int iData { get; set; }
+        ITestItf this[int idx] { get; }
+
+        void Add(ITestItf item);
+    }
+
+    public class TestClass : ITestItf
+    {
+        public TestClass(int v)
+        {
+            iData = v;
+            _list = new CSCode.ParentedItemsList<ITestItf>(this);
+        }
+
+        public ITestItf this[int idx] => _list[idx];
+
+        public ITestItf Parent { get; set; }
+        public int iData { get ; set ; }
+
+        private CSCode.ParentedItemsList<ITestItf> _list;
+
+        public bool Equals(ITestItf? other)
+        {
+            if (other is null)
+                return false;
+            return iData == other.iData;
+        }
+
+        public void Add(ITestItf item)
+        {
+            _list.Add(item);
+        }
+    }
+
+    [TestClass()]
+    public class ParentedItemsListTests : ITestItf
+    {
+        public ITestItf Parent { get ;  set ; }
+        public int iData { get; set; }
+
+        public ITestItf this[int idx] => testClass[idx];
+
+        CSCode.ParentedItemsList<ITestItf> testClass;
+
+
+        [TestInitialize()]
+        public void Init()
+        {
+            testClass = new(this);
+        }
+
+        [TestMethod()]
+        public void IsReadonlyTest()
+        {
+            Assert.AreEqual(false,testClass.IsReadOnly);
+        }
+
+        [TestMethod()]
+        public void ParentedItemsListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexOfTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.AreEqual(0, testClass.Count);
+            testClass.Insert(0,new TestClass(3));
+            testClass.Insert(1,new TestClass(5));
+            testClass[1].Add(new TestClass(11));
+            testClass[1].Add(new TestClass(7));
+            testClass.Insert(2,testClass[1][1]);
+            Assert.AreEqual(3, testClass.Count);
+        }
+
+        [TestMethod()]
+        public void RemoveAtTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.AreEqual(0, testClass.Count);
+            testClass.Add(new TestClass(3));
+            testClass.Add(new TestClass(5));
+            testClass[1].Add(new TestClass(11));
+            testClass[1].Add(new TestClass(7));
+            testClass.Add(testClass[1][1]);
+            Assert.AreEqual(3, testClass.Count);
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            AddTest();
+            testClass.Clear();
+            Assert.AreEqual(0, testClass.Count);
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            AddTest();
+            Assert.AreEqual(true, testClass.Contains( new TestClass(3)));
+            Assert.AreEqual(false, testClass.Contains(new TestClass(2)));
+            Assert.AreEqual(false, testClass.Contains(this));
+        }
+
+        [TestMethod()]
+        public void CopyToTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            Assert.IsNotNull(((IEnumerable)testClass).GetEnumerator());
+        }
+
+        public bool Equals(ITestItf? other)
+        {
+            if (other is null)
+                return false;
+            return iData == other.iData;
+        }
+
+        public void Add(ITestItf item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
