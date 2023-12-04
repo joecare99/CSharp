0236ae4fcfa7aeab1409a73d6fb2308e2551393f
Joe Care <Joe_care_99@yahoo.com>
2023-12-04 03:54:50 +0100

    AppTests & TokenDataTests

---
+++ GenFreeWin/VBUnObfusicatorTests/Models/TokenDataTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static VBUnObfusicator.Models.ICSCode;
+
+namespace VBUnObfusicator.Models.Tests
+{
+    [TestClass]
+    public class TokenDataTests
+    {
+        [TestMethod]
+        public void TokenDataTest()
+        {
+            var t = (TokenData)("If",CodeBlockType.Instruction,2);
+            Assert.AreEqual("If", t.Code);
+            Assert.AreEqual(CodeBlockType.Instruction, t.type);
+            Assert.AreEqual(2, t.Level);
+        }
+
+    }
+}
