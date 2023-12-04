f11ab221727c07c677f573060addb559a92482c3
Joe Care <Joe_care_99@yahoo.com>
2023-12-04 17:58:46 +0100

    Names Enums & Interfaces

--- GenFreeWin/GenFreeBase/Data/EPlaceProp.cs
+++ GenFreeWin/GenFreeBase/Data/EPlaceProp.cs
@@ -4,6 +4,21 @@ namespace GenFree.Data
     public enum EPlaceProp
     {
         ID,
-
+        iOrt,
+        iOrtsteil,
+        iKreis,
+        iLand,
+        iStaat,
+        sStaatk,
+        sPLZ,
+        sTerr,
+        sLoc,
+        sL,
+        sB,
+        sBem,
+        sZusatz,
+        sGOV,
+        sPolName,
+        ig,
     }
 }
