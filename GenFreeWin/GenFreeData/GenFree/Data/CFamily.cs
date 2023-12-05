5f0e833789760706a966220c0c67eb92218a57dc
Joe Care <Joe_care_99@yahoo.com>
2023-12-05 19:08:14 +0100

    CFamily & Tests

---
+++ GenFreeWin/GenFreeData/GenFree/Data/CFamily.cs
@@ -0,0 +1,99 @@
+﻿//using DAO;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using GenFree.Helper;
+using GenFree.Interfaces;
+using GenFree.Interfaces.DB;
+using GenFree.Interfaces.Model;
+using GenFree.Interfaces.Sys;
+
+namespace GenFree.Data;
+
+public class CFamily : CUsesRecordSet<int>, IFamily
+{
+    private ISysTime _sysTime;
+    private Func<IRecordset> _value;
+
+   protected override IRecordset _db_Table => _value();
+
+    protected override string _keyIndex => nameof(FamilyIndex.Fam);
+
+    public CFamily(Func<IRecordset> value, ISysTime sysTime)
+    {
+        this._sysTime = sysTime;
+        _value = value;
+    }
+
+    public void SetNameNr(int iFamInArb, int iName)
+    {
+        var dB_FamilyTable = Seek(iFamInArb);
+        if (dB_FamilyTable.NoMatch)
+        {
+            dB_FamilyTable.AddNew();
+            dB_FamilyTable.Fields[nameof(FamilyFields.AnlDatum)].Value = _sysTime.Now;
+            dB_FamilyTable.Fields[nameof(FamilyFields.EditDat)].Value = 0;
+            dB_FamilyTable.Fields[nameof(FamilyFields.FamNr)].Value = iFamInArb;
+            dB_FamilyTable.Fields[nameof(FamilyFields.Bem1)].Value = " ";
+            dB_FamilyTable.Fields[nameof(FamilyFields.Bem2)].Value = " ";
+            dB_FamilyTable.Fields[nameof(FamilyFields.Bem3)].Value = " ";
+            dB_FamilyTable.Fields[nameof(FamilyFields.Name)].Value = iName;
+            dB_FamilyTable.Fields[nameof(FamilyFields.Prüfen)].Value = "1";
+        }
+        else
+        {
+            dB_FamilyTable.Edit();
+            dB_FamilyTable.Fields[nameof(FamilyFields.Name)].Value = iName;
+        }
+        dB_FamilyTable.Update();
+    }
+
+    public void SetValue(int famInArb, int satz, string[] strings, object[] objects)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IRecordset Seek(int key, out bool xBreak)
+    {
+        _db_Table.Index = _keyIndex;
+        _db_Table.Seek("=", key);
+        xBreak = _db_Table.NoMatch;
+        return xBreak? null: _db_Table;
+    }
+
+    public bool ReadData(int key, out IFamilyData? data)
+    {
+        var dB_FamilyTable = Seek(key, out bool xBreak);
+        data = xBreak ? null : new CFamilyPersons(dB_FamilyTable);
+        return !xBreak;
+    }
+
+    public IEnumerable<IFamilyData> ReadAll()
+    {
+        IRecordset dB_PlaceTable = _db_Table;
+        dB_PlaceTable.Index = _keyIndex;
+        dB_PlaceTable.MoveFirst();
+        while (!dB_PlaceTable.EOF)
+        {
+            yield return new CFamilyPersons(dB_PlaceTable);
+            dB_PlaceTable.MoveNext();
+        }
+    }
+
+    public void SetData(int key, IFamilyData data, string[]? asProps = null)
+    {
+        var dB_FamilyTable = Seek(key);
+        if (dB_FamilyTable != null)
+        {
+            dB_FamilyTable.Edit();
+            data.SetDBValue(dB_FamilyTable, asProps);
+            dB_FamilyTable.Update();
+        }
+    }
+
+    protected override int GetID(IRecordset recordset)
+    {
+        return recordset.Fields[nameof(FamilyFields.FamNr)].AsInt();
+    }
+}
+
---
+++ GenFreeWin/GenFreeDataTests/GenFree/Data/CFamilyTests.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenFree.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenFree.Interfaces.DB;
+using NSubstitute;
+using GenFree.Interfaces.Sys;
+using GenFree.Interfaces.Model;
+using GenFree.Interfaces;
+
+namespace GenFree.Data.Tests
+{
+    [TestClass()]
+    public class CFamilyTests
+    {
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+        private CFamily testClass;
+        private IRecordset testRS;
+#pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+
+        [TestInitialize]
+        public void Init()
+        {
+            testRS = Substitute.For<IRecordset>();
+            var testST = Substitute.For<ISysTime>();
+            testST.Now.Returns(new DateTime(2022, 12, 31));
+            testClass = new CFamily(() => testRS, testST);
+            testRS.NoMatch.Returns(true);
+            testRS.Fields[nameof(FamilyFields.FamNr)].Value.Returns(2, 6, 4, 9);
+            testRS.Fields[nameof(FamilyFields.Eltern)].Value.Returns('N', 'V', 'V', 'A', 'B');
+            testRS.Fields[nameof(FamilyFields.Aeb)].Value.Returns(1, 3, 5);
+            testRS.ClearReceivedCalls();
+        }
+
+        [TestMethod()]
+        public void CFamilyTest()
+        {
+            Assert.IsNotNull(testClass);
+            Assert.IsInstanceOfType(testClass, typeof(IFamily));
+            Assert.IsInstanceOfType(testClass, typeof(CFamily));
+        }
+
+        [TestMethod()]
+        public void MaxIDTest()
+        {
+            Assert.AreEqual(2, testClass.MaxID);
+            Assert.AreEqual(nameof(FamilyIndex.Fam), testRS.Index);
+            testRS.Received(1).MoveLast();
+            testRS.Received(1).Fields[0].Value = 0;
+        }
+
+        [TestMethod()]
+        public void SetNameNrTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetValueTest()
+        {
+            Assert.Fail();
+        }
+
+        [DataTestMethod()]
+        [DataRow("Null", 0, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-None-0", 1, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-Name-2", 1, ETextKennz.tkName, 2, true)]
+
+        public void SeekTest(string sName, int iActFamNr, Enum eTKennz, int iLfNr, bool xExp)
+        {
+            testRS.NoMatch.Returns(iActFamNr is not (> 0 and < 3) || iLfNr / 2 != iActFamNr);
+            Assert.AreEqual(xExp ? testRS : null, testClass.Seek(iActFamNr, out var xBreak));
+            Assert.AreEqual(!xExp, xBreak);
+            Assert.AreEqual(nameof(FamilyIndex.Fam), testRS.Index);
+            testRS.Received().Seek("=", iActFamNr);
+        }
+
+        [DataTestMethod()]
+        [DataRow("Null", 0, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-None-0", 1, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-Name-2", 1, ETextKennz.tkName, 2, true)]
+
+        public void ReadDataTest(string sName, int iActFamNr, Enum eTKennz, int iLfNr, bool xExp)
+        {
+            testRS.NoMatch.Returns(iActFamNr is not (> 0 and < 3) || iLfNr / 2 != iActFamNr);
+            Assert.AreEqual(xExp, testClass.ReadData(iActFamNr, out var cPd));
+            Assert.AreEqual(nameof(FamilyIndex.Fam), testRS.Index);
+            testRS.Received().Seek("=", iActFamNr);
+        }
+
+        [DataTestMethod()]
+        [DataRow("Null", 0, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-None-0", 1, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-Name-2", 1, ETextKennz.tkName, 2, true)]
+
+        public void ReadAllTest(string sName, int iActFamNr, Enum eTKennz, int iLfNr, bool xExp)
+        {
+            testRS.NoMatch.Returns(iActFamNr is not (> 0 and < 3) || iLfNr / 2 != iActFamNr, false, false, true);
+            testRS.EOF.Returns(iActFamNr is not (> 0 and < 3) || iLfNr / 2 != iActFamNr, false, false, true);
+            var iCnt = 0;
+            foreach (var cNm in testClass.ReadAll())
+            {
+                Assert.IsNotNull(cNm);
+                Assert.IsInstanceOfType(cNm, typeof(IFamilyData));
+                Assert.IsInstanceOfType(cNm, typeof(CFamilyPersons));
+                iCnt++;
+            }
+            Assert.AreEqual(xExp ? 3 : 0, iCnt);
+            Assert.AreEqual(nameof(FamilyIndex.Fam), testRS.Index);
+            testRS.Received(xExp ? 3 : 0).MoveNext();
+            testRS.Received(1).MoveFirst();
+
+        }
+
+        [DataTestMethod()]
+        [DataRow("Null", 0, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-None-0", 1, ETextKennz.tkNone, 0, false)]
+        [DataRow("1-Name-2", 1, ETextKennz.tkName, 2, true)]
+
+        public void SetDataTest(string sName, int iActFamNr, Enum eTKennz, int iLfNr, bool xExp)
+        {
+            testRS.NoMatch.Returns(iActFamNr is not (> 0 and < 3) || iLfNr / 2 != iActFamNr);
+            var testPD = Substitute.For<IFamilyData>();
+            testClass.SetData(iActFamNr, testPD);
+            Assert.AreEqual(nameof(FamilyIndex.Fam), testRS.Index);
+            testRS.Received().Seek("=", iActFamNr);
+        }
+    }
+}
