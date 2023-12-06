67c24f1a13aa75035e67e0735ef8e55610741576
Joe Care <Joe_care_99@yahoo.com>
2023-12-06 01:53:56 +0100

    Interface

--- GenFreeWin/GenFreeBase/Interfaces/IPlaceData.cs
+++ GenFreeWin/GenFreeBase/Interfaces/IPlaceData.cs
@@ -1,9 +1,11 @@
 ﻿//using DAO;
 using GenFree.Data;
+using GenFree.Interfaces.DB;
 
 namespace GenFree.Interfaces
 {
-    public interface IPlaceData : IHasPropEnum<EPlaceProp>, IHasID<int> 
+    public interface IPlaceData : IHasPropEnum<EPlaceProp>, IHasID<int>
     {
+        void SetDBValue(IRecordset dB_FamilyTable, string[]? asProps);
     }
 }
