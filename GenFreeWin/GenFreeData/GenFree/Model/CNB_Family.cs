fabbda7bda599dade0841668067b7b772b94a0ab
Joe Care <Joe_care_99@yahoo.com>
2023-12-31 12:12:34 +0100

    GenFreeData (new files)

---
+++ GenFreeWin/GenFreeData/GenFree/Model/CNB_Family.cs
@@ -0,0 +1,46 @@
+﻿using GenFree.Helper;
+using GenFree.Interfaces.DB;
+using GenFree.Interfaces.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenFree.Model
+{
+    public class CNB_Family : CUsesRecordSet<int>, INB_Family
+    {
+        private Func<IRecordset> _value;
+
+        public CNB_Family(Func<IRecordset> value)
+        {
+            _value = value;
+        }
+        protected override string _keyIndex => "Fam";
+
+        protected override IRecordset _db_Table => _value();
+
+        public override IRecordset? Seek(int key, out bool xBreak)
+        {
+            var NB_FamilyTable = _db_Table;
+            NB_FamilyTable.Index = _keyIndex;
+            NB_FamilyTable.Seek("=", key);
+            xBreak = NB_FamilyTable.NoMatch;
+            return xBreak ? null : NB_FamilyTable;
+        }
+
+        protected override int GetID(IRecordset recordset)
+        {
+            return recordset.Fields["Fam"].AsInt();
+        }
+
+        public void Append(int famInArb, bool xAppenWitt = true)
+        {
+            var NB_FamilyTable = _db_Table;
+            NB_FamilyTable.AddNew();
+            NB_FamilyTable.Fields["Fam"].Value = famInArb;
+            NB_FamilyTable.Update();
+        }
+    }
+}
---
+++ GenFreeWin/GenFreeData/GenFree/Model/CUsesRecordSet.cs
@@ -0,0 +1,45 @@
+﻿//using DAO;
+using GenFree.Helper;
+using GenFree.Interfaces.DB;
+
+//using DAO;
+using GenFree.Interfaces.Model;
+using System;
+
+namespace GenFree.Model
+{
+    public abstract class CUsesRecordSet<T> : IUsesRecordset<T>
+    {
+        public int Count => _db_Table.RecordCount;
+        public T MaxID => GetMaxID();
+
+        protected abstract string _keyIndex { get; }
+        protected abstract IRecordset _db_Table { get; }
+        protected abstract T GetID(IRecordset recordset);
+        public abstract IRecordset? Seek(T key, out bool xBreak);
+
+        public bool Delete(T key) => Seek(key, out var x).SetRet(rs => rs?.Delete(), !x);
+        public bool Exists(T key) => Seek(key) != null;
+
+        public void ForEachDo(Action<IRecordset> action)
+        {
+            IRecordset dB_PlaceTable = _db_Table;
+            dB_PlaceTable.Index = _keyIndex;
+            dB_PlaceTable.MoveFirst();
+            while (!dB_PlaceTable.EOF)
+            {
+                try { action?.Invoke(dB_PlaceTable); } catch { };
+                dB_PlaceTable.MoveNext();
+            }
+        }
+
+        public IRecordset? Seek(T Key) => Seek(Key, out _);
+        private T GetMaxID()
+        {
+            IRecordset dB_PlaceTable = _db_Table;
+            dB_PlaceTable.Index = _keyIndex;
+            dB_PlaceTable.MoveLast();
+            return GetID(dB_PlaceTable);
+        }
+    }
+}
