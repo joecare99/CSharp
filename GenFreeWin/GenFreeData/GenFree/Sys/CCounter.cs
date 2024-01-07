4a4c6cf3075994be58ace942766906cb72be6f76
Joe Care <Joe_care_99@yahoo.com>
2024-01-07 18:08:55 +0100

    GenFreeData Constants & Sys

--- GenFreeWin/GenFreeData/GenFree/Sys/CCounter.cs
+++ GenFreeWin/GenFreeData/GenFree/Sys/CCounter.cs
@@ -9,27 +9,28 @@ using System.Threading.Tasks;
 
 namespace GenFree.GenFree.Sys
 {
-    public class CCounter<T> : ICounter<T> where T:struct
+    public class CCounter<T> : ICounter<T> where T : struct
     {
-        static private Func<T, T> _pred;
-        static private Func<T, T> _succ;
+        static private Func<T, T>? _pred;
+        static private Func<T, T>? _succ;
 
-        public CCounter(Func<T,T>? Pred,Func<T,T>? Succ)
+        public CCounter(Func<T, T> Pred, Func<T, T> Succ)
         {
-            _pred ??= Pred; 
+            _pred ??= Pred;
             _succ ??= Succ;
+            Reset();
         }
         public T Value { get; private set; }
 
         public ICounter<T> Dec()
         {
-            Value = _pred(Value);
+            Value = _pred?.Invoke(Value) ?? Value;
             return this;
         }
 
         public ICounter<T> Inc()
         {
-            Value = _succ(Value);
+            Value = _succ?.Invoke(Value) ?? Value;
             return this;
         }
 
