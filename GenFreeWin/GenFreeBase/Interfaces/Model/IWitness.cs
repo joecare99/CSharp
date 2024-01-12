e846591d1da6d82f25ef5a8be9d0db3a0c369213
Joe Care <Joe_care_99@yahoo.com>
2024-01-12 23:01:08 +0100

    GenFreeBase Interfaces Model

--- GenFreeWin/GenFreeBase/Interfaces/Model/IWitness.cs
+++ GenFreeWin/GenFreeBase/Interfaces/Model/IWitness.cs
@@ -12,6 +12,10 @@ namespace GenFree.Data
         IUsesRecordset<(int iLink, int iPers, int iWKennz, EEventArt eArt, short iLfNr)>, 
         IUsesID<(int iLink, int iPers, int iWKennz, EEventArt eArt, short iLfNr)>
     {
+        void DeleteAllE(int persInArb, int eWKennz);
+        void DeleteAllF(int persInArb, int sWKennz);
+        void DeleteAllZ(int persInArb, int sWKennz, EEventArt eArt, short iLfNr);
+        bool ExistZeug(int persInArb, EEventArt eEvtArt, short lfNR, int eWKennz = 10);
         IEnumerable<IWitnessData> ReadAllFams(int iNr, int v);
     }
 }
