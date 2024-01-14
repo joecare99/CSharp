e12c4805f72fd3e59fb124b1077b994cfe00648f
Joe Care <Joe_care_99@yahoo.com>
2024-01-14 13:53:28 +0100

    GenFreeBase/Interfaces/Model

--- GenFreeWin/GenFreeBase/Interfaces/Model/INames.cs
+++ GenFreeWin/GenFreeBase/Interfaces/Model/INames.cs
@@ -1,10 +1,14 @@
 ﻿//using DAO;
 using GenFree.Data;
+using GenFree.Model;
 using System;
 
 namespace GenFree.Interfaces.Model
 {
-    public interface INames : IHasDataItf<INamesData, (int, ETextKennz, int)>, IUsesRecordset<(int, ETextKennz, int)>, IUsesID<(int, ETextKennz, int)>
+    public interface INames :
+        IHasDataItf<INamesData, (int, ETextKennz, int)>,
+        IUsesRecordset<(int, ETextKennz, int)>, IUsesID<(int, ETextKennz, int)>,
+        IHasRSIndex1<NameIndex, NameFields>
     {
         void DeleteAllP(int persInArb);
         bool DeleteNK(int persInArb, ETextKennz kennz);
