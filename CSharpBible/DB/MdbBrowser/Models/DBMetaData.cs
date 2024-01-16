4e24bb7635afac6aed6104b128fae1904618194e
Joe Care <Joe_care_99@yahoo.com>
2024-01-16 10:36:04 +0100

    DB/MdbBrowser/Models

--- CSharpBible/DB/MdbBrowser/Models/DBMetaData.cs
+++ CSharpBible/DB/MdbBrowser/Models/DBMetaData.cs
@@ -5,10 +5,10 @@ namespace MdbBrowser.Models
     //    public record DBMetaData(string Name, EKind Kind, IEnumerable<string> Data);
     public struct DBMetaData
     {
-        public string Name;
-        public EKind Kind;
+        public string Name { get; set; }
+        public EKind Kind { get; set; }
         public object This;
-        public IEnumerable<string> Data;
+        public IEnumerable<string> Data { get; set; }
         public DBMetaData(string Name, EKind Kind, object This, IEnumerable<string> Data) 
         {
             this.Name = Name;
