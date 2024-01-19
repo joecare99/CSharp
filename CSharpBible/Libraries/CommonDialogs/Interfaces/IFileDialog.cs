067e91e92674049a0786a096e9acaea1f163d0c8
Joe Care <Joe_care_99@yahoo.com>
2024-01-19 23:06:05 +0100

    Libraries CommonDialogs

---
+++ CSharpBible/Libraries/CommonDialogs/Interfaces/IFileDialog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonDialogs.Interfaces
+{
+    public interface IFileDialog
+    {
+        string FileName { get; set; }
+        bool? ShowDialog();
+        bool? ShowDialog(object owner);
+    }
+}
