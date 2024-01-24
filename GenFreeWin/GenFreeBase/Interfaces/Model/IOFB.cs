2cc18e3e1ed63c407218987697a224734efa1126
Joe Care <Joe_care_99@yahoo.com>
2024-01-24 20:32:30 +0100

    GenFreeBase

---
+++ GenFreeWin/GenFreeBase/Interfaces/Model/IOFB.cs
@@ -0,0 +1,12 @@
+﻿using GenFree.Data;
+
+namespace GenFree.Interfaces.Model
+{
+    public interface IOFB :
+        IHasIxDataItf<OFBIndex, IOFBData, (int, string, int)>,
+        IUsesRecordset<(int, string, int)>,
+        IUsesID<(int, string, int)>,
+        IHasRSIndex1<OFBIndex, OFBFields>
+    {
+    }
+}
