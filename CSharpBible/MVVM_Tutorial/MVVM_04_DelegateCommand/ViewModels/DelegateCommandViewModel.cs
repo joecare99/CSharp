88b75de4a6340c46f930ec3dca0dee92fdc5d3c6
Joe Care <Joe_care_99@yahoo.com>
2024-01-26 21:08:33 +0100

    MVVM 04 Consolidation

--- CSharpBible/MVVM_Tutorial/MVVM_04_DelegateCommand/ViewModels/DelegateCommandViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_04_DelegateCommand/ViewModels/DelegateCommandViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using MVVM.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVM_04_DelegateCommand.ViewModels
 {
--- CSharpBible/MVVM_Tutorial/MVVM_04_DelegateCommandTests/Views/DelegateCommandViewTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_04_DelegateCommandTests/Views/DelegateCommandViewTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MVVM_04_DelegateCommand.Views.Tests
 {
