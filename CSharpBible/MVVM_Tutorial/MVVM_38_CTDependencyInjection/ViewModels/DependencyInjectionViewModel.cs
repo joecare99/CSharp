8e31d6124d97de10da1ea1f3a5ad7b672a70352b
Joe Care <Joe_care_99@yahoo.com>
2024-02-01 00:06:33 +0100

    Other

---
+++ CSharpBible/MVVM_Tutorial/MVVM_38_CTDependencyInjection/ViewModels/DependencyInjectionViewModel.cs
@@ -0,0 +1,56 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_38_CTDependencyInjection
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="MainWindowViewModel.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using MVVM.ViewModel;
+using MVVM.View.Extension;
+using MVVM_38_CTDependencyInjection.Models.Interfaces;
+using System;
+using System.ComponentModel;
+
+namespace MVVM_38_CTDependencyInjection.ViewModels
+{
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public partial class DependencyInjectionViewModel : BaseViewModelCT
+    {
+        #region Properties
+        private readonly ITemplateModel _model;
+
+        public DateTime Now => _model.Now;
+        #endregion
+  
+        #region Methods
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public DependencyInjectionViewModel():this(IoC.GetRequiredService<ITemplateModel>())
+        {
+        }
+
+        public DependencyInjectionViewModel(ITemplateModel model)
+        {
+            _model = model;
+            _model.PropertyChanged += OnMPropertyChanged;
+        }
+
+        private void OnMPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(e.PropertyName); 
+        }
+
+        #endregion
+    }
+}
