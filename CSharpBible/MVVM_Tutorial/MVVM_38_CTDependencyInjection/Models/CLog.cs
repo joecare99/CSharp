a3790b61508551be0329db967f2930f9d1456fa2
Joe Care <Joe_care_99@yahoo.com>
2024-02-01 00:05:50 +0100

    Models

---
+++ CSharpBible/MVVM_Tutorial/MVVM_38_CTDependencyInjection/Models/CLog.cs
@@ -0,0 +1,15 @@
+﻿using MVVM_38_CTDependencyInjection.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_38_CTDependencyInjection.Models
+{
+    public class CLog : ILog
+    {
+        public void Log(string message) => Debug.WriteLine(message);
+    }
+}
---
+++ CSharpBible/MVVM_Tutorial/MVVM_38_CTDependencyInjection/Models/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MVVM_38_CTDependencyInjection.Models.Interfaces;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_38_CTDependencyInjection.Models
+{
+    public class UserRepository : IUserRepository
+    {
+        private ILog _log;
+
+        public Guid GetUser(string name, string password) =>
+            // Dummy-Implement
+            name.ToUpper() switch
+            {
+                "DAVE" => new Guid("012345678ABCDEF012345678ABCDEF"),
+                "JOE" => new Guid("112345678ABCDEF012345678ABCDEF"),
+                "PETER" => new Guid("212345678ABCDEF012345678ABCDEF"),
+                _ => Guid.Empty
+            };
+
+        public EPermission GetPermission(Guid id,Guid action) {
+            return EPermission.All;
+        }
+
+        public UserRepository(ILog log) {
+            _log = log;
+        }
+    }
+}
