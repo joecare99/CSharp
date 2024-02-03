df458b3b77c1a9cf65f5cb81af53c3a5ec8d8e5f
Joe Care <Joe_care_99@yahoo.com>
2024-02-03 18:09:48 +0100

    MVVM #06.4 DoubleValueConverter & Tests

--- CSharpBible/MVVM_Tutorial/MVVM_06_Converters_4Tests/ValueConverter/DoubleValueConverterTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_06_Converters_4Tests/ValueConverter/DoubleValueConverterTests.cs
@@ -87,7 +87,8 @@ namespace MVVM_06_Converters_4.ValueConverter.Tests
         [DataRow(0.99, "99")]
         [DataRow(double.NaN, "Hallo")]
         [DataRow(double.NaN, "")]
-        public void ConvertBackTest(object? value, string expected)
+        [DataRow(double.NaN, null)]
+        public void ConvertBackTest(object? value, object expected)
         {
             var result = testConv.ConvertBack(expected, typeof(object), null, CultureInfo.InvariantCulture);
             Assert.AreEqual(value, result);
@@ -113,6 +114,7 @@ namespace MVVM_06_Converters_4.ValueConverter.Tests
         [DataTestMethod]
         [DataRow(double.NaN, "Hallo")]
         [DataRow(double.NaN, "")]
+        [DataRow(double.NaN, null)]
         public void ConvertBackTest3(object? value, string expected)
         {
             var result = testConv.ConvertBack(expected, typeof(object), "{0}", CultureInfo.InvariantCulture);
