9f401caba655c1722240ee8924c5a7048cb62bfd
Joe Care <Joe_care_99@yahoo.com>
2024-02-05 20:22:16 +0100

    DemoLibrary

---
+++ CSharpBible/MVVM_Tutorial/MVVM_06_Converters_4Tests/Model/AGV_ModelTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVVM.ViewModel;
+using MVVM_06_Converters_4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_06_Converters_4.Model.Tests
+{
+    [TestClass()]
+    public class AGV_ModelTests:BaseTestViewModel<AGV_Model>
+    {
+        [TestMethod()]
+        public void AGV_ModelTest()
+        {
+            Assert.IsNotNull(testModel);
+            Assert.IsNotNull(testModel2);
+            Assert.IsInstanceOfType(testModel, typeof(AGV_Model));
+            Assert.IsInstanceOfType(testModel, typeof(NotificationObject));
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
