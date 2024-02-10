1b16b6d419b829adb351d526a105a0c682217643
Joe Care <Joe_care_99@yahoo.com>
2024-02-10 13:53:24 +0100

    UserControls

---
+++ CSharpBible/MVVM_Tutorial/MVVM_24_UserControl/ViewModels/UserControlViewModel.cs
@@ -0,0 +1,56 @@
+﻿// ***********************************************************************
+// Assembly         : WpfApp
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="UserControlViewModel.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using MVVM.ViewModel;
+
+namespace MVVM_24_UserControl.ViewModels
+{
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public class UserControlViewModel : BaseViewModel
+    {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UserControlViewModel"/> class.
+		/// </summary>
+		public UserControlViewModel()
+        {
+
+        }
+
+        private string _text1 = "Hello World";
+        
+        public string Text1
+        {
+			get { return _text1; }
+			set
+            {
+                SetProperty(ref _text1, value);
+			}
+		}
+
+
+        public string Text2 { get; set; } = "Hello World 2";
+
+		/// <summary>
+		/// Finalizes an instance of the <see cref="UserControlViewModel"/> class.
+		/// </summary>
+		~UserControlViewModel()
+        {
+            return;
+        }
+
+    }
+}
