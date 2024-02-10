9f457f1fc5ce4fca4392a768dc639d090a3797b6
Joe Care <Joe_care_99@yahoo.com>
2024-02-10 18:21:43 +0100

    MVVM_24c_CTUserControl

--- CSharpBible/MVVM_Tutorial/MVVM_24c_CTUserControl/ViewModels/MainWindowViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_24c_CTUserControl/ViewModels/MainWindowViewModel.cs
@@ -20,7 +20,7 @@ namespace MVVM_24c_CTUserControl.ViewModels
     /// Implements the <see cref="BaseViewModel" />
     /// </summary>
     /// <seealso cref="BaseViewModel" />
-    public class MainWindowViewModel : BaseViewModel
+    public class MainWindowViewModel : BaseViewModelCT
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
--- CSharpBible/MVVM_Tutorial/MVVM_24c_CTUserControl/ViewModels/UserControlViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_24c_CTUserControl/ViewModels/UserControlViewModel.cs
@@ -11,6 +11,7 @@
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
+using CommunityToolkit.Mvvm.ComponentModel;
 using MVVM.ViewModel;
 
 namespace MVVM_24c_CTUserControl.ViewModels
@@ -20,26 +21,24 @@ namespace MVVM_24c_CTUserControl.ViewModels
 	/// Implements the <see cref="BaseViewModel" />
 	/// </summary>
 	/// <seealso cref="BaseViewModel" />
-	public class UserControlViewModel : BaseViewModel
+	public partial class UserControlViewModel : BaseViewModelCT
 	{
+		[ObservableProperty]
 		private string _text1 = "Hello World";
-		public string Text1 { get => _text1; set { SetProperty(ref _text1, value); } }
 
+		[ObservableProperty]
 		private string _text2 = "Hello World2";
-		public string Text2 { get => _text2; set { SetProperty(ref _text2, value); } }
 
+		[ObservableProperty]
 		private bool? _state1 = true;
-		public bool? State1 { get => _state1; set { SetProperty(ref _state1 , value); } }
 
-		private bool? _state2 = true;
-		public bool? State2 { get => _state2; set { SetProperty(ref _state2 , value); } }
+		[ObservableProperty]
+		private bool? _state2 = false;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="UserControlViewModel"/> class.
 		/// </summary>
-		public UserControlViewModel()
-		{
-		}
+		public UserControlViewModel() { }
 
 		/// <summary>
 		/// Finalizes an instance of the <see cref="UserControlViewModel"/> class.
