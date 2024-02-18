2bbafa9fef9095c2f349332f463dea24fb83d44f
Joe Care <Joe_care_99@yahoo.com>
2024-02-18 09:21:14 +0100

    ConsoleDI.Example

---
+++ TestStatements/ConsoleDI.Example/ServiceLifetimeReporter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleDI.Example;
+
+internal sealed class ServiceLifetimeReporter(
+    IExampleTransientService transientService,
+    IExampleScopedService scopedService,
+    IExampleSingletonService singletonService)
+{
+    public void ReportServiceLifetimeDetails(string lifetimeDetails)
+    {
+        Console.WriteLine(lifetimeDetails);
+
+        LogService(transientService, "Always different");
+        LogService(scopedService, "Changes only with lifetime");
+        LogService(singletonService, "Always the same");
+    }
+
+    private static void LogService<T>(T service, string message)
+        where T : IReportServiceLifetime =>
+        Console.WriteLine(
+            $"    {typeof(T).Name}: {service.Id} ({message})\r\n" +
+            $"    {typeof(T).Name}: is {service.Lifetime}");
+}
