0cab8b0babf076358030d967634338a56b8d80e2
Joe Care <Joe_care_99@yahoo.com>
2024-02-19 06:59:41 +0100

    DemoLibrary

---
+++ CSharpBible/MVVM_Tutorial/MVVM_39_MultiModelTest/Models/ScopedModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_39_MultiModelTest.Models
+{
+    public partial class ScopedModel : NotificationObjectCT, IScopedModel
+    {
+        private ISystemModel? _parent;
+        public string Name { get; private set; } = "";
+
+        public string Description { get; private set; }= "";
+
+        public ISystemModel? parent { get => _parent; set => SetParent(value); }
+
+        public Guid Id { get; }= Guid.NewGuid();
+
+        public IServiceScope? Scope { get; set; }
+
+        [ObservableProperty]
+        private int _ICommonValue;
+
+        private void SetParent(ISystemModel? value)
+        {
+            if (_parent == value || _parent != null)
+                return;
+            _parent = value;
+            Name = $"ScopedModel_{_parent!.ScModels.Count}";
+            Description = $"ScopedModel_{_parent.ScModels.Count} Description";
+        }
+
+    }
+}
---
+++ CSharpBible/MVVM_Tutorial/MVVM_39_MultiModelTest/ViewModels/MultiModelMainViewModel.cs
@@ -0,0 +1,81 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_39_MultiModelTest
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="MainWindowViewModel.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using CommunityToolkit.Mvvm.Input;
+using MVVM.View.Extension;
+using MVVM.ViewModel;
+using MVVM_39_MultiModelTest.Models;
+using System;
+using System.ComponentModel;
+
+namespace MVVM_39_MultiModelTest.ViewModels
+{
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public partial class MultiModelMainViewModel : BaseViewModelCT
+    {
+        #region Delegtes
+        public delegate void ShowModelDelegate(IScopedModel model);
+        #endregion
+
+        #region Properties
+        public static Func<ISystemModel> GetModel { get; set; } = () => IoC.GetRequiredService<ISystemModel>();
+
+        private readonly ISystemModel _model;
+        private IScopedModel? scopedModel1;
+        private IScopedModel? scopedModel2;
+
+        public ShowModelDelegate? showModel { get; set; }
+
+        public DateTime Now => _model.Now;
+        #endregion
+  
+        #region Methods
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public MultiModelMainViewModel():this(GetModel())
+        {
+        }
+
+        public MultiModelMainViewModel(ISystemModel model)
+        {
+            _model = model;
+            _model.PropertyChanged += OnMPropertyChanged;
+        }
+
+        [RelayCommand]
+        private void OpenScopedModel1()
+        {
+            scopedModel1 ??= _model.GetNewScopedModel();
+            showModel?.Invoke(scopedModel1);
+        }   
+        
+        [RelayCommand]
+        private void OpenScopedModel2()
+        {
+            scopedModel2 ??= _model.GetNewScopedModel();
+            showModel?.Invoke(scopedModel2);
+        }
+
+        private void OnMPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(e.PropertyName); 
+        }
+
+        #endregion
+    }
+}
