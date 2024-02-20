cf26002bb6415e538b8e17337339bbf848846bdf
Joe Care <Joe_care_99@yahoo.com>
2024-02-20 16:39:37 +0100

    Calc32

---
+++ CSharpBible/MVVM_Tutorial/MVVM_26_CTBindingGroupExp/View/Validation/PriceIsAPositiveNumber.cs
@@ -0,0 +1,16 @@
+﻿using MVVM_26_CTBindingGroupExp.Properties;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace MVVM_26_CTBindingGroupExp.View.Validation
+{
+    public class PriceIsAPositiveNumber : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string s && decimal.TryParse(s,NumberStyles.Float,cultureInfo,out decimal d))
+                return new ValidationResult(d > 0, Resources.Err_MustbePositive);
+            return new ValidationResult(false, Resources.Err_HasWrongType);
+        }
+    }
+}
