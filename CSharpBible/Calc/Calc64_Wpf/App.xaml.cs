17d35a51f46b3241f4b5c7ebe929cf6c49a25cec
Joe Care <Joe_care_99@yahoo.com>
2024-02-26 16:49:38 +0100

    Calc/Calc64_Wpf

---
+++ CSharpBible/Calc/Calc64_Wpf/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Calc64Base;
+using MVVM.View.Extension;
+
+
+namespace Calc64_Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var BaseServices = new ServiceCollection()
+                .AddSingleton<ICalculator, Calc64Model>();
+                
+            var serviceProvider = BaseServices.BuildServiceProvider();
+
+            IoC.Configure(serviceProvider);
+
+        }
+    }
+
+}
