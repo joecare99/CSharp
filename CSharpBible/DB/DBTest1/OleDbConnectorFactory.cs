c1b220c834969c215c0484b0cde9cd6fed43276a
Joe Care <Joe_care_99@yahoo.com>
2024-02-29 03:06:43 +0100

    DBTest

---
+++ CSharpBible/DB/DBTest1/OleDbConnectorFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Common;
+using System.Data.OleDb;
+
+namespace DBTest1
+{
+    public sealed class OleDbConnectorFactory : DbProviderFactory
+    {
+        public static readonly OleDbConnectorFactory Instance = new OleDbConnectorFactory();
+        private OleDbConnectorFactory() { }
+
+        public override DbCommand? CreateCommand() => new OleDbCommand();
+        public override DbCommandBuilder? CreateCommandBuilder() => new OleDbCommandBuilder();
+        public override DbConnection? CreateConnection() => new OleDbConnection();
+        public override DbConnectionStringBuilder? CreateConnectionStringBuilder() => new OleDbConnectionStringBuilder();
+        public override DbDataAdapter? CreateDataAdapter() => new OleDbDataAdapter();
+        public override DbParameter? CreateParameter() => new OleDbParameter();
+
+    }
+}
