100a11c0039d3c81a6592c3b5c0e21dbd8c633b1
Joe Care <Joe_care_99@yahoo.com>
2024-03-04 00:52:05 +0100

    MVVM_09_DialogBoxesTest

--- CSharpBible/MVVM_Tutorial/MVVM_09_DialogBoxesTest/ViewModel/MainWindowViewModelTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_09_DialogBoxesTest/ViewModel/MainWindowViewModelTests.cs
@@ -30,21 +30,7 @@ namespace MVVM_09_DialogBoxes.ViewModel.Tests
             testModel.PropertyChanged += OnVMPropertyChanged;
             if (testModel is INotifyPropertyChanging npchgn)
                 npchgn.PropertyChanging += OnVMPropertyChanging;
-            testModel.DoOpenDialog += DoOpenDialogTest;
-            testModel.DoOpenMessageBox += DoOpenMessageBoxTest;
             ClearLog();
-        }
-
-        private System.Windows.MessageBoxResult DoOpenMessageBoxTest(string title, string name)
-        {
-            DoLog($"DoOpenMessageBox({title},{name})=>{mbResult}");
-            return mbResult;
-        }
-
-        private (string name, string email) DoOpenDialogTest(string name, string email)
-        {
-            DoLog($"DoOpenMessageBox({name},{email})=>{tResult}");
-            return tResult;
         }
 
         /// <summary>
@@ -58,67 +44,6 @@ namespace MVVM_09_DialogBoxes.ViewModel.Tests
             Assert.IsInstanceOfType(testModel, typeof(MainWindowViewModel));
             Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
             Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
-            Assert.IsNotNull(testModel.OpenMsgCommand);
-            Assert.IsInstanceOfType(testModel.OpenMsgCommand, typeof(IRelayCommand));
-            Assert.IsNotNull(testModel.OpenDialogCommand);
-            Assert.IsInstanceOfType(testModel.OpenDialogCommand, typeof(IRelayCommand));
-        }
-
-        [DataTestMethod()]
-        [DataRow(true,MessageBoxResult.Yes, new[] { @"DoOpenMessageBox(Frage,Willst Du Das ?)=>Yes
-PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Name)=42 Entwickler
-", "42 Entwickler" })]
-        [DataRow(false, MessageBoxResult.OK, new[] { @"PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Name)=Nö
-", "Nö" })]
-        [DataRow(null, MessageBoxResult.No, new[] { @"DoOpenMessageBox(Frage,Willst Du Das ?)=>No
-PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Name)=Nö
-", "Nö" })]
-        public void OpenMsgCommandTest(bool? oAct,MessageBoxResult mrExp, string[] asExp)
-        {
-            // Arrange
-            testModel.Name = $"TestName{oAct}";
-            if (oAct == false) testModel.DoOpenMessageBox -= DoOpenMessageBoxTest;
-            mbResult = mrExp;
-            ClearLog();
-
-            // Act
-            Assert.IsTrue(testModel.OpenMsgCommand.CanExecute(oAct));
-            testModel.OpenMsgCommand.Execute(oAct);
-
-            // Assert
-            Assert.AreEqual(asExp[1], testModel.Name);
-            Assert.AreEqual(asExp[0], DebugLog);
-        }
-
-        [DataTestMethod()]
-        [DataRow(true,"0", new[] { @"DoOpenMessageBox(TestNameTrue,TestEmailTrue)=>(TestName0, TestMail0)
-PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Name)=TestName0
-PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Email)=TestMail0
-", "TestName0", "TestMail0" })]
-        [DataRow(false, "1", new[] { @"PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Name)=
-PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Email)=
-", "","" })]
-        [DataRow(null, "2", new[] { @"DoOpenMessageBox(TestName,TestEmail)=>(TestName2, TestMail2)
-PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Name)=TestName2
-PropChg(MVVM_09_DialogBoxes.ViewModel.MainWindowViewModel,Email)=TestMail2
-", "TestName2", "TestMail2" })]
-        public void OpenDialogCommandTest(bool? oAct,string sAct, string[] asExp)
-        {
-            // Arrange
-            testModel.Name = $"TestName{oAct}";
-            testModel.Email = $"TestEmail{oAct}";
-            tResult = ($"TestName{sAct}", $"TestMail{sAct}");
-            if (oAct == false) testModel.DoOpenDialog -= DoOpenDialogTest;
-            ClearLog();
-
-            // Act    
-            Assert.IsTrue(testModel.OpenDialogCommand.CanExecute(oAct));
-            testModel.OpenDialogCommand.Execute(oAct);
-            
-            // Assert
-            Assert.AreEqual(asExp[1], testModel.Name);
-            Assert.AreEqual(asExp[2], testModel.Email);
-            Assert.AreEqual(asExp[0], DebugLog);
         }
     }
 }
