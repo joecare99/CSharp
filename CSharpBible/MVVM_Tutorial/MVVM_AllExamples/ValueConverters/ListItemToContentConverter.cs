da90bef1347e93ca4f6a905aaa076b0bafcbee76
Joe Care <Joe_care_99@yahoo.com>
2024-03-03 15:21:22 +0100

    MVVM_AllExamples

--- CSharpBible/MVVM_Tutorial/MVVM_AllExamples/ValueConverters/ListItemToContentConverter.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_AllExamples/ValueConverters/ListItemToContentConverter.cs
@@ -24,10 +24,57 @@ public class ListItemToContentConverter : IValueConverter
             var v = Activator.CreateInstance(lbi.ExType);
             if (v is Page fe)
             {
-                return new Frame() { Content = fe };
+                if (lbi.Additionals is Dictionary<string, string> d)
+                {
+                    var tv = new TabControl();
+                    string Title = lbi.Description;
+                    string Description = lbi.Description;
+                    if (d.TryGetValue("Title", out string? t))
+                        Title = t;
+                    if (d.TryGetValue("Description", out string? dsc))
+                        Description = dsc;
+                    tv.ToolTip = Description;
+                    tv.Items.Add(new TabItem() { Header = Title, Content = new Frame() { Content = fe } });
+                    if (d.Keys.FirstOrDefault((o) => o.EndsWith("Views")) is string xaml)
+                    {
+                        tv.Items.Add(new TabItem()
+                        {
+                            Header = "Xaml",
+                            Content = new TextBox() { Text = d[xaml], IsReadOnly = true, VerticalScrollBarVisibility = ScrollBarVisibility.Visible }
+                        });
+                    }
+                    if (d.Keys.FirstOrDefault((o) => o.EndsWith("ViewModel")) is string vm)
+                    {
+                        tv.Items.Add(new TabItem()
+                        {
+                            Header = "ViewModel",
+                            Content = new TextBox() { Text = d[vm], IsReadOnly = true, VerticalScrollBarVisibility = ScrollBarVisibility.Visible }
+                        });
+                    }
+                    if (d.Keys.FirstOrDefault((o) => o.EndsWith("Model") && !o.EndsWith("ViewModel")) is string mdl)
+                    {
+                        tv.Items.Add(new TabItem()
+                        {
+                            Header = "Model",
+                            Content = new TextBox() { Text = d[mdl], IsReadOnly = true, VerticalScrollBarVisibility = ScrollBarVisibility.Visible }
+                        });
+                    }
+                    if (d.Keys.FirstOrDefault((o) => o.EndsWith("Converter")) is string vc)
+                    {
+                        tv.Items.Add(new TabItem()
+                        {
+                            Header = "ValueConverter",
+                            Content = new TextBox() { Text = d[vc], IsReadOnly = true, VerticalScrollBarVisibility = ScrollBarVisibility.Visible }
+                        });
+                    }
+
+                    return tv;
+                }
+                else
+                    return new Frame() { Content = fe };
             }
             else
-            return v;
+                return v;
         }
         return null;
     }
