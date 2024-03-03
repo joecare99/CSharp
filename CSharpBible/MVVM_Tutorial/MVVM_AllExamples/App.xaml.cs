40e30367c6e4e8a143e047651431145aeee9213a
Joe Care <Joe_care_99@yahoo.com>
2024-03-03 09:48:26 +0100

    DemoLibrary

---
+++ CSharpBible/MVVM_Tutorial/MVVM_AllExamples/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_AllExamples
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Windows;
+
+namespace MVVM_AllExamples
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
+namespace MVVM_AllExamples.Models { }
+namespace MVVM_AllExamples.ValueConverter { }
+namespace MVVM_AllExamples.Services { }
---
+++ CSharpBible/MVVM_Tutorial/MVVM_AllExamples/ValueConverters/ListItemToContentConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Data;
+using MVVM_AllExamples.Models;
+
+namespace MVVM_AllExamples.ValueConverters;
+
+public class ListItemToContentConverter : IValueConverter
+{
+
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return null;
+        if (value is ExItem lbi)
+        {
+            var v = Activator.CreateInstance(lbi.ExType);
+            if (v is Page fe)
+            {
+                return new Frame() { Content = fe };
+            }
+            else
+            return v;
+        }
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
