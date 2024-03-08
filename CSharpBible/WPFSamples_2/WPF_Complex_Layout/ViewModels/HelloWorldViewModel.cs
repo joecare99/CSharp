bac0e9af71ab85a0699893c0b63967ca7c1f6d7f
Joe Care <Joe_care_99@yahoo.com>
2024-03-08 19:42:30 +0100

    WPF_Complex_Layout

---
+++ CSharpBible/WPFSamples_2/WPF_Complex_Layout/ViewModels/HelloWorldViewModel.cs
@@ -0,0 +1,41 @@
+﻿// ***********************************************************************
+// Assembly         : WPF_Complex_Layout
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="HelloWorldViewModel.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using MVVM.ViewModel;
+using WPF_Complex_Layout.Models;
+using System;
+using System.ComponentModel;
+
+namespace WPF_Complex_Layout.ViewModels
+{
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public partial class HelloWorldViewModel : BaseViewModelCT
+    {
+        #region Properties
+        #endregion
+  
+        #region Methods
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public HelloWorldViewModel()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ CSharpBible/WPFSamples_2/WPF_Complex_Layout/Views/HelloWorldView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPF_Complex_Layout.Views
+{
+    /// <summary>
+    /// Interaktionslogik für HelloWorldView.xaml
+    /// </summary>
+    public partial class HelloWorldView : Page
+    {
+        public HelloWorldView()
+        {
+            InitializeComponent();
+        }
+    }
+}
