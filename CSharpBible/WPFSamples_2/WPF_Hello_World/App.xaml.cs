5f3cd51a22573999f29f4c13c97d22daf363dc1a
Joe Care <Joe_care_99@yahoo.com>
2024-03-08 06:20:19 +0100

    WPF_Hello_World

---
+++ CSharpBible/WPFSamples_2/WPF_Hello_World/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿// ***********************************************************************
+// Assembly         : WPF_Hello_World
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Windows;
+
+namespace WPF_Hello_World
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
+namespace WPF_Hello_World.Models { }
+namespace WPF_Hello_World.ValueConverter { }
+namespace WPF_Hello_World.Services { }
---
+++ CSharpBible/WPFSamples_2/WPF_Hello_World/ViewModels/HelloWorldViewModel.cs
@@ -0,0 +1,41 @@
+﻿// ***********************************************************************
+// Assembly         : WPF_Hello_World
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="HelloWorldViewModel.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using MVVM.ViewModel;
+using WPF_Hello_World.Models;
+using System;
+using System.ComponentModel;
+
+namespace WPF_Hello_World.ViewModels
+{
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public partial class HelloWorldViewModel : BaseViewModelCT
+    {
+        #region Properties
+        #endregion
+  
+        #region Methods
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public HelloWorldViewModel()
+        {
+        }
+
+        #endregion
+    }
+}
