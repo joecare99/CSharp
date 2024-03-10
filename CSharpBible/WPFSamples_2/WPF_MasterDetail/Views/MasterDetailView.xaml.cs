d2ef3e5593052d2f22a4f97dd7cfc91cb47cc8a9
Joe Care <Joe_care_99@yahoo.com>
2024-03-10 15:55:19 +0100

    WPF_AnimationTiming

---
+++ CSharpBible/WPFSamples_2/WPF_MasterDetail/Views/MasterDetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPF_MasterDetail.Views
+{
+    /// <summary>
+    /// Interaktionslogik für MasterDetailView.xaml
+    /// </summary>
+    public partial class MasterDetailView : Page
+    {
+        public MasterDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
