7f726ae7c9c123a392668f8f803c85700835f91e
Joe Care <Joe_care_99@yahoo.com>
2024-03-10 14:29:55 +0100

    WPF_AnimationTiming

---
+++ CSharpBible/WPFSamples_2/WPF_CustomAnimation/Views/CustomAnimationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WPF_CustomAnimation.Views
+{
+    /// <summary>
+    /// Interaktionslogik für CustomAnimationView.xaml
+    /// </summary>
+    public partial class CustomAnimationView : Page
+    {
+        public CustomAnimationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
