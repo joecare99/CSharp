c8e308f2f8f2f9055ca1d5af98b96b1f33507732
Joe Care <Joe_care_99@yahoo.com>
2024-05-11 01:08:08 +0200

    MVVM_06_Converters_3Tests

--- CSharpBible/MVVM_Tutorial/MVVM_06_Converters_3Tests/ViewModel/CurrencyViewViewModelTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_06_Converters_3Tests/ViewModel/CurrencyViewViewModelTests.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MVVM_06_Converters_3.ViewModel;
 using MVVM.ViewModel;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVM_06_Converters_3.ViewModel.Tests
 {
