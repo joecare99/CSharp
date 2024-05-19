74efea7ce4a7c77a8ab8087b040dc7d475863a55
Joe Care <Joe_care_99@yahoo.com>
2024-05-19 15:28:25 +0200

    AboutEx

---
+++ CSharpBible/MVVM_Tutorial/SyncAsyncParallel/Model/SyncAsyncModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SyncAsyncParallel.Model;
+
+public class SyncAsyncModel : ISyncAsyncModel
+{
+    /// <summary>
+    /// The url's
+    /// </summary>
+    readonly string[] urls = new string[]{
+        "https://www.stackoverflow.com",
+        "https://www.microsoft.com",
+        "https://www.youtube.com",
+        "https://www.windows.com",
+        "https://www.asm.net",
+        "https://www.jc99.de",
+        "https://www.google.com",
+        "https://www.yahoo.com",
+        "https://www.bing.com",
+        "https://www.gmail.com"
+    };
+
+    public long Download_sync(Action<string> actS)
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+
+        List<DownloadResult> results = new(urls.Length);
+        foreach (string url in urls)
+        {
+            results.Add(_DownloadUrl(url));
+            ShowResults(results,actS);
+            Application.Current?.Dispatcher.Invoke(DispatcherPriority.Background, new ThreadStart(delegate { }));
+        }
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+
+    public async Task<long> Download_async(Action<string> actS)
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        List<DownloadResult> results = new(urls.Length);
+        foreach (string url in urls)
+        {
+            results.Add(await Task.Run(() => _DownloadUrl(url)));
+            ShowResults(results, actS);
+        }
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+
+    public async Task<long> Download_async_para(Action<string> actS)
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        List<Task<DownloadResult>> tasklist = new(urls.Length);
+        foreach (string url in urls)
+        {
+            tasklist.Add(Task.Run(() => _DownloadUrl(url)));
+        }
+
+        List<DownloadResult> results = new();
+        while (tasklist.Count > 0)
+        {
+            var result = await Task.WhenAny(tasklist);
+            tasklist.Remove(result);
+            results.Add(result.Result);
+            ShowResults(results,actS);
+        }
+        watch.Stop();
+        return watch.ElapsedMilliseconds;
+    }
+
+    /// <summary>
+    /// Downloads the URL.
+    /// </summary>
+    /// <param name="url">The URL.</param>
+    /// <returns>DownloadResult.</returns>
+    private DownloadResult _DownloadUrl(string url)
+    {
+        using HttpClient client = new();
+        try
+        {
+            string html = client.GetStringAsync(url).Result;
+            return new DownloadResult()
+            {
+                Html = html,
+                Url = url
+            };
+        }
+        catch (Exception e)
+        {
+            return new DownloadResult()
+            {
+                Html = e.Message,
+                Url = url
+            };
+
+        }
+    }
+    /// <summary>
+    /// Shows the results.
+    /// </summary>
+    /// <param name="results">The results.</param>
+    private void ShowResults(List<DownloadResult> results, Action<string> actS)
+    {
+        StringBuilder text = new();
+        foreach (var result in results)
+        {
+            text.Append(result.Url);
+            text.Append('\t');
+            text.Append(result.ContentLength);
+            text.Append(Environment.NewLine);
+        }
+        actS(text.ToString());
+
+    }
+}
---
+++ CSharpBible/MVVM_Tutorial/SyncAsyncParallel/Views/SyncAsyncView.xaml.cs
@@ -0,0 +1,30 @@
+﻿// ***********************************************************************
+// Assembly         : SyncAsyncParallel
+// Author           : Mir
+// Created          : 12-26-2021
+//
+// Last Modified By : Mir
+// Last Modified On : 12-26-2021
+// ***********************************************************************
+// <copyright file="SyncAsyncView.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2021
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Windows.Controls;
+
+namespace SyncAsyncParallel.Views;
+
+/// <summary>
+/// Interaktionslogik für SyncAsyncView.xaml
+/// </summary>
+public partial class SyncAsyncView : Page
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SyncAsyncView"/> class.
+    /// </summary>
+    public SyncAsyncView()
+    {
+        InitializeComponent();
+    }
+}
