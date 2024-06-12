776067f9bcd173f439c5612cd25009ea9fe9d857
Joe Care <Joe_care_99@yahoo.com>
2024-06-12 22:04:41 +0200

    AboutEx

---
+++ CSharpBible/MVVM_Tutorial/MVVM_40_Wizzard/Models/SimpleLog.cs
@@ -0,0 +1,29 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_00_IoCTemplate
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using BaseLib.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace MVVM_40_Wizzard.Models;
+
+public class SimpleLog(ISysTime sysTime):ILog
+{
+    ISysTime _sysTime { get; } = sysTime;
+
+    public void Log(string message) 
+        => Debug.WriteLine($"{_sysTime.Now.ToString(CultureInfo.InvariantCulture)}: Msg: {message}");
+    public void Log(string message, Exception exception) 
+        => Debug.WriteLine($"{_sysTime.Now.ToString(CultureInfo.InvariantCulture)}: Err: {message}, {exception}");
+}
---
+++ CSharpBible/MVVM_Tutorial/MVVM_40_Wizzard/ViewModels/WizzardViewModel.cs
@@ -0,0 +1,56 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_40_Wizzard
+// Author           : Mir
+// Created          : 08-11-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-24-2022
+// ***********************************************************************
+// <copyright file="MainWindowViewModel.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using MVVM.View.Extension;
+using MVVM.ViewModel;
+using MVVM_40_Wizzard.Models;
+using System;
+using System.ComponentModel;
+
+namespace MVVM_40_Wizzard.ViewModels
+{
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public partial class WizzardViewModel : BaseViewModelCT
+    {
+        #region Properties
+        private readonly IWizzardModel _model;
+
+        public DateTime Now => _model.Now;
+        #endregion
+  
+        #region Methods
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        public WizzardViewModel():this(IoC.GetRequiredService<IWizzardModel>())
+        {
+        }
+
+        public WizzardViewModel(IWizzardModel model)
+        {
+            _model = model;
+            _model.PropertyChanged += OnMPropertyChanged;
+        }
+
+        private void OnMPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(e.PropertyName); 
+        }
+
+        #endregion
+    }
+}
