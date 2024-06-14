6493c132f3c331c8c6d00cb94f801ec9fec81975
Joe Care <Joe_care_99@yahoo.com>
2024-06-14 20:45:28 +0200

    MVVM_BaseLib

--- CSharpBible/Libraries/MVVM_BaseLib/View/ValueConverter/Bool2VisibilityConverter.cs
+++ CSharpBible/Libraries/MVVM_BaseLib/View/ValueConverter/Bool2VisibilityConverter.cs
@@ -1,12 +1,12 @@
 ﻿// ***********************************************************************
-// Assembly         : MVVM_6_Converters_3
+// Assembly         : MVVM.View.Converters
 // Author           : Mir
 // Created          : 07-03-2022
 //
 // Last Modified By : Mir
 // Last Modified On : 07-04-2022
 // ***********************************************************************
-// <copyright file="Bool2VisibilityConverter.cs" company="MVVM_6_Converters_3">
+// <copyright file="Bool2VisibilityConverter.cs" company="MVVM.View.Converter">
 //     Copyright (c) JC-Soft. All rights reserved.
 // </copyright>
 // <summary></summary>
@@ -16,45 +16,44 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace MVVM.View.ValueConverter
+namespace MVVM.View.ValueConverter;
+
+/// <summary>
+/// Class Bool2VisibilityConverter.
+/// Implements the <see cref="IValueConverter" />
+/// </summary>
+/// <seealso cref="IValueConverter" />
+public class Bool2VisibilityConverter : IValueConverter
 {
     /// <summary>
-    /// Class Bool2VisibilityConverter.
-    /// Implements the <see cref="IValueConverter" />
+    /// Converts a value.
     /// </summary>
-    /// <seealso cref="IValueConverter" />
-    public class Bool2VisibilityConverter : IValueConverter
+    /// <param name="value">The value produced by the binding source.</param>
+    /// <param name="targetType">The type of the binding target property.</param>
+    /// <param name="parameter">The converter parameter to use.</param>
+    /// <param name="culture">The culture to use in the converter.</param>
+    /// <returns>A converted value. If the method returns <see langword="null" />, the valid null value is used.</returns>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        /// <summary>
-        /// Converts a value.
-        /// </summary>
-        /// <param name="value">The value produced by the binding source.</param>
-        /// <param name="targetType">The type of the binding target property.</param>
-        /// <param name="parameter">The converter parameter to use.</param>
-        /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value. If the method returns <see langword="null" />, the valid null value is used.</returns>
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is bool x)
-                return x ? Visibility.Visible : Visibility.Hidden;
-            else
-                return Visibility.Visible;
-        }
+        if (value is bool x)
+            return x ? Visibility.Visible : Visibility.Hidden;
+        else
+            return Visibility.Visible;
+    }
 
-        /// <summary>
-        /// Converts a value.
-        /// </summary>
-        /// <param name="value">The value that is produced by the binding target.</param>
-        /// <param name="targetType">The type to convert to.</param>
-        /// <param name="parameter">The converter parameter to use.</param>
-        /// <param name="culture">The culture to use in the converter.</param>
-        /// <returns>A converted value. If the method returns <see langword="null" />, the valid null value is used.</returns>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is Visibility v)
-                return Visibility.Visible == v;
-            else
-                return false;
-        }
+    /// <summary>
+    /// Converts a value.
+    /// </summary>
+    /// <param name="value">The value that is produced by the binding target.</param>
+    /// <param name="targetType">The type to convert to.</param>
+    /// <param name="parameter">The converter parameter to use.</param>
+    /// <param name="culture">The culture to use in the converter.</param>
+    /// <returns>A converted value. If the method returns <see langword="null" />, the valid null value is used.</returns>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Visibility v)
+            return Visibility.Visible == v;
+        else
+            return false;
     }
 }
