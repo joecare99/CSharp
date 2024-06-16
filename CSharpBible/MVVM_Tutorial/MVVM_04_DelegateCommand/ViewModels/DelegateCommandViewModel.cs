eebd012824dbf67c6bb68ade19899fabaace8931
Joe Care <Joe_care_99@yahoo.com>
2024-06-16 22:57:42 +0200

    MVVM_04_DelegateCommand

--- CSharpBible/MVVM_Tutorial/MVVM_04_DelegateCommand/ViewModels/DelegateCommandViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_04_DelegateCommand/ViewModels/DelegateCommandViewModel.cs
@@ -1,34 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using MVVM.ViewModel;
 
-namespace MVVM_04_DelegateCommand.ViewModels
+namespace MVVM_04_DelegateCommand.ViewModels;
+
+public class DelegateCommandViewModel : BaseViewModel
 {
-    public class DelegateCommandViewModel : BaseViewModel
+    #region Properties
+    private string _firstname;
+    private string _lastname;
+
+    public RelayCommand<object> ClearCommand { get; set; }
+    public string Firstname { get => _firstname; set => SetProperty(ref _firstname, value); }
+    public string Lastname { get => _lastname; set => SetProperty(ref _lastname , value); }
+    public string Fullname => $"{Lastname}, {Firstname}";
+    #endregion
+
+    #region Methods
+    public DelegateCommandViewModel()
     {
-        #region Properties
-        private string _firstname;
-        private string _lastname;
-
-        public RelayCommand<object> ClearCommand { get; set; }
-        public string Firstname { get => _firstname; set => SetProperty(ref _firstname, value); }
-        public string Lastname { get => _lastname; set => SetProperty(ref _lastname , value); }
-        public string Fullname => $"{Lastname}, {Firstname}";
-        #endregion
-
-        #region Methods
-        public DelegateCommandViewModel()
-        {
-            _firstname = "Dave";
-            _lastname = "Dev";
-            AddPropertyDependency(nameof(Fullname), nameof(Lastname));
-            AddPropertyDependency(nameof(Fullname), nameof(Firstname));
-            AddPropertyDependency(nameof(ClearCommand), nameof(Lastname));
-            AddPropertyDependency(nameof(ClearCommand), nameof(Firstname));
-            ClearCommand = new(
-                (o)=> { Firstname = ""; Lastname = ""; }, 
-                (o) => !string.IsNullOrEmpty(_firstname) || !string.IsNullOrEmpty(_lastname));
-        }
-        #endregion
-
+        _firstname = "Dave";
+        _lastname = "Dev";
+        AddPropertyDependency(nameof(Fullname), nameof(Lastname));
+        AddPropertyDependency(nameof(Fullname), nameof(Firstname));
+        AddPropertyDependency(nameof(ClearCommand), nameof(Lastname));
+        AddPropertyDependency(nameof(ClearCommand), nameof(Firstname));
+        ClearCommand = new(
+            (o)=> { Firstname = ""; Lastname = ""; }, 
+            (o) => !string.IsNullOrEmpty(_firstname) || !string.IsNullOrEmpty(_lastname));
     }
+    #endregion
+
 }
