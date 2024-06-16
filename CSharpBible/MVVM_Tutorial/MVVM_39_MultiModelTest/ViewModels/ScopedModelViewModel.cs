d856868fd75cab34de5fafd81245681aadfd921e
Joe Care <Joe_care_99@yahoo.com>
2024-06-16 13:11:04 +0200

    MVVM_39_MultiModelTest

--- CSharpBible/MVVM_Tutorial/MVVM_39_MultiModelTest/ViewModels/ScopedModelViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_39_MultiModelTest/ViewModels/ScopedModelViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,7 +22,7 @@ public partial class ScopedModelViewModel: BaseViewModelCT
     public int ICommonValue { get => _model?.ICommonValue ?? 0; set => _model.ICommonValue = value; }
 
     [ObservableProperty]
-    private string _FrameName = new Uri($"/{nameof(MVVM_39_MultiModelTest)};component/views/DetailPage1.xaml", UriKind.Relative).ToString();
+    private string _FrameName = new Uri($"/{Assembly.GetExecutingAssembly().GetName().Name};component/views/DetailPage1.xaml", UriKind.Relative).ToString();
 
     public Action<object> DoClose { get; set; } = _ => { };
 
@@ -55,21 +56,21 @@ public partial class ScopedModelViewModel: BaseViewModelCT
     private void Detail1()
     {
         IoC.SetCurrentScope(Scope);
-        Uri resourceLocater = new Uri($"/{nameof(MVVM_39_MultiModelTest)};component/views/DetailPage1.xaml", UriKind.Relative);
+        Uri resourceLocater = new Uri($"/{Assembly.GetExecutingAssembly().GetName().Name};component/views/DetailPage1.xaml", UriKind.Relative);
         FrameName = resourceLocater.ToString();
     }
     [RelayCommand]
     private void Detail2()
     {
         IoC.SetCurrentScope(Scope);
-        Uri resourceLocater = new Uri($"/{nameof(MVVM_39_MultiModelTest)};component/views/DetailPage2.xaml", UriKind.Relative);
+        Uri resourceLocater = new Uri($"/{Assembly.GetExecutingAssembly().GetName().Name};component/views/DetailPage2.xaml", UriKind.Relative);
         FrameName = resourceLocater.ToString();
     }
     [RelayCommand]
     private void Detail3()
     {
         IoC.SetCurrentScope(Scope);
-        Uri resourceLocater = new Uri($"/{nameof(MVVM_39_MultiModelTest)};component/views/DetailPage3.xaml", UriKind.Relative);
+        Uri resourceLocater = new Uri($"/{Assembly.GetExecutingAssembly().GetName().Name};component/views/DetailPage3.xaml", UriKind.Relative);
         FrameName = resourceLocater.ToString();
     }
 
