efcb4cf9ac69d57581e249a01881a883dda8c4ab
Joe Care <Joe_care_99@yahoo.com>
2024-06-16 22:57:40 +0200

    MVVM_03_NotifyChange

--- CSharpBible/MVVM_Tutorial/MVVM_03_NotifyChange/App.xaml.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_03_NotifyChange/App.xaml.cs
@@ -13,12 +13,11 @@
 // ***********************************************************************
 using System.Windows;
 
-namespace MVVM_03_NotifyChange
+namespace MVVM_03_NotifyChange;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
--- CSharpBible/MVVM_Tutorial/MVVM_03_NotifyChange/ViewModels/NotifyChangeViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_03_NotifyChange/ViewModels/NotifyChangeViewModel.cs
@@ -1,27 +1,26 @@
 ﻿using MVVM.ViewModel;
 
-namespace MVVM_03_NotifyChange.ViewModels
+namespace MVVM_03_NotifyChange.ViewModels;
+
+public class NotifyChangeViewModel : BaseViewModel
 {
-    public class NotifyChangeViewModel : BaseViewModel
+    #region Properties
+    private string _firstname;
+    private string _lastname;
+
+    public string Firstname { get => _firstname; set => SetProperty(ref _firstname, value); }
+    public string Lastname { get => _lastname; set => SetProperty(ref _lastname , value); }
+    public string Fullname => $"{Lastname}, {Firstname}";
+    #endregion
+
+    #region Methods
+    public NotifyChangeViewModel()
     {
-        #region Properties
-        private string _firstname;
-        private string _lastname;
-
-        public string Firstname { get => _firstname; set => SetProperty(ref _firstname, value); }
-        public string Lastname { get => _lastname; set => SetProperty(ref _lastname , value); }
-        public string Fullname => $"{Lastname}, {Firstname}";
-        #endregion
-
-        #region Methods
-        public NotifyChangeViewModel()
-        {
-            _firstname = "Dave";
-            _lastname = "Dev";
-            AddPropertyDependency(nameof(Fullname), nameof(Lastname));
-            AddPropertyDependency(nameof(Fullname), nameof(Firstname));
-        }
-        #endregion
-
+        _firstname = "Dave";
+        _lastname = "Dev";
+        AddPropertyDependency(nameof(Fullname), nameof(Lastname));
+        AddPropertyDependency(nameof(Fullname), nameof(Firstname));
     }
+    #endregion
+
 }
