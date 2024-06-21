2ea3a871d327fd0794a62cd97b043932115019ce
Joe Care <Joe_care_99@yahoo.com>
2024-06-21 09:04:28 +0200

    TestStatementsNew

--- TestStatements/TestStatementsNew/Program.cs
+++ TestStatements/TestStatementsNew/Program.cs
@@ -7,6 +7,7 @@ static void Init()
 static void Run()
 {
     TestStatementsNew.Anweisungen.SwitchStatement3.ShowTollCollector();
+    TestStatements.SystemNS.Printing.Printing_Ex.PrintDocument();
 }
 
 // See https://aka.ms/new-console-template for more information
