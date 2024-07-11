bc77afb2515ba460eb1ff8e37d1a8e513778781f
Joe Care <Joe_care_99@yahoo.com>
2024-07-11 19:06:42 +0200

    GenFreeBaseTests

--- GenFreeWin/GenFreeBaseTests/Data/IndexDefTests.cs
+++ GenFreeWin/GenFreeBaseTests/Data/IndexDefTests.cs
@@ -10,13 +10,14 @@ namespace GenFree.Data.Tests
     [TestClass()]
     public class IndexDefTests
     {
-
+        private TableDef td;
         private IndexDef testClass;
 
         [TestInitialize]
         public void Init()
         {
-            testClass = new IndexDef();
+            td = new TableDef(null,"");
+            testClass = new IndexDef(td,"Name","sField",true,false);
         }
 
         [TestMethod()]
