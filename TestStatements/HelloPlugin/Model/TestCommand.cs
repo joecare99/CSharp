a280ee650123ae30b38790f9b485740c7efcec64
Joe Care <Joe_care_99@yahoo.com>
2024-12-23 00:08:34 +0100

    AppWithPlugin

---
+++ TestStatements/HelloPlugin/Model/TestCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloPlugin.Properties;
+using Microsoft.Extensions.Logging;
+using PluginBase.Interfaces;
+
+namespace HelloPlugin.Model;
+
+public class TestCommand : ICommand
+{
+    private IEnvironment? _env;
+    private IRandom? _rnd;
+    private ILogger? _logger;
+    private ISysTime? _time;
+
+    public string Name { get => "test"; }
+    public string Description { get => Resources.testDescription; }
+    public void Initialize(IEnvironment env)
+    {
+        _env = env;
+        _rnd = _env.GetService<IRandom>();
+        _logger = _env.GetService<ILogger>();
+        _time = _env.GetService<ISysTime>();
+    }
+
+    public int Execute()
+    {
+        _logger?.LogDebug($"{nameof(TestCommand)}.{nameof(Execute)}");
+        _env?.ui.ShowMessage(Resources.msgTest);
+        _env?.ui.ShowMessage(string.Format(Resources.msgRandom,[_rnd?.Next()]));
+        _env?.ui.ShowMessage(string.Format(Resources.msgCurrentTime,[_time?.Now]));
+        return 0;
+    }
+}
+
---
+++ TestStatements/PluginBase/Interfaces/IEnvironment.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginBase.Interfaces;
+
+public interface IEnvironment
+{
+    IData data { get; }
+    IUserInterface ui { get; }
+    IMessenger messaging { get; }
+    T? GetService<T>();
+
+    bool AddService<T>(T service);
+}
