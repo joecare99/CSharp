5caca8ce2b5005c8316af608fead71d04e336346
Joe Care <Joe_care_99@yahoo.com>
2025-01-06 11:14:30 +0100

    GenFreeDataTests

--- GenFreeWin/GenFreeDataTests/GenFree/Data/CPersonDataTests.cs
+++ GenFreeWin/GenFreeDataTests/GenFree/Data/CPersonDataTests.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GenFree.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GenFree.Interfaces.DB;
 using NSubstitute;
 using GenFree.Helper;
