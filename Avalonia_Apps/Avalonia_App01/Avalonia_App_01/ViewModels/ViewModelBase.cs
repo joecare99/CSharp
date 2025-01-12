fa25fe1081167477cdeee1c4d0ff2a095702750d
Joe Care <Joe_care_99@yahoo.com>
2025-01-12 16:26:56 +0100

    Avalonia_App_01

---
+++ Avalonia_Apps/Avalonia_App01/Avalonia_App_01/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia_App_01.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Avalonia_Apps/Avalonia_App02/Avalonia_App02/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿// ***********************************************************************
+// Assembly         : Avalonia_App02
+// Author           : Mir
+// Created          : 01-11-2025
+//
+// Last Modified By : Mir
+// Last Modified On : 01-12-2025
+// ***********************************************************************
+// <copyright file="MainWindowViewModel.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2025
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+/// <summary>
+/// The ViewModels namespace.
+/// </summary>
+namespace Avalonia_App02.ViewModels;
+
+/// <summary>
+/// Class MainWindowViewModel.
+/// Implements the <see cref="Avalonia_App02.ViewModels.ViewModelBase" />
+/// </summary>
+/// <seealso cref="Avalonia_App02.ViewModels.ViewModelBase" />
+public class MainWindowViewModel : ViewModelBase
+{
+    /// <summary>
+    /// Gets the greeting.
+    /// </summary>
+    /// <value>The greeting.</value>
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
