060df3ade748115eb25325dc341967f1f7818238
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:54:05 +0100

    MVVM_31a_CTValidation3Tests

--- CSharpBible/MVVM_Tutorial/MVVM_31a_CTValidation3Tests/Validator/NotTheSpecDataTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_31a_CTValidation3Tests/Validator/NotTheSpecDataTests.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MVVM_31a_CTValidation3.Validator.Tests
-{
-    [TestClass]
-    public class NotTheSpecDataTests
-    {
+namespace MVVM_31a_CTValidation3.Validator.Tests;
 
-        [DataTestMethod()]
-        [DataRow("BlaBla", "BlaBla", false)]
-        [DataRow("BlaBla", "BlaBla1", true)]
-        [DataRow("BlaBla", null, true)]
-        [DataRow("BlaBla", 5, true)]
-        public void IsValidTest(string sAct1,object? sAct2,bool xExp)
-        {
-            var test = new NotTheSpecData(sAct1);
-            Assert.AreEqual(xExp,test.IsValid(sAct2));
-        }
+[TestClass]
+public class NotTheSpecDataTests
+{
+
+    [DataTestMethod()]
+    [DataRow("BlaBla", "BlaBla", false)]
+    [DataRow("BlaBla", "BlaBla1", true)]
+    [DataRow("BlaBla", null, true)]
+    [DataRow("BlaBla", 5, true)]
+    public void IsValidTest(string sAct1,object? sAct2,bool xExp)
+    {
+        var test = new NotTheSpecData(sAct1);
+        Assert.AreEqual(xExp,test.IsValid(sAct2));
     }
 }
