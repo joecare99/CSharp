3b08bda908ff98d206d7e5ce4d8a7d822d2333f7
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:54:00 +0100

    MVVM_28_DataGrid

--- CSharpBible/MVVM_Tutorial/MVVM_28_DataGrid/Models/Person.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_28_DataGrid/Models/Person.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 
-namespace MVVM_28_DataGrid.Models
+namespace MVVM_28_DataGrid.Models;
+
+public class Person
 {
-    public class Person
-    {
-        public int Id { get; set; } = -1;
-        public string? LastName { get; set; } = null;
-        public string? FirstName { get; set; } = null;
-        public string? Title { get; set; } = null;
-        public string? Email { get; set; } = null;
-        public DateTime? Birthday { get; set; } = null;
-        public Department? Department { get; set; } = null;
-    }
+    public int Id { get; set; } = -1;
+    public string? LastName { get; set; } = null;
+    public string? FirstName { get; set; } = null;
+    public string? Title { get; set; } = null;
+    public string? Email { get; set; } = null;
+    public DateTime? Birthday { get; set; } = null;
+    public Department? Department { get; set; } = null;
 }
