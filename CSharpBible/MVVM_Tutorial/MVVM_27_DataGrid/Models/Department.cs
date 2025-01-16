902242cbd2ebbaab2ca7b168a44224a45af50d19
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:55 +0100

    MVVM_27_DataGrid

--- CSharpBible/MVVM_Tutorial/MVVM_27_DataGrid/Models/Department.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_27_DataGrid/Models/Department.cs
@@ -1,12 +1,11 @@
 ﻿using MVVM.ViewModel;
 
-namespace MVVM_27_DataGrid.Models
-{
-    public class Department : NotificationObject
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+namespace MVVM_27_DataGrid.Models;
 
-    }
+public class Department : NotificationObject
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
 }
--- CSharpBible/MVVM_Tutorial/MVVM_27_DataGrid/Services/PersonService.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_27_DataGrid/Services/PersonService.cs
@@ -2,28 +2,27 @@
 using System;
 using System.Collections.Generic;
 
-namespace MVVM_27_DataGrid.Services
+namespace MVVM_27_DataGrid.Services;
+
+public class PersonService
 {
-    public class PersonService
+    public IEnumerable<Person> GetPersons()
     {
-        public IEnumerable<Person> GetPersons()
+        var deps = GetDepartments();
+        //var 
+        return new Person[]
         {
-            var deps = GetDepartments();
-            //var 
-            return new Person[]
-            {
-                    new(){FirstName="Max",LastName="Muster",Email="max@muster.com",Id=1,Department = deps[0] },
-                    new(){FirstName="Susi",LastName="Müller",Email="susi@muster.com",Id=2,Department = deps[1],Birthday= new DateTime(1980,1,1) },
-                    new(){FirstName="Dave",LastName="Dev",Email="dev.dave@muster.com",Id=3,Department = deps[3],Birthday= new DateTime(1988,5,2) },
-                    new(){FirstName="Herbert",LastName="Bossinger",Email="ceo@muster.com",Id=4,Department = deps[2],Birthday= new DateTime(1999,7,7) }
-            };
-        }
-        public Department[] GetDepartments() 
-            => new Department[]{
-                new(){ Id=1,Name="Engineering",Description="The Engineering-department" },
-                new(){ Id=2,Name="Sales",Description="The Sales-department" },
-                new(){ Id=3,Name="Management",Description="The Management" },
-                new(){ Id=4,Name="Service",Description="The Service-department" } }
-                ;
+                new(){FirstName="Max",LastName="Muster",Email="max@muster.com",Id=1,Department = deps[0] },
+                new(){FirstName="Susi",LastName="Müller",Email="susi@muster.com",Id=2,Department = deps[1],Birthday= new DateTime(1980,1,1) },
+                new(){FirstName="Dave",LastName="Dev",Email="dev.dave@muster.com",Id=3,Department = deps[3],Birthday= new DateTime(1988,5,2) },
+                new(){FirstName="Herbert",LastName="Bossinger",Email="ceo@muster.com",Id=4,Department = deps[2],Birthday= new DateTime(1999,7,7) }
+        };
     }
+    public Department[] GetDepartments() 
+        => new Department[]{
+            new(){ Id=1,Name="Engineering",Description="The Engineering-department" },
+            new(){ Id=2,Name="Sales",Description="The Sales-department" },
+            new(){ Id=3,Name="Management",Description="The Management" },
+            new(){ Id=4,Name="Service",Description="The Service-department" } }
+            ;
 }
