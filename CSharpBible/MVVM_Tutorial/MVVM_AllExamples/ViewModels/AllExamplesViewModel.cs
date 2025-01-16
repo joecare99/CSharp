cc1d12db3c9c48916a5f4de9613a83d7d4dee03f
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:54:30 +0100

    MVVM_AllExamples

--- CSharpBible/MVVM_Tutorial/MVVM_AllExamples/ViewModels/AllExamplesViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_AllExamples/ViewModels/AllExamplesViewModel.cs
@@ -18,59 +18,58 @@ using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 
-namespace MVVM_AllExamples.ViewModels
+namespace MVVM_AllExamples.ViewModels;
+
+/// <summary>
+/// Class BindingGroupViewModel.
+/// Implements the <see cref="BaseViewModel" />
+/// </summary>
+/// <seealso cref="BaseViewModel" />
+public partial class AllExamplesViewModel : BaseViewModelCT
 {
+    #region Properties
+    public static Func<IAllExampleModel> GetModel { get; set; } = () => new AllExampleModel();
+
+    private readonly IAllExampleModel _model;
+
+    public DateTime Now => _model.Now;
+
+    [ObservableProperty]
+    private ObservableCollection<ExItem> _examples = new();
+
+    [ObservableProperty]
+    string _exFilter;
+    #endregion
+
+    #region Methods
     /// <summary>
-    /// Class BindingGroupViewModel.
-    /// Implements the <see cref="BaseViewModel" />
+    /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
     /// </summary>
-    /// <seealso cref="BaseViewModel" />
-    public partial class AllExamplesViewModel : BaseViewModelCT
+    public AllExamplesViewModel():this(GetModel())
     {
-        #region Properties
-        public static Func<IAllExampleModel> GetModel { get; set; } = () => new AllExampleModel();
-
-        private readonly IAllExampleModel _model;
-
-        public DateTime Now => _model.Now;
-
-        [ObservableProperty]
-        private ObservableCollection<ExItem> _examples = new();
-
-        [ObservableProperty]
-        string _exFilter;
-        #endregion
-  
-        #region Methods
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
-        /// </summary>
-        public AllExamplesViewModel():this(GetModel())
-        {
-        }
-
-        public AllExamplesViewModel(IAllExampleModel model)
-        {
-            _model = model;
-            _model.PropertyChanged += OnMPropertyChanged;
-
-            OnExFilterChanged(ExFilter);
-        }
-
-        private void OnMPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            OnPropertyChanged(e.PropertyName); 
-        }
-
-        partial void OnExFilterChanged(string value)
-        {
-            Examples.Clear();
-            foreach (var ex in _model.Examples)
-            {
-                if (string.IsNullOrEmpty(value) || ex.Description.Contains(value))
-                    Examples.Add(ex);
-            }
-        }
-        #endregion
     }
+
+    public AllExamplesViewModel(IAllExampleModel model)
+    {
+        _model = model;
+        _model.PropertyChanged += OnMPropertyChanged;
+
+        OnExFilterChanged(ExFilter);
+    }
+
+    private void OnMPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        OnPropertyChanged(e.PropertyName); 
+    }
+
+    partial void OnExFilterChanged(string value)
+    {
+        Examples.Clear();
+        foreach (var ex in _model.Examples)
+        {
+            if (string.IsNullOrEmpty(value) || ex.Description.Contains(value))
+                Examples.Add(ex);
+        }
+    }
+    #endregion
 }
