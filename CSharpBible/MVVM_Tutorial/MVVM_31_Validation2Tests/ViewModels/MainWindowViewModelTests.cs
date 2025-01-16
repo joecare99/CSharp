4acf22fc8fdaaf0a5f0252450aa9b512c8058b37
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:54:07 +0100

    MVVM_31_Validation2Tests

--- CSharpBible/MVVM_Tutorial/MVVM_31_Validation2Tests/ViewModels/MainWindowViewModelTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_31_Validation2Tests/ViewModels/MainWindowViewModelTests.cs
@@ -2,28 +2,27 @@
 using MVVM.ViewModel;
 using System.ComponentModel;
 
-namespace MVVM_31_Validation2.ViewModels.Tests
+namespace MVVM_31_Validation2.ViewModels.Tests;
+
+[TestClass()]
+public class MainWindowViewModelTests
 {
-    [TestClass()]
-    public class MainWindowViewModelTests
-    {
 #pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
-        MainWindowViewModel testModel;
+    MainWindowViewModel testModel;
 #pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
 
-        [TestInitialize]
-        public void Init()
-        {
-            testModel = new();
-        }
+    [TestInitialize]
+    public void Init()
+    {
+        testModel = new();
+    }
 
-        [TestMethod()]
-        public void SetupTest()
-        {
-            Assert.IsNotNull(testModel);
-            Assert.IsInstanceOfType(testModel, typeof(MainWindowViewModel));
-            Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
-            Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
-        }
+    [TestMethod()]
+    public void SetupTest()
+    {
+        Assert.IsNotNull(testModel);
+        Assert.IsInstanceOfType(testModel, typeof(MainWindowViewModel));
+        Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
+        Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
     }
 }
