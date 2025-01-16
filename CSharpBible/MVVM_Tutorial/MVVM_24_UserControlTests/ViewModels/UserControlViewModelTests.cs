1f32c9a60d6e2efa3dff858fcfc7ac2343a72b0d
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:51 +0100

    MVVM_24_UserControlTests

--- CSharpBible/MVVM_Tutorial/MVVM_24_UserControlTests/ViewModels/UserControlViewModelTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_24_UserControlTests/ViewModels/UserControlViewModelTests.cs
@@ -4,36 +4,36 @@ using MVVM.ViewModel;
 using BaseLib.Helper;
 using System.Collections.Generic;
 
-namespace MVVM_24_UserControl.ViewModels.Tests
-{
-    [TestClass()]
-    public class UserControlViewModelTests : BaseTestViewModel<UserControlViewModel>
-    {
-        [TestMethod()]
-        public void SetupTest()
-        {
-            Assert.IsNotNull(testModel);
-            Assert.IsNotNull(testModel2);
-            Assert.IsInstanceOfType(testModel, typeof(UserControlViewModel));
-            Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
-            Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
-        }
+namespace MVVM_24_UserControl.ViewModels.Tests;
 
-        [DataTestMethod()]
-        [DataRow(nameof(UserControlViewModel.Text1), "Hello World", new[] { @"" })]
-        [DataRow(nameof(UserControlViewModel.Text1), "Hello World2_", new[] { @"PropChg(MVVM_24_UserControl.ViewModels.UserControlViewModel,Text1)=Hello World2_
+[TestClass()]
+public class UserControlViewModelTests : BaseTestViewModel<UserControlViewModel>
+{
+    [TestMethod()]
+    public void SetupTest()
+    {
+        Assert.IsNotNull(testModel);
+        Assert.IsNotNull(testModel2);
+        Assert.IsInstanceOfType(testModel, typeof(UserControlViewModel));
+        Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
+        Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
+    }
+
+    [DataTestMethod()]
+    [DataRow(nameof(UserControlViewModel.Text1), "Hello World", new[] { @"" })]
+    [DataRow(nameof(UserControlViewModel.Text1), "Hello World2_", new[] { @"PropChg(MVVM_24_UserControl.ViewModels.UserControlViewModel,Text1)=Hello World2_
 " })]
 		[DataRow(nameof(UserControlViewModel.Text2), "Hello World 2", new[] { @"" })]
 		[DataRow(nameof(UserControlViewModel.Text2), "Hello World2_", new[] { @"" })]
 		public void PropTest(string sPropame, object? sValue, string[] asExp)
-        {
+    {
 			var data = GetDefaultData();
 			var expected = data[sPropame];
 			var actual = testModel.GetProp(sPropame);
 			Assert.AreEqual(expected, actual);
-            testModel.SetProp(sPropame, sValue);
+        testModel.SetProp(sPropame, sValue);
 			Assert.AreEqual(sValue, testModel.GetProp(sPropame));
-            Assert.AreEqual(asExp[0], DebugLog);
+        Assert.AreEqual(asExp[0], DebugLog);
 		}
 
 		protected override Dictionary<string, object?> GetDefaultData() => new()
@@ -44,4 +44,3 @@ namespace MVVM_24_UserControl.ViewModels.Tests
 				//{ nameof(UserControlViewModel.State2), false },
 			};
 	}
-}
