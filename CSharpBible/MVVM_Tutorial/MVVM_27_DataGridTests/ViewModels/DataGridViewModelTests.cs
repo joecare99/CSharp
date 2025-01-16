2f7dae99c7026851ea41e2e04e57588b8b982af8
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:56 +0100

    MVVM_27_DataGridTests

--- CSharpBible/MVVM_Tutorial/MVVM_27_DataGridTests/ViewModels/DataGridViewModelTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_27_DataGridTests/ViewModels/DataGridViewModelTests.cs
@@ -1,24 +1,23 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MVVM_27_DataGrid.ViewModels.Tests
+namespace MVVM_27_DataGrid.ViewModels.Tests;
+
+[TestClass]
+public class DataGridViewModelTests
 {
-    [TestClass]
-    public class DataGridViewModelTests
-    {
 #pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
-        DataGridViewModel testModel;
+    DataGridViewModel testModel;
 #pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
 
-        [TestInitialize]
-        public void Init()
-        {
-            testModel = new DataGridViewModel();
-        }
+    [TestInitialize]
+    public void Init()
+    {
+        testModel = new DataGridViewModel();
+    }
 
-        [TestMethod]
-        public void SetupTest() {
-            Assert.IsNotNull(testModel);
-            Assert.IsInstanceOfType(testModel, typeof(DataGridViewModel));
-        }
+    [TestMethod]
+    public void SetupTest() {
+        Assert.IsNotNull(testModel);
+        Assert.IsInstanceOfType(testModel, typeof(DataGridViewModel));
     }
 }
