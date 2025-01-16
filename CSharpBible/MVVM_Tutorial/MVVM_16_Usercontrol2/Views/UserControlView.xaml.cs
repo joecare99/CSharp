bec75936b76fe3d0945a654130a5f71d192dbe2d
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:35 +0100

    MVVM_16_Usercontrol2

--- CSharpBible/MVVM_Tutorial/MVVM_16_Usercontrol2/Views/UserControlView.xaml.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_16_Usercontrol2/Views/UserControlView.xaml.cs
@@ -1,13 +1,12 @@
 ﻿using System.Windows.Controls;
 
-namespace MVVM_16_UserControl2.Views
-{
-    /// <summary>
-    /// Interaktionslogik für UserControlView.xaml
-    /// </summary>
-    public partial class UserControlView : Page {
+namespace MVVM_16_UserControl2.Views;
+
+/// <summary>
+/// Interaktionslogik für UserControlView.xaml
+/// </summary>
+public partial class UserControlView : Page {
 		public UserControlView() {
 			InitializeComponent();
 		}
 	}
-}
