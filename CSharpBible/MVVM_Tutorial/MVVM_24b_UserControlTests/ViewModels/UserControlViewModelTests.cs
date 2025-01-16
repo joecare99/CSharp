cabc5881514f29e3103977f678bc37028551a0a3
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:48 +0100

    MVVM_24b_UserControlTests

--- CSharpBible/MVVM_Tutorial/MVVM_24b_UserControlTests/ViewModels/UserControlViewModelTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_24b_UserControlTests/ViewModels/UserControlViewModelTests.cs
@@ -4,20 +4,20 @@ using MVVM.ViewModel;
 using BaseLib.Helper;
 using System.Collections.Generic;
 
-namespace MVVM_24b_UserControl.ViewModels.Tests
-{
-    [TestClass()]
-    public class UserControlViewModelTests: BaseTestViewModel<UserControlViewModel>
+namespace MVVM_24b_UserControl.ViewModels.Tests;
+
+[TestClass()]
+public class UserControlViewModelTests: BaseTestViewModel<UserControlViewModel>
 	{
-        [TestMethod()]
-        public void SetupTest()
-        {
-            Assert.IsNotNull(testModel);
-            Assert.IsNotNull(testModel2);
-            Assert.IsInstanceOfType(testModel, typeof(UserControlViewModel));
-            Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
-            Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
-        }
+    [TestMethod()]
+    public void SetupTest()
+    {
+        Assert.IsNotNull(testModel);
+        Assert.IsNotNull(testModel2);
+        Assert.IsInstanceOfType(testModel, typeof(UserControlViewModel));
+        Assert.IsInstanceOfType(testModel, typeof(BaseViewModel));
+        Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
+    }
 
 		[DataTestMethod()]
 		[DataRow(nameof(UserControlViewModel.Text1), "Hello World", new[] { @"" })]
@@ -60,4 +60,3 @@ namespace MVVM_24b_UserControl.ViewModels.Tests
 			};
 
 	}
-}
