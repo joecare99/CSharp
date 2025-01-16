428438a22dc24ed0fc4abf23cfbda1b6150747b3
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:22 +0100

    MVVM_05a_CTCommandParCalc

--- CSharpBible/MVVM_Tutorial/MVVM_05a_CTCommandParCalc/ViewModels/MainWindowViewModel.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_05a_CTCommandParCalc/ViewModels/MainWindowViewModel.cs
@@ -13,21 +13,20 @@
 // ***********************************************************************
 using MVVM.ViewModel;
 
-namespace MVVM_05a_CTCommandParCalc.ViewModels
+namespace MVVM_05a_CTCommandParCalc.ViewModels;
+
+/// <summary>
+/// Class MainWindowViewModel.
+/// Implements the <see cref="BaseViewModel" />
+/// </summary>
+/// <seealso cref="BaseViewModel" />
+public class MainWindowViewModel : BaseViewModelCT
 {
     /// <summary>
-    /// Class MainWindowViewModel.
-    /// Implements the <see cref="BaseViewModel" />
+    /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
     /// </summary>
-    /// <seealso cref="BaseViewModel" />
-    public class MainWindowViewModel : BaseViewModelCT
+    public MainWindowViewModel()
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
-        /// </summary>
-        public MainWindowViewModel()
-        {
 
-        }
     }
 }
