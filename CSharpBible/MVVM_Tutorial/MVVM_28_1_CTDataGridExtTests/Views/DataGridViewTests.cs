b373b81c5869632d411d5602b99e0d99970a05b2
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:58 +0100

    MVVM_28_1_CTDataGridExtTests

--- CSharpBible/MVVM_Tutorial/MVVM_28_1_CTDataGridExtTests/Views/DataGridViewTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_28_1_CTDataGridExtTests/Views/DataGridViewTests.cs
@@ -3,22 +3,21 @@ using System.Threading;
 using NSubstitute;
 using MVVM.View.Extension;
 
-namespace MVVM_28_1_CTDataGridExt.Views.Tests
+namespace MVVM_28_1_CTDataGridExt.Views.Tests;
+
+[TestClass()]
+public class DataGridViewTests
 {
-    [TestClass()]
-    public class DataGridViewTests
+    [TestMethod()]
+    public void DataGridViewTest()
     {
-        [TestMethod()]
-        public void DataGridViewTest()
-        {
-            IoC.GetReqSrv = (t) => Substitute.For(new[] { t }, null!);
-            DataGridView? testView=null;
-            var t = new Thread(()=> testView = new());
-            t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
-            t.Start();
-            t.Join(); //Wait for the thread to end
-            Assert.IsNotNull(testView);
-            Assert.IsInstanceOfType(testView, typeof(DataGridView));    
-        }
+        IoC.GetReqSrv = (t) => Substitute.For(new[] { t }, null!);
+        DataGridView? testView=null;
+        var t = new Thread(()=> testView = new());
+        t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+        t.Start();
+        t.Join(); //Wait for the thread to end
+        Assert.IsNotNull(testView);
+        Assert.IsInstanceOfType(testView, typeof(DataGridView));    
     }
 }
