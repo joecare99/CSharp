138d971b242020280e51c666baea9aa19fc0e5b0
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 02:40:38 +0100

    MVVM_05a_CTCommandParCalc

--- CSharpBible/MVVM_Tutorial/MVVM_05a_CTCommandParCalc/App.xaml.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_05a_CTCommandParCalc/App.xaml.cs
@@ -11,14 +11,27 @@
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
+using Microsoft.Extensions.DependencyInjection;
+using MVVM.View.Extension;
+using MVVM_05a_CTCommandParCalc.Model;
+using MVVM_05a_CTCommandParCalc.Model.Interfaces;
 using System.Windows;
 
-namespace MVVM_05a_CTCommandParCalc
+namespace MVVM_05a_CTCommandParCalc;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    protected override void OnStartup(StartupEventArgs e)
     {
+        var sc = new ServiceCollection()
+            .AddTransient<ICalculatorModel, CalculatorModel>();
+
+        var bc = sc.BuildServiceProvider();
+
+        IoC.Configure(bc);
     }
+
 }
