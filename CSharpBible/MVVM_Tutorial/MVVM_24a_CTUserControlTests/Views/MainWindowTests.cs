56c501b0b6e6124dee073304f8345005c74ac7ef
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:47 +0100

    MVVM_24a_CTUserControlTests

--- CSharpBible/MVVM_Tutorial/MVVM_24a_CTUserControlTests/Views/MainWindowTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_24a_CTUserControlTests/Views/MainWindowTests.cs
@@ -1,21 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
 
-namespace MVVM_24a_CTUserControl.Views.Tests
+namespace MVVM_24a_CTUserControl.Views.Tests;
+
+[TestClass()]
+public class MainWindowTests
 {
-    [TestClass()]
-    public class MainWindowTests
+    [TestMethod()]
+    public void MainWindowTest()
     {
-        [TestMethod()]
-        public void MainWindowTest()
-        {
-            MainWindow? mw=null;
-            var t = new Thread(()=> mw = new());
-            t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
-            t.Start();
-            t.Join(); //Wait for the thread to end
-            Assert.IsNotNull(mw);
-            Assert.IsInstanceOfType(mw, typeof(MainWindow));    
-        }
+        MainWindow? mw=null;
+        var t = new Thread(()=> mw = new());
+        t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+        t.Start();
+        t.Join(); //Wait for the thread to end
+        Assert.IsNotNull(mw);
+        Assert.IsInstanceOfType(mw, typeof(MainWindow));    
     }
 }
--- CSharpBible/MVVM_Tutorial/MVVM_24a_CTUserControlTests/Views/UserControlViewTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_24a_CTUserControlTests/Views/UserControlViewTests.cs
@@ -1,21 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
 
-namespace MVVM_24a_CTUserControl.Views.Tests
+namespace MVVM_24a_CTUserControl.Views.Tests;
+
+[TestClass()]
+public class UserControlViewTests
 {
-    [TestClass()]
-    public class UserControlViewTests
+    [TestMethod()]
+    public void MainWindowTest()
     {
-        [TestMethod()]
-        public void MainWindowTest()
-        {
 			UserControlView? mw=null;
-            var t = new Thread(()=> mw = new());
-            t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
-            t.Start();
-            t.Join(); //Wait for the thread to end
-            Assert.IsNotNull(mw);
-            Assert.IsInstanceOfType(mw, typeof(UserControlView));    
-        }
+        var t = new Thread(()=> mw = new());
+        t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+        t.Start();
+        t.Join(); //Wait for the thread to end
+        Assert.IsNotNull(mw);
+        Assert.IsInstanceOfType(mw, typeof(UserControlView));    
     }
 }
