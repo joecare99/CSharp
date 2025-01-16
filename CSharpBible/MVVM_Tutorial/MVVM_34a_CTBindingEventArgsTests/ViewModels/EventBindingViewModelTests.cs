3e47f0f8faf05228fef6c7553dd2871efa2b2e9a
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:54:12 +0100

    MVVM_34a_CTBindingEventArgsTests

--- CSharpBible/MVVM_Tutorial/MVVM_34a_CTBindingEventArgsTests/ViewModels/EventBindingViewModelTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_34a_CTBindingEventArgsTests/ViewModels/EventBindingViewModelTests.cs
@@ -6,92 +6,91 @@ using System.Threading;
 using System;
 using System.Windows.Interop;
 
-namespace MVVM_34a_CTBindingEventArgs.ViewModels.Tests
+namespace MVVM_34a_CTBindingEventArgs.ViewModels.Tests;
+
+[TestClass()]
+public class EventBindingViewModelTests : BaseTestViewModel
 {
-    [TestClass()]
-    public class EventBindingViewModelTests : BaseTestViewModel
-    {
 #pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
-        EventsBindingViewModel testModel;
+    EventsBindingViewModel testModel;
 #pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
 
-        [TestInitialize]
-        public void Init()
-        {
-            testModel = new();
-            testModel.PropertyChanged += OnVMPropertyChanged;
-            if (testModel is INotifyPropertyChanging inpcn)
-                inpcn.PropertyChanging += OnVMPropertyChanging;
-            ClearLog();
-        }
+    [TestInitialize]
+    public void Init()
+    {
+        testModel = new();
+        testModel.PropertyChanged += OnVMPropertyChanged;
+        if (testModel is INotifyPropertyChanging inpcn)
+            inpcn.PropertyChanging += OnVMPropertyChanging;
+        ClearLog();
+    }
 
-        [TestMethod()]
-        public void SetupTest()
-        {
-            Assert.IsNotNull(testModel);
-            Assert.IsInstanceOfType(testModel, typeof(EventsBindingViewModel));
-            Assert.IsInstanceOfType(testModel, typeof(BaseViewModelCT));
-            Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
-            Assert.AreEqual("", DebugLog);
-        }
+    [TestMethod()]
+    public void SetupTest()
+    {
+        Assert.IsNotNull(testModel);
+        Assert.IsInstanceOfType(testModel, typeof(EventsBindingViewModel));
+        Assert.IsInstanceOfType(testModel, typeof(BaseViewModelCT));
+        Assert.IsInstanceOfType(testModel, typeof(INotifyPropertyChanged));
+        Assert.AreEqual("", DebugLog);
+    }
 
-        [TestMethod]
-        public void LostFocusCommandTest()
-        {
-            testModel.LostFocusCommand.Execute(null);
-            Assert.AreEqual("Lost focus", testModel.State);
-            Assert.AreEqual("PropChgn(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=\r\nPropChg(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=Lost focus\r\n", DebugLog);
-        }
+    [TestMethod]
+    public void LostFocusCommandTest()
+    {
+        testModel.LostFocusCommand.Execute(null);
+        Assert.AreEqual("Lost focus", testModel.State);
+        Assert.AreEqual("PropChgn(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=\r\nPropChg(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=Lost focus\r\n", DebugLog);
+    }
 
-        [TestMethod]
-        public void GotFocusCommandTest()
-        {
-            testModel.GotFocusCommand.Execute(null);
-            Assert.AreEqual("Got focus", testModel.State);
-            Assert.AreEqual("PropChgn(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=\r\nPropChg(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=Got focus\r\n", DebugLog);
-        }
+    [TestMethod]
+    public void GotFocusCommandTest()
+    {
+        testModel.GotFocusCommand.Execute(null);
+        Assert.AreEqual("Got focus", testModel.State);
+        Assert.AreEqual("PropChgn(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=\r\nPropChg(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=Got focus\r\n", DebugLog);
+    }
 
-        [TestMethod]
-        public void KeyDownCommandTest()
-        {
-            testModel.KeyDownCommand.Execute(null);
-            Assert.AreEqual("TextChanged()", testModel.State);
-            Assert.AreEqual("PropChgn(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=\r\nPropChg(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=TextChanged()\r\n", DebugLog);
-        }
+    [TestMethod]
+    public void KeyDownCommandTest()
+    {
+        testModel.KeyDownCommand.Execute(null);
+        Assert.AreEqual("TextChanged()", testModel.State);
+        Assert.AreEqual("PropChgn(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=\r\nPropChg(MVVM_34a_CTBindingEventArgs.ViewModels.EventsBindingViewModel,State)=TextChanged()\r\n", DebugLog);
+    }
 
-        [TestMethod]
-        [DataRow(null, false)]
-        [DataRow(Key.None, false)]
-        [DataRow(Key.Tab, false)]
-        [DataRow(Key.Enter, true)]
-        [DataRow(Key.Escape, true)]
-        public void CanKeyDownCommandTest(Key? key, bool xExp)
-        {
-            Assert.AreEqual(xExp, testModel.KeyDownCommand.CanExecute(key is Key k ? GetKeyEventArg(k) : null));
-            Assert.AreEqual("", testModel.State);
+    [TestMethod]
+    [DataRow(null, false)]
+    [DataRow(Key.None, false)]
+    [DataRow(Key.Tab, false)]
+    [DataRow(Key.Enter, true)]
+    [DataRow(Key.Escape, true)]
+    public void CanKeyDownCommandTest(Key? key, bool xExp)
+    {
+        Assert.AreEqual(xExp, testModel.KeyDownCommand.CanExecute(key is Key k ? GetKeyEventArg(k) : null));
+        Assert.AreEqual("", testModel.State);
 
-            static KeyEventArgs GetKeyEventArg(Key key)
+        static KeyEventArgs GetKeyEventArg(Key key)
+        {
+            KeyEventArgs? ke = null;
+            Func<Key, KeyEventArgs> _Action = (k) => new KeyEventArgs(Keyboard.PrimaryDevice,
+        new HwndSource(0, 0, 0, 0, 0, "", IntPtr.Zero),
+        0, k)
             {
-                KeyEventArgs? ke = null;
-                Func<Key, KeyEventArgs> _Action = (k) => new KeyEventArgs(Keyboard.PrimaryDevice,
-            new HwndSource(0, 0, 0, 0, 0, "", IntPtr.Zero),
-            0, k)
-                {
-                    RoutedEvent = Keyboard.KeyDownEvent
-                };
+                RoutedEvent = Keyboard.KeyDownEvent
+            };
 #if NET5_0_OR_GREATER
-                var t = new Thread(()=>ke=_Action(key));
-                t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
-                t.Start();
+            var t = new Thread(()=>ke=_Action(key));
+            t.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+            t.Start();
+            Thread.Sleep(10);
+            t.Join();
+            while (ke == null);
                 Thread.Sleep(10);
-                t.Join();
-                while (ke == null);
-                    Thread.Sleep(10);
 #else
-                ke=_Action(key);
+            ke=_Action(key);
 #endif
-                return ke;
-            }
+            return ke;
         }
     }
 }
