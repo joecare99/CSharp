b29ab8d8e77390f725282fffee80f6ce43c6bb6d
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:49 +0100

    MVVM_24c_CTUserControl

--- CSharpBible/MVVM_Tutorial/MVVM_24c_CTUserControl/Views/Converters/BoolToBrushConverter.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_24c_CTUserControl/Views/Converters/BoolToBrushConverter.cs
@@ -4,14 +4,14 @@ using System.Windows.Data;
 using System.Windows.Media;
 
 
-namespace MVVM_24c_CTUserControl.Views.Converters
+namespace MVVM_24c_CTUserControl.Views.Converters;
+
+public class BoolToBrushConverter : IValueConverter
 {
-    public class BoolToBrushConverter : IValueConverter
-    {
 		public	Brush TrueBrush { get; set; } = Brushes.GreenYellow;
 		public	Brush FalseBrush { get; set; } = Brushes.Maroon;
 		public	Brush DefaultBrush { get; set; } = Brushes.DarkGray;
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
 			 return (value, parameter) switch
 			{
@@ -23,8 +23,7 @@ namespace MVVM_24c_CTUserControl.Views.Converters
 
 		//  Visible/Collapsed --> bool
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+    {
+        throw new NotImplementedException();
     }
 }
