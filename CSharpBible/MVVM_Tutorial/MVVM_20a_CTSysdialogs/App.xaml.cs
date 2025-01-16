8dc7a1f59c19d17f152969b2fbc1f37dd611e016
Joe Care <Joe_care_99@yahoo.com>
2025-01-16 14:53:39 +0100

    MVVM_20a_CTSysdialogs

--- CSharpBible/MVVM_Tutorial/MVVM_20a_CTSysdialogs/App.xaml.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_20a_CTSysdialogs/App.xaml.cs
@@ -13,12 +13,11 @@
 // ***********************************************************************
 using System.Windows;
 
-namespace MVVM_20_Sysdialogs
+namespace MVVM_20_Sysdialogs;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
 }
--- CSharpBible/MVVM_Tutorial/MVVM_20a_CTSysdialogs/Converter/FontConverter.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_20a_CTSysdialogs/Converter/FontConverter.cs
@@ -2,20 +2,19 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace MVVM_20_Sysdialogs.Converter
-{
-    public class FontConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value switch
-            {
-                System.Drawing.Font f => new System.Windows.Media.FontFamily(f.Name),
-                _ => null
-            } ;
-        }
+namespace MVVM_20_Sysdialogs.Converter;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
-            => throw new NotImplementedException();
+public class FontConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            System.Drawing.Font f => new System.Windows.Media.FontFamily(f.Name),
+            _ => null
+        } ;
     }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+        => throw new NotImplementedException();
 }
