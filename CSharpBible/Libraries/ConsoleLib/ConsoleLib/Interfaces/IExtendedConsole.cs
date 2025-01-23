afee300a119798005febfcc094222bb987499d9c
Joe Care <Joe_care_99@yahoo.com>
2025-01-23 01:28:59 +0100

    AboutEx

---
+++ CSharpBible/Libraries/ConsoleLib/ConsoleLib/Interfaces/IExtendedConsole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleDisplay.View;
+
+namespace ConsoleLib.Interfaces;
+
+public interface IExtendedConsole : IConsole
+{
+}
