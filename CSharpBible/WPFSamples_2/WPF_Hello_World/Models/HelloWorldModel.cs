ccf935df4dfad23156888490fa8dbee3807d7097
Joe Care <Joe_care_99@yahoo.com>
2025-01-28 19:50:08 +0100

    AboutEx

---
+++ CSharpBible/WPFSamples_2/WPF_Hello_World/Models/HelloWorldModel.cs
@@ -0,0 +1,56 @@
+﻿using BaseLib.Models.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using WPF_Hello_World.Models.Interfaces;
+
+namespace WPF_Hello_World.Models;
+
+public partial class HelloWorldModel : BaseViewModelCT, IHelloWorldModel
+{
+    private readonly ICyclTimer timer;
+
+    [ObservableProperty]
+    private EGreeting _greeting = EGreeting.None;
+
+    public HelloWorldModel(ICyclTimer timer)
+    {
+        this.timer = timer;
+        timer.Start();
+        timer.Interval = 3000;
+        timer.Elapsed += _Timer_Elapsed;
+        timer.Start();
+    }
+
+    private void _Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        (Greeting, timer.Interval) = Greeting switch
+        {
+            EGreeting.None => (EGreeting.Greeting, 10000),
+            EGreeting.Greeting => (EGreeting.NiceDay, 5000),
+            _ => (EGreeting.NiceDay, 5000)
+        };
+        if (Greeting == EGreeting.NiceDay)
+            timer.Stop();
+    }
+
+    [RelayCommand]
+    private void Closing()
+    {
+        timer.Stop();
+        Greeting = EGreeting.Goodbye;
+        for (int i = 0; i < 10; i++)
+        {
+            Thread.Sleep(100);
+        }
+    }
+
+}
---
+++ CSharpBible/WPFSamples_2/WPF_Hello_World/Models/Interfaces/EGreeting.cs
@@ -0,0 +1,9 @@
+﻿namespace WPF_Hello_World.Models.Interfaces;
+
+public enum EGreeting
+{
+    None = 0,
+    Greeting = 1,
+    NiceDay = 2,
+    Goodbye = 3,
+}
