61d27cf0e7beaf3fbe29e4ada0e8f7d6069b2dd0
Joe Care <Joe_care_99@yahoo.com>
2025-01-30 20:18:36 +0100

    CanvasWPF2_CTItemTemplateSelector

--- CSharpBible/Graphics/CanvasWPF2_CTItemTemplateSelector/ViewModels/MainWindowViewmodel.cs
+++ CSharpBible/Graphics/CanvasWPF2_CTItemTemplateSelector/ViewModels/MainWindowViewmodel.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using MVVM.ViewModel;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Threading;
-using BaseLib.Helper.MVVM;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using BaseLib.Interfaces;
 
 namespace CanvasWPF2_CTItemTemplateSelector.ViewModel
 {
-	/// <summary>
-	/// Class MainWindowViewModel.
-	/// Implements the <see cref="BaseViewModel" />
-	/// </summary>
-	/// <seealso cref="BaseViewModel" />
-	public partial class MainWindowViewModel : BaseViewModelCT
+    /// <summary>
+    /// Class MainWindowViewModel.
+    /// Implements the <see cref="BaseViewModel" />
+    /// </summary>
+    /// <seealso cref="BaseViewModel" />
+    public partial class MainWindowViewModel : BaseViewModelCT
 	{
         #region Properties
         #region private Propeties
