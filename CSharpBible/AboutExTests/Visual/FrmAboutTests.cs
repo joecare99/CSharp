c7dac07cab867e3d46fe6c7f584182802181164a
Joe Care <Joe_care_99@yahoo.com>
2025-01-30 20:17:59 +0100

    AboutExTests

--- CSharpBible/AboutExTests/Visual/FrmAboutTests.cs
+++ CSharpBible/AboutExTests/Visual/FrmAboutTests.cs
@@ -13,7 +13,6 @@
 // ***********************************************************************
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
-using System.Windows.Forms;
 
 namespace CSharpBible.AboutEx.Visual.Tests
 {
