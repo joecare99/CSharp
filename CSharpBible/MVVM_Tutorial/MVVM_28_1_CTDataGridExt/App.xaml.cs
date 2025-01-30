612e1065bc830a5b1c150fba9f8bfe0cc9be101e
Joe Care <Joe_care_99@yahoo.com>
2025-01-30 20:16:58 +0100

    MVVM_28_1_CTDataGridExt

--- CSharpBible/MVVM_Tutorial/MVVM_28_1_CTDataGridExt/App.xaml.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_28_1_CTDataGridExt/App.xaml.cs
@@ -14,7 +14,6 @@
 using System.Windows;
 using Microsoft.Extensions.DependencyInjection;
 using BaseLib.Interfaces;
-using BaseLib.Helper;
 using MVVM.View.Extension;
 using MVVM_28_1_CTDataGridExt.Services;
 
