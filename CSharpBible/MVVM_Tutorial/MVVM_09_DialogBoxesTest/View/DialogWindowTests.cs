b6633f5be25f976c4b280fffd938e45c62b6791e
Joe Care <Joe_care_99@yahoo.com>
2025-01-30 20:16:44 +0100

    MVVM_09_DialogBoxesTest

--- CSharpBible/MVVM_Tutorial/MVVM_09_DialogBoxesTest/View/DialogWindowTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_09_DialogBoxesTest/View/DialogWindowTests.cs
@@ -16,7 +16,6 @@ using MVVM.ViewModel;
 using MVVM_09_DialogBoxes.ViewModels;
 using System.ComponentModel;
 using System.Threading;
-using System.Windows;
 
 /// <summary>
 /// The Tests namespace.
