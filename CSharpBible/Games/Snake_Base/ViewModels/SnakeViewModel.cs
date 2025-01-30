d0382ec07749c7ebf4b388bd9658987203133651
Joe Care <Joe_care_99@yahoo.com>
2025-01-30 20:18:29 +0100

    Snake_Base

--- CSharpBible/Games/Snake_Base/ViewModels/SnakeViewModel.cs
+++ CSharpBible/Games/Snake_Base/ViewModels/SnakeViewModel.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BaseLib.Helper;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Snake_Base.Models;
 using Snake_Base.Models.Data;
