3bf09b643718a5011baea6fb2aa37f73263f28ca
Joe Care <Joe_care_99@yahoo.com>
2025-02-01 22:48:38 +0100

    CSFreeVision

--- CSharpBible/Libraries/CSFreeVision/Properties/AssemblyInfo.cs
+++ CSharpBible/Libraries/CSFreeVision/Properties/AssemblyInfo.cs
@@ -13,16 +13,6 @@
 // ***********************************************************************
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("CSFreeVision")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("JC-Soft")]
-[assembly: AssemblyProduct("CSFreeVision")]
-[assembly: AssemblyCopyright("Copyright © JC-Soft 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,16 +23,3 @@ using System.Runtime.InteropServices;
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("05bbd8c1-c970-4b4b-ae79-892d25441e0c")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// indem Sie "*" wie unten gezeigt eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
