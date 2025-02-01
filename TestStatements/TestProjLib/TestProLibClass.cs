ba87ab7c12d092d8fec8cbd2e4fa3a458f748354
Joe Care <Joe_care_99@yahoo.com>
2025-02-01 09:47:53 +0100

    AppWithPlugin

---
+++ TestStatements/TestProjLib/TestProLibClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjLib
+{
+    public static class TestProLibClass
+    {
+        public static string GetTestString()
+        {
+            return "Test String from TestProjLib";
+        }
+    }
+}
