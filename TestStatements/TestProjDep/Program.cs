c865183015a99bb2b57a0cfeee12a63692db446d
Joe Care <Joe_care_99@yahoo.com>
2025-02-01 14:20:25 +0100

    TestProjDep

--- TestStatements/TestProjDep/Program.cs
+++ TestStatements/TestProjDep/Program.cs
@@ -1,3 +1,14 @@
 ﻿using System;
+using System.Windows.Forms;
+using TestProjDep.Views;
 
-Console.WriteLine(TestProjLib.TestProLibClass.GetTestString());
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1());
+    }
+}
