ed39e31bedaaf6fb2e26f9ad77bc992f83928e82
Joe Care <Joe_care_99@yahoo.com>
2025-02-09 22:52:26 +0100

    AboutEx

---
+++ CSharpBible/WPFSamples_2/WPF_Geometry/ViewModels/SampleViewerViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Geometry.ViewModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry.ViewModels;
+
+public partial class SampleViewerViewModel : ObservableObject, ISampleViewerViewModel
+{
+    private readonly List<object> _examples = new();
+    private int _sampleIndex;
+    public SampleViewerViewModel()
+    {
+    }
+
+    [ObservableProperty]
+    private object _actView;
+
+    public event EventHandler ZoomOut;
+
+    [RelayCommand]
+    private void SampleSelected(object index)
+    {
+        _sampleIndex = int.Parse($"0{index}"); 
+        ZoomOut?.Invoke(this, EventArgs.Empty);
+    }
+
+    [RelayCommand]
+    private void ZoomOutComplete()
+    {
+        ActView = _examples[_sampleIndex];
+    }
+
+    public void AddView(object geometryUsageExample)
+    {
+        _examples.Add(geometryUsageExample);
+    }
+}
