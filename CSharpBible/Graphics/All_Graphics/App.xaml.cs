d613725ad1e671e7e79523c49a6f1ab98183d808
Joe Care <Joe_care_99@yahoo.com>
2025-02-09 14:32:44 +0100

    All_Graphics

--- CSharpBible/Graphics/All_Graphics/App.xaml.cs
+++ CSharpBible/Graphics/All_Graphics/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using BaseLib.Models;
+using BaseLib.Models.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
 
 namespace All_Graphics
 {
@@ -7,6 +10,17 @@ namespace All_Graphics
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var sc = new ServiceCollection()
+                .AddSingleton<ITemplateModel, TemplateModel>()
+                .AddTransient<ICyclTimer, TimerProxy>()
+                .AddTransient<ISysTime, SysTime>();
+            var sp = sc.BuildServiceProvider();
+
+            IoC.Configure(sp);
+            base.OnStartup(e);
+        }
     }
 
 }
