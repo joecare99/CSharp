613529f9ac95f336127a781104c5d3345e87912f
Joe Care <Joe_care_99@yahoo.com>
2025-03-06 13:46:34 +0100

    WPF_Brushes

--- CSharpBible/WPFSamples_2/WPF_Brushes/Properties/AssemblyInfo.cs
+++ CSharpBible/WPFSamples_2/WPF_Brushes/Properties/AssemblyInfo.cs
@@ -9,13 +9,6 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Brushes")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Brushes")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -54,6 +47,3 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
