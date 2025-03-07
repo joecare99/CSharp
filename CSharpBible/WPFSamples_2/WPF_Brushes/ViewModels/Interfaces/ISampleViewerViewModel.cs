031d1761e11f4a3e9cbd2298257f781060b2b7a2
Joe Care <Joe_care_99@yahoo.com>
2025-03-07 14:38:21 +0100

    AboutEx

---
+++ CSharpBible/WPFSamples_2/WPF_Brushes/ViewModels/Interfaces/ISampleViewerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.ComponentModel;
+
+namespace Brushes.ViewModels.Interfaces;
+
+public interface ISampleViewerViewModel : INotifyPropertyChanged
+{
+    string NavigationSource { get; }
+
+    IRelayCommand<object> SampleSelectedCommand { get; }
+    IRelayCommand TransitionCommand { get; }
+    IRelayCommand<object> SelectedItemChangedCommand { get; }
+    IRelayCommand ExitCommand { get; }
+
+    event EventHandler ZoomOut;
+    event EventHandler DoExit;
+}
---
+++ CSharpBible/WPFSamples_2/WPF_Brushes/ViewModels/SampleViewerViewModel.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Brushes.ViewModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows;
+
+namespace Brushes.ViewModels;
+
+public partial class SampleViewerViewModel : ObservableObject, ISampleViewerViewModel
+{
+    private int _sampleIndex;
+    public SampleViewerViewModel()
+    {
+    }
+
+    [ObservableProperty]
+    private string _navigationSource = "BrushTypesExample.xaml";
+
+    private string _preNavigationSource;
+
+    public event EventHandler ZoomOut;
+
+    public event EventHandler DoExit;
+
+    [RelayCommand]
+    private void SampleSelected(object index)
+    {
+        _sampleIndex = int.Parse($"0{index}"); 
+        ZoomOut?.Invoke(this, EventArgs.Empty);
+    }
+
+    [RelayCommand]
+    private void SelectedItemChanged(object index)
+    {
+        var Sample = index as RoutedPropertyChangedEventArgs<object>;
+        var _value = Sample?.NewValue as XmlElement; 
+        var sampleuri = _value?.Attributes["Uri"];
+        _preNavigationSource =  sampleuri?.Value;
+    }
+
+    [RelayCommand]
+    private void Exit()
+    {
+        DoExit?.Invoke(this,EventArgs.Empty);
+    }
+
+    [RelayCommand]
+    private void Transition()
+    {
+        NavigationSource = _preNavigationSource;
+    }
+
+}
