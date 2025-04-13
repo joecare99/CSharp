7abc87d7a868dc7dbb00927677dc0e4a494c0d5c
Joe Care <Joe_care_99@yahoo.com>
2025-04-13 10:11:57 +0200

    CanvasWPF2_CTItemTemplateSelector

--- CSharpBible/Graphics/CanvasWPF2_CTItemTemplateSelector/ViewModels/MainWindowViewmodel.cs
+++ CSharpBible/Graphics/CanvasWPF2_CTItemTemplateSelector/ViewModels/MainWindowViewmodel.cs
@@ -5,7 +5,6 @@ using System.Collections.ObjectModel;
 using System.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using BaseLib.Interfaces;
 using BaseLib.Models.Interfaces;
 using BaseLib.Models;
 
