4608fa3be5e91c44cc0f61021579368018066e84
Joe Care <Joe_care_99@yahoo.com>
2025-04-13 10:12:59 +0200

    MVVM_00_IoCTemplateTests

--- CSharpBible/MVVM_Tutorial/MVVM_00_IoCTemplateTests/AppTests.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_00_IoCTemplateTests/AppTests.cs
@@ -1,5 +1,4 @@
-﻿using BaseLib.Interfaces;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BaseLib.Helper;
 using MVVM_00_IoCTemplate.Models;
 using NSubstitute;
