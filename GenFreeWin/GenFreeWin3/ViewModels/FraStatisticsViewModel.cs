450efea0c0896c8beaf554bee9442df90e417178
Joe Care <Joe_care_99@yahoo.com>
2025-05-31 13:41:10 +0200

    DAO

---
+++ GenFreeWin/GenFreeWin3/ViewModels/FraStatisticsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Gen_FreeWin;
+using GenFree.Interfaces.UI;
+using GenFree.ViewModels.Interfaces;
+
+namespace GenFreeWpf.ViewModels;
+
+public partial class FraStatisticsViewModel : ObservableObject, IFraStatisticsViewModel
+{
+    private readonly IApplUserTexts _userTexts;
+
+    public FraStatisticsViewModel(IApplUserTexts userTexts)
+    {
+        _userTexts = userTexts;
+        UpdateStat();
+    }
+
+    public string PersonsHeader => _userTexts[EUserText.t84_Persons];
+    public string FamiliesHeader => _userTexts[EUserText.t83_Families];
+    public string PlacesHeader => _userTexts[EUserText.t99_Places];
+    public string DatesHeader => _userTexts[EUserText.t100_Dates];
+    public string TextsHeader => _userTexts[EUserText.t101_Texts];
+
+    [ObservableProperty]
+    private int persons;
+
+    [ObservableProperty]
+    private int families;
+
+    [ObservableProperty]
+    private int places;
+
+    [ObservableProperty]
+    private int dates;
+
+    [ObservableProperty]
+    private int texts;
+
+    public void UpdateStat()
+    {
+        // Hier sollten die echten Werte aus dem Model geladen werden
+        Persons = 0;
+        Families = 0;
+        Places = 0;
+        Dates = 0;
+        Texts = 0;
+    }
+}
---
+++ GenFreeWin/GenFreeWin3/ViewModels/RepoPageViewModel.cs
@@ -0,0 +1,82 @@
+﻿using MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GenFree.ViewModels.Interfaces;
+
+namespace GenFreeWpf.ViewModels;
+
+public partial class RepoPageViewModel : BaseViewModelCT, IRepoViewModel
+{
+    [ObservableProperty]
+    private string repoName_Text;
+
+    [ObservableProperty]
+    private string textBox2_Text;
+
+    [ObservableProperty]
+    private string textBox3_Text;
+
+    [ObservableProperty]
+    private string textBox4_Text;
+
+    [ObservableProperty]
+    private string textBox5_Text;
+
+    [ObservableProperty]
+    private string textBox6_Text;
+
+    [ObservableProperty]
+    private string richTextBox1_Text;
+
+    [ObservableProperty]
+    private string richTextBox2_Text;
+
+    [ObservableProperty]
+    private Action doClose;
+
+    public bool BtnDeleteVisible => true;
+
+    public int SourceCount => 0;
+
+    public float FontSize => 12.0f;
+
+    public object HintFarb => default!;
+
+    public IRelayCommand FormLoadCommand { get; }
+    public IRelayCommand LinkClickCommand { get; }
+    public IRelayCommand SaveCommand { get; }
+    public IRelayCommand Save2Command { get; }
+    public IRelayCommand CloseCommand { get; }
+    public IRelayCommand NewEntryCommand { get; }
+    public IRelayCommand DeleteCommand { get; }
+    public IRelayCommand Sources_DblCommand { get; }
+    public IRelayCommand List2DblCommand { get; }
+
+    public RepoPageViewModel()
+    {
+        FormLoadCommand = new RelayCommand(OnFormLoad);
+        LinkClickCommand = new RelayCommand(OnLinkClick);
+        SaveCommand = new RelayCommand(OnSave);
+        Save2Command = new RelayCommand(OnSave2);
+        CloseCommand = new RelayCommand(OnClose);
+        NewEntryCommand = new RelayCommand(OnNewEntry);
+        DeleteCommand = new RelayCommand(OnDelete);
+        Sources_DblCommand = new RelayCommand(OnList1Dbl);
+        List2DblCommand = new RelayCommand(OnList2Dbl);
+    }
+
+    private void OnFormLoad() { }
+    private void OnLinkClick() { }
+    private void OnSave() { }
+    private void OnSave2() { }
+    private void OnClose() => DoClose?.Invoke();
+    private void OnNewEntry() { }
+    private void OnDelete() { }
+    private void OnList1Dbl() { }
+    private void OnList2Dbl() { }
+}
