3cef55d1c89b77609a23e6515e8bf47b3e25315b
Joe Care <Joe_care_99@yahoo.com>
2025-06-09 12:40:53 +0200

    GenFreeBaseTests

--- GenFreeWin/GenFreeBaseTests/Interfaces/DB/IFieldsCollectionTests.cs
+++ GenFreeWin/GenFreeBaseTests/Interfaces/DB/IFieldsCollectionTests.cs
@@ -18,21 +18,16 @@ public class IFieldsCollectionTests
     {
         // Arrange
         var fieldsCollection = Substitute.For<IFieldsCollection>();
-        fieldsCollection["TestField"].Value.Returns(1,2,3);
         (fieldsCollection["TestField"] as IHasValue).Value.Returns(1,2,3);
-        fieldsCollection[2].Value.Returns(1.1,2.2,3.3);
         (fieldsCollection[2] as IHasValue).Value.Returns(1.1,2.2,3.3);
-        fieldsCollection[TypeCode.Double].Value.Returns("1","2","3");
         (fieldsCollection[TypeCode.Double] as IHasValue).Value.Returns("1","2","3");
         // Act
         var result1 = fieldsCollection[TypeCode.Double].AsString();
         var result2 = fieldsCollection[2].AsDouble();
         var result3 = fieldsCollection["TestField"].AsInt();
-        var result1a = fieldsCollection[TypeCode.Double].Value;
-        var result2a = fieldsCollection[2].Value;
-        var result3a = fieldsCollection["TestField"].Value;
         // Assert
-        Assert.AreEqual("", result1);
-        Assert.AreEqual("1", result1a);
+        Assert.AreEqual("1", result1);
+        Assert.AreEqual(1.1, result2);
+        Assert.AreEqual(1, result3);
     }
 }
