e06238c8dd5bea9a6179b7b7d2f3fa2ecae3471b
Joe Care <Joe_care_99@yahoo.com>
2025-06-09 12:42:16 +0200

    DAO

---
+++ GenFreeWin/GenFreeBase/Interfaces/DB/IIndexDef.cs
@@ -0,0 +1,11 @@
+﻿//using DAO;
+namespace GenFree.Interfaces.DB
+{
+    public interface IIndexDef
+    {
+        string[]? Fields { get; set; }
+        bool IgnoreNulls { get; set; }
+        string? Name { get; set; }
+        bool Unique { get; set; }
+    }
+}
---
+++ GenFreeWin/GenFreeBase/Interfaces/DB/ITableDef.cs
@@ -0,0 +1,13 @@
+﻿//using DAO;
+using GenFree.Data;
+using System.Collections.Generic;
+
+namespace GenFree.Interfaces.DB
+{
+    public interface ITableDef
+    {
+        List<IFieldDef> Fields { get; }
+        List<IIndexDef> Indexes { get; }
+        string? Name { get; set; }
+    }
+}
