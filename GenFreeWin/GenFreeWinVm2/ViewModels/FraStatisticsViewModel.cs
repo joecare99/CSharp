de9906e0e297844e6dd4bcccb33f95e9048b1f74
Joe Care <Joe_care_99@yahoo.com>
2025-06-20 19:31:30 +0200

    GenFreeWinVm2

--- GenFreeWin/GenFreeWinVm2/ViewModels/FraStatisticsViewModel.cs
+++ GenFreeWin/GenFreeWinVm2/ViewModels/FraStatisticsViewModel.cs
@@ -9,17 +9,30 @@ public partial class FraStatisticsViewModel : BaseViewModelCT, IFraStatisticsVie
 {
     public int Persons => 1;
 
-    public int Families => 2;
-
-    public int Places =>3;
-
-    public int Dates => 4;
+    [ObservableProperty]
+    public partial int Families { get; set; } = 2;
 
     [ObservableProperty]
-    public int _texts = 5;
+    public partial int Places { get; set; } = 3;
+
+    [ObservableProperty]
+    public partial int Dates { get; set; } = 4;
+
+    [ObservableProperty]
+    public partial int Texts { get; set; } = 5;
 
     public void UpdateStat()
     {
         Texts++;
+    }
+
+    public void SetDates(int count)
+    {
+        Dates = count;
+    }
+
+    public void SetTexts(int Count)
+    {
+        Texts = Count;
     }
 }
