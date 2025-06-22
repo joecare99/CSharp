1ab5231e99db77924798733a5977843545beb66b
Joe Care <Joe_care_99@yahoo.com>
2025-06-22 02:20:32 +0200

    GenFreeHelper

--- GenFreeWin/GenFreeHelper/Helper/DBHelper.cs
+++ GenFreeWin/GenFreeHelper/Helper/DBHelper.cs
@@ -1,28 +1,51 @@
-﻿using GenFree.Data;
+﻿using BaseLib.Helper;
+using GenFree.Data;
+using GenFree.Interfaces.DB;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.OleDb;
 using System.Linq;
-using GenFree.Interfaces.DB;
-using BaseLib.Helper;
 
 namespace GenFree.Helper;
 public static class DBHelper
 {
     public static IEnumerable<ITableDef> TableDefs(this IDatabase db)
     {
+        var dbDataTypes = db.GetDataTypes();
         var enumerator = db.GetSchema("Tables").Rows;
         foreach (DataRow row in enumerator)
         {
             TableDef td = new(db.Connection, row["TABLE_NAME"].AsString());
             db.GetSchema("Columns", new[] { null, null, td.Name }).Rows
                 .Cast<DataRow>().Select(
-                r => new FieldDef(td, r["COLUMN_NAME"].AsString(), r["DATA_TYPE"].AsString(), r["CHARACTER_MAXIMUM_LENGTH"].AsInt())).ToList().ForEach(r => { });
+                r => new FieldDef(td, r["COLUMN_NAME"].AsString(),dbDataTypes.GetTypeCode(r["DATA_TYPE"].AsInt()), r["CHARACTER_MAXIMUM_LENGTH"].AsInt())).ToList().ForEach(r => { });
             db.GetSchema("Indexes").Rows
                 .Cast<DataRow>().Where(r => r["TABLE_NAME"].AsString() == td.Name)?.Select(
                 r => new IndexDef(td, r["INDEX_NAME"].AsString(), r["COLUMN_NAME"].AsString(), r["PRIMARY_KEY"].AsBool(), r["UNIQUE"].AsBool())).ToList().ForEach(r => { /*if (r.Name == null) td.Indexes.Add(r);*/ });
             yield return td;
         }
+    }
+
+    public static IDictionary<int,(string name,TypeCode tc)> GetDataTypes(this IDatabase db)
+    {
+        Dictionary<int, (string name, TypeCode tc)> result = [];
+        var datatypes = db.GetSchema("DataTypes");
+        result[130] = ("VarChar", TypeCode.String);
+        // 11
+        foreach (DataRow datatype in datatypes.Rows)
+        {
+            result[datatype[1].AsInt()]=(datatype[0].AsString(),Type.GetTypeCode(Type.GetType(datatype[5].AsString(),false,true)));
+        }
+        return result;
+    }
+
+    public static TypeCode GetTypeCode(this IDictionary<int, (string name, TypeCode tc)> dbTypes, int iType)
+    {
+        if (dbTypes.TryGetValue(iType, out var rslt))
+            return rslt.tc;
+        else
+            return TypeCode.Empty;
     }
 
     public static bool DbFieldExists(this IDatabase TabDef, Enum field, string sFieldName)
