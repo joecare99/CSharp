1610e7864825f93cbd2b8422e12c1346385c945f
Joe Care <Joe_care_99@yahoo.com>
2025-07-12 13:04:12 +0200

    DAO

---
+++ GenFreeWin/GenFreeUIItfs/ViewModels/Interfaces/IFamilieViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace GenFree.ViewModels.Interfaces;
+
+public interface IFamilieViewModel: INotifyPropertyChanged
+{
+    IContainerControl View { get; set; }
+    int iFamNr { get; set; }
+    IPersonRedViewModel Father { get; }
+    IPersonRedViewModel Mother { get; }
+
+    void Fameinlesen(int famInArb, out short rich);
+    void Familie_FormClosed(object sender, FormClosedEventArgs e);
+    void Familie_Load(object sender, EventArgs e);
+}
