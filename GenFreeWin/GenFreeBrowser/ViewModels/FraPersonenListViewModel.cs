60e1d66947c129cdbe4b419b712d675d785fab1d
Joe Care <Joe_care_99@yahoo.com>
2025-07-16 07:23:54 +0200

    DAO

---
+++ GenFreeWin/GenFreeBrowser/ViewModels/FraPersonenListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenFreeBrowser.ViewModels;
+
+public partial class FraPersonenListViewModel:BaseViewModelCT
+{
+    [ObservableProperty]
+    public partial ObservableCollection<DispPersones> Personen { get; set; }
+}
---
+++ GenFreeWin/GenFreeBrowser/Views/FraPersonenList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenFreeBrowser.Views
+{
+    /// <summary>
+    /// Interaktionslogik für FraPersonenList.xaml
+    /// </summary>
+    public partial class FraPersonenList : UserControl
+    {
+        public FraPersonenList()
+        {
+            InitializeComponent();
+        }
+    }
+}
