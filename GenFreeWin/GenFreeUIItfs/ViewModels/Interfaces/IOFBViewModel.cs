26e47c7588e51b59ec51f5b1e82bee07b2d86a63
Joe Care <Joe_care_99@yahoo.com>
2025-07-19 08:32:56 +0200

    DAO

---
+++ GenFreeWin/GenFreeUIItfs/ViewModels/Interfaces/IOFBViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Gen_FreeWin.Views;
+
+public interface IOFBViewModel: INotifyPropertyChanged
+{
+    IContainerControl View { get; set; }
+
+    void Command1_Click(object s, EventArgs e);
+    void List1_DoubleClick(object s, EventArgs e);
+    void List2_DoubleClick(object s, EventArgs e);
+    void List3_DoubleClick(object s, EventArgs e);
+    void List4_DoubleClick(object s, EventArgs e);
+    void List4_SelectedIndexChanged(object s, EventArgs e);
+    void List5_DoubleClick(object sender, EventArgs e);
+    void OFB_Load(object sender, EventArgs e);
+    void Text1_KeyUp(object s, KeyEventArgs e);
+    void Text1_TextChanged(object s, EventArgs e);
+    void Text2_KeyUp(object sender, KeyEventArgs e);
+    void Text2_TextChanged(object sender, EventArgs e);
+}
---
+++ GenFreeWin/GenFreeUIItfs/ViewModels/Interfaces/IOrtsVerViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Gen_FreeWin.Views;
+
+public interface IOrtsVerViewModel : INotifyPropertyChanged
+{
+    IContainerControl View { get; set; }
+    IRelayCommand NextCommand { get; }
+    IRelayCommand PrevCommand { get; }
+    IRelayCommand ShowPlaceGECommand { get; }
+    IRelayCommand ShowPlaceGMCommand { get; }
+    IRelayCommand LinkGOVCommand { get; }
+    IRelayCommand SearchGOVCommand { get; }
+    IRelayCommand ConvertKoordsCommand { get; }
+    IRelayCommand SearchNameCommand { get; }
+    IRelayCommand SearchNumberCommand { get; }
+
+    void Button10_Click(object s, EventArgs e);
+    void Button11_Click(object s, EventArgs e);
+    void Button12_Click(object s, EventArgs e);
+    void Button13_Click(object s, EventArgs e);
+    void Button14_Click(object s, EventArgs e);
+    void Button15_Click(object s, EventArgs e);
+    void Button16_Click(object s, EventArgs e);
+    void Button17_Click(object s, EventArgs e);
+    void Button18_Click(object s, EventArgs e);
+    void Button19_Click(object s, EventArgs e);
+    void Button20_Click(object s, EventArgs e);
+    void Button21_Click(object s, EventArgs e);
+    void Button22_Click(object s, EventArgs e);
+    void Button23_Click(object s, EventArgs e);
+    void Button24_Click(object s, EventArgs e);
+    void Form_Load(object sender, EventArgs e);
+    void Label27_TextChanged(object s, EventArgs e);
+    void Label32_TextChanged(object s, EventArgs e);
+    void ListBox1_DoubleClick(object sender, EventArgs e);
+    void ListBox2_DoubleClick(object s, EventArgs e);
+    void ListBox3_DoubleClick(object s, EventArgs e);
+    void ListBox4_DoubleClick(object s, EventArgs e);
+    void RTB1_KeyUp(object sender, KeyEventArgs e);
+    void TextBox13_TextChanged(object sender, EventArgs e);
+    void Textbox1_KeyPress(object eventSender, KeyPressEventArgs eventArgs);
+    void TextBox1_KeyUp(object sender, KeyEventArgs e);
+    void TextBox30_TextChanged(object s, EventArgs e);
+    void TextBox31_TextChanged(object s, EventArgs e);
+    void TextBox32_TextChanged(object s, EventArgs e);
+}
