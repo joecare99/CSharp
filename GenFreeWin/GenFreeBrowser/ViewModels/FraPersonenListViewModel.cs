54ac89acb5721f553a1aee7bf8a92616f21776e0
Joe Care <Joe_care_99@yahoo.com>
2025-08-10 23:50:01 +0200

    GenFreeBrowser

--- GenFreeWin/GenFreeBrowser/ViewModels/FraPersonenListViewModel.cs
+++ GenFreeWin/GenFreeBrowser/ViewModels/FraPersonenListViewModel.cs
@@ -4,10 +4,13 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using GenFreeBrowser.Model;
+using MVVM.ViewModel;
 
 namespace GenFreeBrowser.ViewModels;
 
-public partial class FraPersonenListViewModel:BaseViewModelCT
+public partial class FraPersonenListViewModel : BaseViewModelCT
 {
     [ObservableProperty]
     public partial ObservableCollection<DispPersones> Personen { get; set; }
