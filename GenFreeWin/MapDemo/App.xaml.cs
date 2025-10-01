46c169a9c57723361e565b2c1e0ac7b0ab118e52
Joe Care <Joe_care_99@yahoo.com>
2025-10-01 23:54:24 +0200

    MapDemo

--- GenFreeWin/MapDemo/App.xaml.cs
+++ GenFreeWin/MapDemo/App.xaml.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 using GenFreeBrowser.Map;
 using GenFreeBrowser.Map.DI;
 using GenFreeBrowser.Map.Interfaces;
+using GenFreeBrowser.Places;
+using GenFreeBrowser.Places.Interface;
 
 namespace MapDemo;
 
@@ -27,6 +29,13 @@ public partial class App : Application
         // Active provider state + tilesource factory
         sc.AddSingleton<IMapProvider>(bingareal); // default
         sc.AddSingleton<ITileSource>(sp => new HttpTileSource(sp.GetRequiredService<IMapProvider>(), sp.GetRequiredService<ITileCache>()));
+
+        // Place Authorities
+        sc.AddHttpClient<GeoNamesAuthority>();
+        sc.AddHttpClient<GovAuthority>();
+        sc.AddSingleton<IPlaceAuthority>(sp => new GeoNamesAuthority(sp.GetRequiredService<System.Net.Http.IHttpClientFactory>().CreateClient(nameof(GeoNamesAuthority)))); // TODO: username config
+        sc.AddSingleton<IPlaceAuthority>(sp => new GovAuthority(sp.GetRequiredService<System.Net.Http.IHttpClientFactory>().CreateClient(nameof(GovAuthority))));
+        sc.AddSingleton<IPlaceSearchService, PlaceSearchService>();
 
         sc.AddTransient<MainViewModel>();
         Services = sc.BuildServiceProvider();
