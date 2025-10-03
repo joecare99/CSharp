f93b2994ba2e228394d3592ea990b175945e7a44
Joe Care <Joe_care_99@yahoo.com>
2025-10-03 18:04:31 +0200

    AboutEx

---
+++ CSharpBible/SomeThing/Statistic_ausf3/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    enum Op
+    {
+        RndSecret = 1, ReadNumber = 2, CmpEq = 3, CmpLt = 4, BrTrue = 5,
+        PrintSentence = 6, PrintLabelAndNumber = 7, PrintRandomStatus = 8,
+        Progress = 9, Chart = 10, Jump = 11, Halt = 99
+    }
+
+    class VM
+    {
+        public int Secret, Input, PC;
+        public bool Result;
+        public Random Rnd = new Random();
+    }
+
+    static void Main()
+    {
+        // Fetzen (obfuskiert): erstes Element = Offset, danach Codepoint+Offset
+        var shards = new List<int[]>
+        {
+            new[]{300,365,410,397,408,415,401},                   // 0: "Analyse"
+            new[]{250,336,347,364,355,347,360,372},               // 1: "Varianz"
+            new[]{240,310,351,357,354,345,341,354},               // 2: "Fourier"
+            new[]{200,277,311,310,316,301},                       // 3: "Monte"
+            new[]{260,335,371,378,357,374,365,357,370,382},       // 4: "Kovarianz"
+            new[]{150,196,196,196},                               // 5: "..."
+            new[]{100,132},                                       // 6: " "
+            new[]{180,238,212},                                   // 7: ": "
+            new[]{200,297,298,303,301,315,299,304,308,311,315,315,301,310}, // 8: "abgeschlossen"
+            new[]{220,307,321,334,336},                           // 9: "Wert"
+            new[]{210,318,315,311,313,326},                       //10: "liegt"
+            new[]{190,275,268,274,259,272},                       //11: "UNTER"
+            new[]{300,520,366,369,382},                           //12: "ÜBER"
+            new[]{160,260,261,269},                               //13: "dem"
+            new[]{230,299,344,349,327,344,346,347,340,333,345,328,331,344,331,335,329,334}, //14: "Erwartungsbereich"
+            new[]{210,289,322,326,315,319,307,318,311},           //15: "Optimale"
+            new[]{250,326,496,365,367,360,353},                   //16: "Lösung"
+            new[]{200,303,301,302,317,310,300,301,310},           //17: "gefunden"
+            new[]{120,153},                                       //18: "!"
+            new[]{240,308,357,354,339,344,355,339,344,350,345,356,356,355,359,341,354,356}, //19: "Durchschnittswert"
+            new[]{190,265,301,304,304,291,298,287,306,295,301,300}, //20: "Korrelation"
+            new[]{50,60},                                         //21: "\n"
+            new[]{200,266,305,316,316,301},                       //22: "Bitte"
+            new[]{150,240,247,254,258},                           //23: "Zahl"
+            new[]{170,271,275,280,273,271,268,271,280},           //24: "eingeben"
+            new[]{140,186},                                       //25: "."
+        };
+
+        // Sätze als Reihen von Shard-Indizes (alle enden mit "\n" via Shard 21)
+        var sentences = new List<int[]>
+        {
+            new[]{0,5,21},                                        // 0: "Analyse...\n"
+            new[]{1,5,21},                                        // 1: "Varianz...\n"
+            new[]{2,5,21},                                        // 2: "Fourier...\n"
+            new[]{3,5,21},                                        // 3: "Monte...\n"
+            new[]{4,5,21},                                        // 4: "Kovarianz...\n"
+
+            new[]{22,6,23,6,24,7,21},                             // 5: "Bitte Zahl eingeben: \n"
+
+            new[]{0,6,8,7,9,6,10,6,11,6,13,6,14,25,21},           // 6: Unter-Meldung
+            new[]{0,6,8,7,9,6,10,6,12,6,13,6,14,25,21},           // 7: Über-Meldung
+            new[]{0,6,8,7,15,6,16,6,17,18,21},                    // 8: Erfolg-Meldung
+        };
+
+        // Labels für Zahlen/Stats (ohne Newline; dieser folgt nach Zahl)
+        var labels = new List<int[]>
+        {
+            new[]{19,7},                                          // 0: "Durchschnittswert: "
+            new[]{1,7},                                           // 1: "Varianz: "
+            new[]{20,7},                                          // 2: "Korrelation: "
+        };
+
+        var statusIdx = new int[] { 0, 1, 2, 3, 4 };
+
+        // Bytecode mit korrekten Offsets (Unter @28, Über @32, Erfolg @36)
+        int[] program =
+        {
+            (int)Op.RndSecret, 20,            // 0
+
+            // LOOP @2
+            (int)Op.PrintSentence, 5,         // 2: Prompt
+            (int)Op.ReadNumber, 0,            // 4: Eingabe
+
+            (int)Op.PrintRandomStatus, 0,     // 6
+            (int)Op.Progress, 20,             // 8
+            (int)Op.PrintLabelAndNumber, 0,   // 10
+            (int)Op.PrintLabelAndNumber, 1,   // 12
+            (int)Op.PrintLabelAndNumber, 2,   // 14
+            (int)Op.Chart, (6<<8)|10,         // 16
+
+            (int)Op.CmpEq, 0,                 // 18
+            (int)Op.BrTrue, 36,               // 20 -> Erfolg @36
+            (int)Op.CmpLt, 0,                 // 22
+            (int)Op.BrTrue, 28,               // 24 -> Unter @28
+            (int)Op.Jump, 32,                 // 26 -> Über @32
+
+            // Unter-Block @28
+            (int)Op.PrintSentence, 6,         // 28
+            (int)Op.Jump, 2,                  // 30 zurück zur LOOP
+
+            // Über-Block @32
+            (int)Op.PrintSentence, 7,         // 32
+            (int)Op.Jump, 2,                  // 34 zurück zur LOOP
+
+            // Erfolg @36
+            (int)Op.PrintSentence, 8,         // 36
+            (int)Op.Halt, 0                   // 38
+        };
+
+        Run(new VM(), program, shards, sentences, labels, statusIdx);
+    }
+
+    // Dekodiert und schreibt einen Satz (Liste von Shards)
+    static void WriteSentence(int[] sentence, List<int[]> shards)
+    {
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            var s = shards[sentence[i]];
+            int off = s[0];
+            for (int j = 1; j < s.Length; j++)
+                Console.Write((char)(s[j] - off));
+        }
+    }
+
+    static void Run(VM vm, int[] prog, List<int[]> shards, List<int[]> sentences, List<int[]> labels, int[] statusIdx)
+    {
+        while (vm.PC < prog.Length)
+        {
+            var op = (Op)prog[vm.PC++]; int arg = prog[vm.PC++];
+            switch (op)
+            {
+                case Op.RndSecret:
+                    vm.Secret = vm.Rnd.Next(1, arg + 1);
+                    break;
+
+                case Op.ReadNumber:
+                    {
+                        // Einlesen ohne String-Literale
+                        string s = Console.ReadLine();
+                        int x; vm.Input = int.TryParse(s, out x) ? x : int.MinValue;
+                        break;
+                    }
+
+                case Op.CmpEq: vm.Result = (vm.Input == vm.Secret); break;
+                case Op.CmpLt: vm.Result = (vm.Input < vm.Secret); break;
+                case Op.BrTrue: if (vm.Result) vm.PC = arg; break;
+                case Op.Jump: vm.PC = arg; break;
+
+                case Op.PrintSentence:
+                    WriteSentence(sentences[arg], shards);
+                    break;
+
+                case Op.PrintLabelAndNumber:
+                    {
+                        // Label ausgeben
+                        var lbl = labels[arg];
+                        for (int i = 0; i < lbl.Length; i++)
+                        {
+                            var s = shards[lbl[i]];
+                            int off = s[0];
+                            for (int j = 1; j < s.Length; j++)
+                                Console.Write((char)(s[j] - off));
+                        }
+
+                        // Zahl/Wert ohne Format-String
+                        if (arg == 0)
+                        {
+                            int v = vm.Rnd.Next(10, 100);
+                            Console.Write(v);
+                        }
+                        else if (arg == 1)
+                        {
+                            double v = Math.Round(vm.Rnd.NextDouble(), 3);
+                            Console.Write(v);
+                        }
+                        else
+                        {
+                            double v = Math.Round(vm.Rnd.NextDouble(), 2);
+                            Console.Write(v);
+                        }
+                        Console.Write((char)10); // Newline
+                        break;
+                    }
+
+                case Op.PrintRandomStatus:
+                    {
+                        int w = statusIdx[vm.Rnd.Next(statusIdx.Length)];
+                        WriteSentence(sentences[w], shards);
+                        break;
+                    }
+
+                case Op.Progress:
+                    RenderProgress(arg, 24);
+                    break;
+
+                case Op.Chart:
+                    {
+                        int min = (arg >> 8) & 0xFF;
+                        int max = arg & 0xFF;
+                        RenderChart(vm.Rnd, min, max);
+                        break;
+                    }
+
+                case Op.Halt:
+                    return;
+            }
+        }
+    }
+
+    // Fortschrittsbalken ohne String-Literale
+    static void RenderProgress(int width, int delayMs)
+    {
+        for (int i = 0; i <= width; i++)
+        {
+            Console.Write((char)91); // '['
+            for (int k = 0; k < i; k++) Console.Write((char)35);      // '#'
+            for (int k = 0; k < (width - i); k++) Console.Write((char)32); // ' '
+            Console.Write((char)93); // ']'
+            Console.Write((char)32); // ' '
+            int pct = (i * 100 / width);
+            Console.Write(pct);
+            Console.Write((char)37); // '%'
+            Console.Write((char)13); // '\r'
+            Thread.Sleep(delayMs);
+        }
+        Console.Write((char)10); // '\n'
+    }
+
+    // ASCII-Chart ohne String-Literale
+    static void RenderChart(Random rnd, int minBars, int maxBars)
+    {
+        int bars = rnd.Next(minBars, maxBars + 1);
+        for (int i = 0; i < bars; i++)
+        {
+            int h = rnd.Next(5, 20);
+            for (int k = 0; k < h; k++) Console.Write((char)124); // '|'
+            Console.Write((char)10);
+        }
+    }
+}
---
+++ CSharpBible/SomeThing/Statistik_Ausf2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "Stat-Analyzer v5.3";
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("=== Stat-Analyzer v5.3 ===");
+        Console.ResetColor();
+
+        var rnd = new Random();
+        int geheimzahl = rnd.Next(1, 21); // 1..20
+        bool erraten = false;
+
+        // "Analyse..." über 7-bit gepackte Distanzen (Bias 64), Start 'A' (65)
+        // Diffs: [45, -13, 11, 13, -6, -14, -55, 0, 0]
+        // Gepackte Konstante (korrekt berechnet):
+        // 4_647_756_131_583_842_797UL
+        string phraseAnalyse = DecodePackedDiffs(
+            packed: 4_647_756_131_583_842_797UL,
+            steps: 9,
+            startChar: 65,
+            bitWidth: 7,
+            bias: 64
+        );
+
+        // Korrekte Diff-Listen für die anderen Phrasen (Startwert = erstes Zeichen)
+        // "Varianz..."
+        string phraseVarianz = DecodeDiffs(86, new int[] { 11, 17, -9, -8, 13, 12, -76, 0, 0 });
+        // "Fourier..."
+        string phraseFourier = DecodeDiffs(70, new int[] { 41, 6, -3, -9, -4, 13, -68, 0, 0 });
+        // "Monte..."
+        string phraseMonte = DecodeDiffs(77, new int[] { 34, -1, 6, -15, -55, 0, 0 });
+        // "Kovarianz..."
+        string phraseKov = DecodeDiffs(75, new int[] { 36, 7, -21, 17, -9, -8, 13, 12, -76, 0, 0 });
+
+        var phrases = new List<string> { phraseAnalyse, phraseVarianz, phraseFourier, phraseMonte, phraseKov };
+
+        Console.WriteLine("Bitte geben Sie Zahlen ein, um die 'Analyse' zu starten (1–20).");
+
+        while (!erraten)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("\nEingabe: ");
+            Console.ResetColor();
+            string input = Console.ReadLine();
+
+            // Dynamische Tarn-Phrase
+            string status = phrases[rnd.Next(phrases.Count)];
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(status);
+            Console.ResetColor();
+
+            // Fortschrittsbalken
+            RenderProgressBar(20, 28);
+
+            // Pseudo-Kennzahlen
+            Console.WriteLine($"Durchschnittswert: {rnd.Next(10, 100)}");
+            Console.WriteLine($"Varianz: {rnd.NextDouble():F3}");
+            Console.WriteLine($"Korrelation: {rnd.NextDouble():F2}");
+
+            // ASCII-"Diagramm"
+            RenderAsciiChart(rnd);
+
+            // Spiel-Logik (in Tarnung)
+            if (int.TryParse(input, out int zahl))
+            {
+                if (zahl == geheimzahl)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Analyse abgeschlossen: Optimale Lösung gefunden!");
+                    Console.ResetColor();
+                    erraten = true;
+                }
+                else if (zahl < geheimzahl)
+                {
+                    Console.WriteLine("Analyse abgeschlossen: Wert liegt UNTER dem Erwartungsbereich.");
+                }
+                else
+                {
+                    Console.WriteLine("Analyse abgeschlossen: Wert liegt ÜBER dem Erwartungsbereich.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Analyse fehlgeschlagen: Eingabe konnte nicht verarbeitet werden.");
+            }
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\nVerifikation: Prozesse erfolgreich abgeschlossen.");
+        Console.ResetColor();
+    }
+
+    // Rekonstruiert einen String aus Distanzen (Startwert + Differenzen)
+    static string DecodeDiffs(int startChar, int[] diffs)
+    {
+        var chars = new List<char> { (char)startChar };
+        int current = startChar;
+        foreach (int d in diffs)
+        {
+            current += d;
+            chars.Add((char)current);
+        }
+        return new string(chars.ToArray());
+    }
+
+    // Rekonstruiert einen String aus einem 64-bit Wert mit festen Bitbreiten (z. B. 7 Bit) und Bias
+    static string DecodePackedDiffs(ulong packed, int steps, int startChar, int bitWidth, int bias)
+    {
+        var chars = new List<char> { (char)startChar };
+        int current = startChar;
+        for (int i = 0; i < steps; i++)
+        {
+            int mask = (1 << bitWidth) - 1;
+            int chunk = (int)((packed >> (i * bitWidth)) & (ulong)mask);
+            int diff = chunk - bias;
+            current += diff;
+            chars.Add((char)current);
+        }
+        return new string(chars.ToArray());
+    }
+
+    static void RenderProgressBar(int width, int delayMs)
+    {
+        for (int i = 0; i <= width; i++)
+        {
+            Console.Write("[" + new string('#', i) + new string(' ', width - i) + $"] {(i * 100 / width),3}%\r");
+            Thread.Sleep(delayMs);
+        }
+        Console.WriteLine();
+    }
+
+    static void RenderAsciiChart(Random rnd)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        int bars = rnd.Next(6, 10);
+        for (int i = 0; i < bars; i++)
+        {
+            int h = rnd.Next(5, 20);
+            Console.WriteLine(new string('|', h));
+        }
+        Console.ResetColor();
+    }
+}
