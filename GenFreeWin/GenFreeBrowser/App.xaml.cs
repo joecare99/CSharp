5dfd7de491bd950ef3ae23447124d9f46b4be6ef
Joe Care <Joe_care_99@yahoo.com>
2025-10-04 16:11:26 +0200

    GenFreeBrowser

--- GenFreeWin/GenFreeBrowser/App.xaml.cs
+++ GenFreeWin/GenFreeBrowser/App.xaml.cs
@@ -1,14 +1,51 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using GenFreeBrowser.ViewModels.Interfaces;
+using GenFreeBrowser.ViewModels;
+using GenFreeBrowser.Map.DI;
 
-namespace GenFreeBrowser
+namespace GenFreeBrowser;
+
+/// <summary>
+/// WPF App mit einfachem DI (ServiceCollection -> ServiceProvider).
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public static ServiceProvider? ServiceProvider { get; private set; }
+
+    public static T GetRequiredService<T>() where T : class =>
+        ServiceProvider!.GetRequiredService<T>();
+
+    protected override void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
+        var services = new ServiceCollection();
+
+        // Windows
+        services.AddSingleton<MainWindow>();
+
+        // ViewModels
+        services.AddSingleton<IPersonenListViewModel, FraPersonenListViewModel>();
+        services.AddSingleton<MainWindowViewModel>();
+
+        // Domain Services
+        services.AddSingleton<IPersonenService, PersonenService>();
+
+        // Module
+        services.AddMapViewer();
+
+        ServiceProvider = services.BuildServiceProvider();
+
+        var mainWindow = GetRequiredService<MainWindow>();
+        mainWindow.DataContext = GetRequiredService<MainWindowViewModel>();
+        MainWindow = mainWindow;
+        mainWindow.Show();
     }
 
+    protected override void OnExit(ExitEventArgs e)
+    {
+        ServiceProvider?.Dispose();
+        base.OnExit(e);
+    }
 }
