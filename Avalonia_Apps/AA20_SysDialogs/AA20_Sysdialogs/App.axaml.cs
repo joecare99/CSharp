4925b95a17b636847264a7ee5bd9b0d0e779d8a8
Joe Care <Joe_care_99@yahoo.com>
2025-10-28 22:42:20 +0100

    AA05_CommandParCalc

---
+++ Avalonia_Apps/AA20_SysDialogs/AA20_Sysdialogs/App.axaml.cs
@@ -0,0 +1,55 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_20_Sysdialogs
+// Author           : Mir
+// Created          : 08-09-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-09-2022
+// ***********************************************************************
+// <copyright file="App.xaml.cs" company="MVVM_20_Sysdialogs">
+//     Copyright (c) JC-Soft. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AA20_SysDialogs;
+
+public partial class App : Application
+{
+    public static IServiceProvider Services { get; private set; } = default!;
+
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        Services = services.BuildServiceProvider();
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var mainWindow = Services.GetRequiredService<MainWindow>();
+            desktop.MainWindow = mainWindow;
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // ViewModels
+        services.AddSingleton<ViewModels.SysDialogsViewModel>();
+
+        // Views
+        services.AddTransient<MainWindow>();
+        services.AddTransient<Views.SysDialogs>();
+    }
+}
