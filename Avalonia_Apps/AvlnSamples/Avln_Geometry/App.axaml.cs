05d6110468a5c7fc39962af2e4c142f982bc783c
Joe Care <Joe_care_99@yahoo.com>
2025-10-31 20:18:42 +0100

    AvlnSamples/Avln_Geometry

---
+++ Avalonia_Apps/AvlnSamples/Avln_Geometry/App.axaml.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
+using Avln_Geometry.ViewModels;
+using Avln_Geometry.ViewModels.Interfaces;
+using Avln_Geometry.Views;
+using BaseLib.Helper;
+
+namespace Avln_Geometry;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var sc = new ServiceCollection()
+      .AddTransient<SampleViewer>()
+            .AddTransient<ISampleViewerViewModel, SampleViewerViewModel>();
+
+        var sp = sc.BuildServiceProvider();
+        IoC.Configure(sp);
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ Avalonia_Apps/AvlnSamples/Avln_Geometry/ViewModels/Interfaces/ISampleViewerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace Avln_Geometry.ViewModels.Interfaces;
+
+public interface ISampleViewerViewModel
+{
+    IRelayCommand<object>  SampleSelectedCommand { get; }
+    IRelayCommand ZoomOutCompleteCommand { get; }
+
+    void AddView(object geometryUsageExample);
+}
