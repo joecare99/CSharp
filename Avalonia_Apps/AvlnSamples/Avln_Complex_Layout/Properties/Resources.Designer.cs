b037701abcaf9757b0d1200f0c3df7880929f447
Joe Care <Joe_care_99@yahoo.com>
2025-11-01 16:54:13 +0100

    AvlnSamples/Avln_Complex_Layout

---
+++ Avalonia_Apps/AvlnSamples/Avln_Complex_Layout/Properties/Resources.Designer.cs
@@ -0,0 +1,16 @@
+// Auto-generated placeholder. Replace with ResX-generated class if needed.
+namespace Avln.ComplexLayout.Properties
+{
+ public static class Resources
+ {
+ public static string txtHeader => "Some text as header";
+ public static string txtLeft => "Some text\non the left";
+ public static string txtClient => "Some text over the buttons";
+ public static string btnButton1 => "Button1";
+ public static string btnButton2 => "Button2";
+ public static string txtMsgTitle => "Message:";
+ public static string txtFooter => "Some text at the bottom of the page";
+ public static string txtBtn1React => "Button1 was clicked";
+ public static string txtBtn2React => "Button2 was clicked";
+ }
+}
