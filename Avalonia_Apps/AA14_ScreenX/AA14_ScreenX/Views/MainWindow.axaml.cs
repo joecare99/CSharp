2030d3db74be7100a05b0b73c5b92abf5b5fd930
Joe Care <Joe_care_99@yahoo.com>
2025-11-02 21:10:00 +0100

    AA05_CommandParCalc

---
+++ Avalonia_Apps/AA14_ScreenX/AA14_ScreenX/Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using System;
+using Avalonia.Controls;
+using AA14_ScreenX.ViewModels;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace AA14_ScreenX.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+}
