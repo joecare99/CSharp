04a6011b9895f88da1c4e9f025667886f1efdbc8
Joe Care <Joe_care_99@yahoo.com>
2025-11-02 20:47:18 +0100

    ScreenX.Base

---
+++ Avalonia_Apps/AA14_ScreenX/ScreenX.Base/ExRect.cs
@@ -0,0 +1,10 @@
+namespace ScreenX.Base;
+
+public readonly struct ExRect
+{
+ public readonly double X1, Y1, X2, Y2;
+ public ExRect(double x1, double y1, double x2, double y2)
+ {
+ X1 = x1; Y1 = y1; X2 = x2; Y2 = y2;
+ }
+}
