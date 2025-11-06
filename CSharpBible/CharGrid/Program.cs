23658e478e077f80d7abe803c1ea3b1171446b62
Joe Care <Joe_care_99@yahoo.com>
2025-11-06 11:38:57 +0100

    CharGrid

--- CSharpBible/CharGrid/Program.cs
+++ CSharpBible/CharGrid/Program.cs
@@ -1,36 +1,37 @@
 ﻿// ***********************************************************************
-// Assembly         : CharGrid
-// Author           : Mir
-// Created          : 12-19-2021
+// Assembly : CharGrid
+// Author : Mir
+// Created :12-19-2021
 //
-// Last Modified By : Mir
-// Last Modified On : 02-29-2020
-// ***********************************************************************
-// <copyright file="Program.cs" company="JC-Soft">
-//     Copyright © JC-Soft 2020
-// </copyright>
-// <summary></summary>
+// Last Modified By : GitHub Copilot
+// Last Modified On :2025-11-05
 // ***********************************************************************
 using System;
 using System.Windows.Forms;
-using CSharpBible.CharGrid.Visual;
+using CSharpBible.CharGrid.Views;
+using CSharpBible.CharGrid.Services;
+using CSharpBible.CharGrid.ViewModels.Interfaces;
+using CSharpBible.CharGrid.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CSharpBible.CharGrid
 {
-    /// <summary>
-    /// Class Program.
-    /// </summary>
     static class Program
     {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCharGridMain());
+
+            var services = new ServiceCollection()
+            .AddSingleton<IRandomCharService, RandomCharService>()
+            .AddSingleton<ICharGridProvider, InMemoryCharGridProvider>()
+            .AddTransient<ICharGridViewModel, CharGridViewModel>()
+            .AddTransient<FrmCharGridMain>()
+            .BuildServiceProvider();
+
+            Application.Run(services.GetRequiredService<FrmCharGridMain>());
         }
     }
 }
