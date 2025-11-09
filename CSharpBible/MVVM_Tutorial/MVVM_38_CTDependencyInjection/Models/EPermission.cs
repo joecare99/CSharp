b334bda13a5c2eec3669b9af584bfab30b2b4078
Joe Care <Joe_care_99@yahoo.com>
2025-11-09 21:42:27 +0100

    MVVM_38_CTDependencyInjection

--- CSharpBible/MVVM_Tutorial/MVVM_38_CTDependencyInjection/Models/EPermission.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVVM_38_CTDependencyInjection.Models;
-
-public enum EPermission
-{
-    None = 0,
-    Read = 1,
-    Write = 2,
-    All = 3,
-}
--- CSharpBible/MVVM_Tutorial/MVVM_38_CTDependencyInjection/Models/Interfaces/ILog.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MVVM_38_CTDependencyInjection.Models.Interfaces;
-
-public interface ILog
-{
-    void Log(string message);
-}
