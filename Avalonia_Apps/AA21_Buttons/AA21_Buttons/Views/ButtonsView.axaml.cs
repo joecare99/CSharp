d19e89e7c171f3daed9ddd14b1b944c50486d712
Joe Care <Joe_care_99@yahoo.com>
2025-11-11 16:07:37 +0100

    AA05_CommandParCalc

---
+++ Avalonia_Apps/AA21_Buttons/AA21_Buttons/Views/ButtonsView.axaml.cs
@@ -0,0 +1,36 @@
+﻿// ***********************************************************************
+// Assembly         : MVVM_21_Buttons
+// Author           : Mir
+// Created          : 08-12-2022
+//
+// Last Modified By : Mir
+// Last Modified On : 08-12-2022
+// ***********************************************************************
+// <copyright file="ButtonsView.xaml.cs" company="JC-Soft">
+//     Copyright © JC-Soft 2022
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using AA21_Buttons.ViewModels;
+using Avalonia.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AA21_Buttons.Views;
+
+/// <summary>
+/// ButtonsView: UserControl mit 3x3 Button-Grid für Flip-Spiel.
+/// </summary>
+public partial class ButtonsView : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ButtonsView"/> class.
+    /// </summary>
+    public ButtonsView()
+    {
+        InitializeComponent();
+        if (Design.IsDesignMode) return;
+
+        var vm = App.Services.GetRequiredService<ButtonsViewViewModel>();
+        DataContext = vm;
+    }
+}
