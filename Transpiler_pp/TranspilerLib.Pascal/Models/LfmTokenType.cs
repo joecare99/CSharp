4b88132238d1481649250a16d9f4e8eb0a3ec5a4
Joe Care <Joe_care_99@yahoo.com>
2025-11-11 21:57:46 +0100

    Analyzer1

---
+++ Transpiler_pp/TranspilerLib.Pascal/Models/LfmTokenType.cs
@@ -0,0 +1,20 @@
+namespace TranspilerLib.Pascal.Models;
+
+public enum LfmTokenType
+{
+    OBJECT,
+    END,
+    INHERITED,
+    COLON,
+    EQUALS,
+    DOT,
+    LANGLE,
+    RANGLE,
+    LPAREN,
+    RPAREN,
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    BOOLEAN,
+    EOF
+}
