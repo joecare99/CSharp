40e0e9da5befd071ca6980b37ddeeceadd63bbce
Joe Care <Joe_care_99@yahoo.com>
2025-11-16 22:58:37 +0100

    MVVM_26_CTBindingGroupExp

--- CSharpBible/MVVM_Tutorial/MVVM_26_CTBindingGroupExp/View/MainWindow.xaml.cs
+++ CSharpBible/MVVM_Tutorial/MVVM_26_CTBindingGroupExp/View/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ public partial class MainWindow : Window
         InitializeComponent();
         Loaded += Window_Loaded;
     }
-    private void Window_Loaded(object sender, RoutedEventArgs e)
+    private void Window_Loaded(object? sender, RoutedEventArgs e)
     {
         if (DataContext is MainWindowViewModel vm)
         {
@@ -30,9 +30,9 @@ public partial class MainWindow : Window
 
     // This event occurs when a ValidationRule in the BindingGroup
     // or in a Binding fails.
-    private void ItemError(object sender, ValidationErrorEventArgs e) => (DataContext as MainWindowViewModel)?.ItemError(sender, e);
+    private void ItemError(object? sender, ValidationErrorEventArgs e) => (DataContext as MainWindowViewModel)?.ItemError(sender, e);
 
-    void stackPanel1_Loaded(object sender, RoutedEventArgs e)
+    void stackPanel1_Loaded(object? sender, RoutedEventArgs e)
     {
         // Begin an edit transaction that enables
         // the object to accept or roll back changes.
