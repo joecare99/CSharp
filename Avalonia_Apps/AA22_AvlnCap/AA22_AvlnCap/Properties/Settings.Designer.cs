386cefa6b1345b21bf21d03247f68561e8d25c61
Joe Care <Joe_care_99@yahoo.com>
2025-11-24 19:12:23 +0100

    AA22_AvlnCap

--- Avalonia_Apps/AA22_AvlnCap/AA22_AvlnCap/Properties/Settings.Designer.cs
+++ Avalonia_Apps/AA22_AvlnCap/AA22_AvlnCap/Properties/Settings.Designer.cs
@@ -13,7 +13,7 @@ namespace AA22_AvlnCap.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
