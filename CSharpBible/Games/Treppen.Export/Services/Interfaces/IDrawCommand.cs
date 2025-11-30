d74aafd72a34725ec6ebbf94ae020f85532799df
Joe Care <Joe_care_99@yahoo.com>
2025-11-30 23:12:17 +0100

    Treppen.Export

---
+++ CSharpBible/Games/Treppen.Export/Services/Interfaces/IDrawCommand.cs
@@ -0,0 +1,6 @@
+namespace Treppen.Export.Services.Interfaces;
+
+public interface IDrawCommand
+{
+    void Render(object dc);
+}
---
+++ CSharpBible/Games/Treppen.Export/Services/Interfaces/ILabyrinth3dDrawer.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+using Treppen.Base;
+
+namespace Treppen.Export.Services.Interfaces;
+
+public interface ILabyrinth3dDrawer
+{
+    // Liefert abstrakte Zeichenkommandos anstatt direkt zu zeichnen
+    IReadOnlyList<IDrawCommand> Build(IHeightLabyrinth labyrinth, Size printableSize, IDrawCommandFactory dcFactory);
+}
