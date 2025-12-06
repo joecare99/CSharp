65a6303c8f8f02619311dd0f8f7778436aaa70ac
Joe Care <Joe_care_99@yahoo.com>
2025-12-06 10:25:19 +0100

    CSharpBible

--- CSharpBible/Libraries/MVVM_BaseLib/View/Extension/IIoC.cs
+++ CSharpBible/Libraries/MVVM_BaseLib/View/Extension/IIoC.cs
@@ -13,11 +13,23 @@
 // ***********************************************************************
 using System;
 
-namespace MVVM.View.Extension;
+namespace MVVM.Views.Extension;
 
+/// <summary>
+/// Defines a contract for Inversion of Control (IoC) container integration with XAML markup extensions.
+/// </summary>
 public interface IIoC
 {
+    /// <summary>
+    /// Gets or sets the type to be resolved from the IoC container.
+    /// </summary>
+    /// <value>The type to resolve.</value>
     Type Type { get; set; }
 
+    /// <summary>
+    /// Provides the resolved value from the IoC container.
+    /// </summary>
+    /// <param name="serviceProvider">The service provider that can provide services for the markup extension.</param>
+    /// <returns>The resolved object instance, or <c>null</c> if the type cannot be resolved.</returns>
     object? ProvideValue(IServiceProvider serviceProvider);
 }
--- CSharpBible/Libraries/MVVM_BaseLibTests/Helper/ObjectHelper2Tests.cs
+++ CSharpBible/Libraries/MVVM_BaseLibTests/Helper/ObjectHelper2Tests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace BaseLib.Helper.Tests;
 
@@ -10,7 +11,8 @@ public class ObjectHelper2Tests
 #pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Fügen Sie ggf. den „erforderlichen“ Modifizierer hinzu, oder deklarieren Sie den Modifizierer als NULL-Werte zulassend.
 
     [TestInitialize]
-    public void Init() { 
+    public void Init()
+    {
         _testClass = new ControlArray<int?>();
     }
 
@@ -20,23 +22,24 @@ public class ObjectHelper2Tests
         // Act
         _testClass.SetIndex(8, 4);
         _testClass.SetIndex(2, 1);
-         // Assert
+        // Assert
         Assert.AreEqual(2, _testClass.Count);
-        Assert.AreEqual(8, _testClass[5]);
-        Assert.AreEqual(2, _testClass[2]);
+        Assert.AreEqual(8, _testClass[4]);
+        Assert.AreEqual(2, _testClass[1]);
     }
 
     [TestMethod()]
     public void GetIndexTest()
     {
         // Arrange
-        _testClass[5] = 8;
-        _testClass[1] = null!;
-        _testClass[2] = 2;
+        var d = (Dictionary<int, int?>)_testClass;
+        d[5] = 8;
+        d[1] = null!;
+        d[2] = 2;
 
         // Assert
-        Assert.AreEqual(4,_testClass.GetIndex(8));
-        Assert.AreEqual(1,_testClass.GetIndex(2));
-        Assert.AreEqual(-1,_testClass.GetIndex(1));
+        Assert.AreEqual(4, _testClass.GetIndex(8));
+        Assert.AreEqual(1, _testClass.GetIndex(2));
+        Assert.AreEqual(-1, _testClass.GetIndex(1));
     }
 }
