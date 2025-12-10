725ce55dbcec2c392df5944c49939fd6f69357b0
Joe Care <Joe_care_99@yahoo.com>
2025-12-10 19:17:11 +0100

    Document.Base

---
+++ CSharpBible/MVVM_Tutorial/WpfApp1/Views/Frm_W2kMain.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp.Views;
+
+public partial class Frm_W2kMain : Window
+{
+    public Frm_W2kMain()
+    {
+        InitializeComponent();
+    }
+}
+
