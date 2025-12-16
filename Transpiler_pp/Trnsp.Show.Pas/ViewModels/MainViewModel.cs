0e9d96db1347ae21bdf8d427670601f0542817b6
Joe Care <Joe_care_99@yahoo.com>
2025-12-16 01:53:43 +0100

    Trnsp.Show.Pas

--- Transpiler_pp/Trnsp.Show.Pas/ViewModels/MainViewModel.cs
+++ Transpiler_pp/Trnsp.Show.Pas/ViewModels/MainViewModel.cs
@@ -3,54 +3,46 @@ using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using Trnsp.Show.Pas.Services;
 
-namespace Trnsp.Show.Pas.ViewModels
+namespace Trnsp.Show.Pas.ViewModels;
+
+public partial class MainViewModel : ObservableObject
 {
-    public partial class MainViewModel : ObservableObject
+    private readonly IFileService _fileService;
+    private readonly IPascalParserService _parserService;
+
+    public ObservableCollection<CodeBlockNode> RootNodes { get; } = new();
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(SourceSelectionStart))]
+    [NotifyPropertyChangedFor(nameof(SourceSelectionLength))]
+    private CodeBlockNode? _selectedNode;
+
+    public int SourceSelectionStart => SelectedNode != null ? SelectedNode.SourcePos : -1;
+    public int SourceSelectionLength => SelectedNode != null ? SelectedNode.Length : 0;
+
+    [ObservableProperty]
+    private string _fullSourceCode = string.Empty;
+    public MainViewModel(IFileService fileService, IPascalParserService parserService)
     {
-        private readonly IFileService _fileService;
-        private readonly IPascalParserService _parserService;
+        _fileService = fileService;
+        _parserService = parserService;
+    }
 
-        public ObservableCollection<CodeBlockNode> RootNodes { get; } = new();
+    public MainViewModel() : this(new FileService(), new PascalParserService(null!))
+    {
+    }
 
-        [ObservableProperty]
-        [NotifyPropertyChangedFor(nameof(SourceSelectionStart))]
-        [NotifyPropertyChangedFor(nameof(SourceSelectionLength))]
-        private CodeBlockNode? _selectedNode;
-
-        public int SourceSelectionStart => SelectedNode != null ? SelectedNode.CodeBlockStart : -1;
-        public int SourceSelectionLength => SelectedNode != null ? SelectedNode.Length : 0;
-
-        private string _fullSourceCode = string.Empty;
-        public string FullSourceCode
+    [RelayCommand]
+    private void LoadFile()
+    {
+        var filePath = _fileService.OpenFileDialog("Open Pascal File", "Pascal Files (*.pas;*.pp)|*.pas;*.pp|All Files (*.*)|*.*");
+        if (filePath != null)
         {
-            get => _fullSourceCode;
-            set => SetProperty(ref _fullSourceCode, value);
-        }
-
-        public IRelayCommand LoadFileCommand { get; }
-
-        public MainViewModel(IFileService fileService, IPascalParserService parserService)
-        {
-            _fileService = fileService;
-            _parserService = parserService;
-            LoadFileCommand = new RelayCommand(LoadFile);
-        }
-
-        public MainViewModel() : this(new FileService(), new PascalParserService(null!))
-        {
-        }
-
-        private void LoadFile()
-        {
-            var filePath = _fileService.OpenFileDialog("Open Pascal File", "Pascal Files (*.pas;*.pp)|*.pas;*.pp|All Files (*.*)|*.*");
-            if (filePath != null)
-            {
-                var content = _fileService.ReadAllText(filePath);
-                FullSourceCode = content;
-                var rootBlock = _parserService.Parse(content);
-                RootNodes.Clear();
-                RootNodes.Add(new CodeBlockNode(rootBlock));
-            }
+            var content = _fileService.ReadAllText(filePath);
+            FullSourceCode = content;
+            var rootBlock = _parserService.Parse(content);
+            RootNodes.Clear();
+            RootNodes.Add(new CodeBlockNode(rootBlock));
         }
     }
 }
