287ab7885b52cab1d129e1ef1b7eea772449e67f
Joe Care <Joe_care_99@yahoo.com>
2025-12-20 14:59:40 +0100

    Analyzer1

---
+++ Transpiler_pp/TranspilerLib.DriveBASIC/Models/CommandBuilderState.cs
@@ -0,0 +1,30 @@
+﻿using TranspilerLib.DriveBASIC.Data;
+using static TranspilerLib.DriveBASIC.Data.ParseDefinitions;
+
+namespace TranspilerLib.DriveBASIC.Models;
+
+public sealed class CommandBuilderState
+{
+    public bool HasToken { get; private set; }
+    public EDriveToken Token { get; private set; }
+    public int SubToken { get; private set; }
+    public int Param1 { get; set; }
+    public int Param2 { get; set; }
+    public double Param3 { get; set; }
+
+    public void Initialize(EDriveToken token, int subToken)
+    {
+        HasToken = true;
+        Token = token;
+        SubToken = TteTokens.Contains(token) ? subToken * 64 : subToken;
+        Param1 = 0;
+        Param2 = 0;
+        Param3 = 0d;
+    }
+
+    public void AddSubToken(int delta) => SubToken += delta;
+
+    public DriveCommand ToDriveCommand()
+        => new(Token, [(SubToken & 0xFF), Param1, Param2, Param3]);
+}
+
---
+++ Transpiler_pp/TranspilerLib.DriveBASIC/Models/CompilerLabel.cs
@@ -0,0 +1,11 @@
+﻿using TranspilerLib.DriveBASIC.Data.Interfaces;
+
+namespace TranspilerLib.DriveBASIC.Models;
+
+
+public sealed class CompilerLabel : ILabel
+{
+    public string? Name { get; set; }
+    public int Index { get; set; } = -1;
+}
+
