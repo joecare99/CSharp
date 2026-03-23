13e1d102e7d358ba4f0c817bc1b44c55b9bfad81
Joe Care <Joe_care_99@yahoo.com>
2026-03-23 22:10:19 +0100

    Core.Tests

---
+++ JC-AMS/Core.Tests/Core/Logging/SLoggingTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace JCAMS.Core.Logging.Tests
+{
+    [TestClass()]
+    public class SLoggingTests
+    {
+        string DebugResult = "";
+        private readonly string cExpLogTest = "L:c:\\JCAMSstartup.txt,'Exception;TestException;;',(LogTest)\r\nL:c:\\JCAMSstartup.txt,'\n',()\r\n";
+        private readonly string cExpLogTest0_1 = "L:c:\\JCAMSstartup.txt,'Exception;TestException;;',(LogTest0_1)\r\nL:c:\\JCAMSstartup.txt,'\n',()\r\n";
+        private readonly string cExpLogTest0 = "";
+        private readonly string cExpLogTest1 = "L:c:\\JCAMSstartup.txt,'LogTest1: {0}, {1}, {2}',(4321;1234;3rd Param)\r\nL:c:\\JCAMSstartup.txt,'\n',()\r\n";
+        private readonly string cExpLogTest2 = "";
+        private readonly string cExpLogTest2_1 = "";
+        private readonly string cExpLogTest2_2 = "L:c:\\JCAMSstartup.txt,'LogTest2: {0}, {1}, {2}',(4321;1234;3rd Param)\r\nL:c:\\JCAMSstartup.txt,'\n',()\r\n";
+
+        [TestInitialize()]
+        public void Init()
+        {
+            SLogging.DoWriteLog = LogDoWrite;
+            DebugResult = "";
+        }
+
+        private bool LogDoWrite(object oDest, string sText, object[] oParam)
+        {
+            string sParam = "";
+            if (oParam != null)
+                foreach (object arg4 in oParam.ToList())
+                    sParam += $";{arg4}";
+            DebugResult += $"L:{oDest},'{sText}',({sParam.TrimStart(';')})"+Environment.NewLine;
+            return true; // todo:
+        }
+
+        [TestMethod()]
+        public void CreateProtocolTest()
+        {         
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DebugPrintTest()
+        {
+            SLogging.DebugPrint("Hello World!");
+        }
+
+        [TestMethod()]
+        public void DebugPrintTest1()
+        {
+            SLogging.DebugPrint("{0} {1}!","Ciao","Bella");
+        }
+
+        [TestMethod()]
+        public void DeleteStartupLogTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LogTest()
+        {
+            SLogging.xTriggerError = false;
+            SLogging.Log(new Exception("TestException"));
+            Assert.AreEqual(cExpLogTest,DebugResult);
+        }
+
+        [TestMethod()]
+        public void LogTest0_1()
+        {
+            SLogging.xTriggerError = true;
+            SLogging.Log(new Exception("TestException"));
+            Assert.AreEqual(cExpLogTest0_1, DebugResult);
+        }
+
+        [TestMethod()]
+        public void LogTest1()
+        {
+            SLogging.xTriggerError = false;
+            SLogging.Log("LogTest1: {0}, {1}, {2}",4321,1234,"3rd Param");
+            Assert.AreEqual(cExpLogTest1, DebugResult);
+        }
+
+        [TestMethod()]
+        public void LogTest2()
+        {
+            SLogging.xTriggerError = false;
+            SLogging.Log(ELogTopic.Debug, "LogTest2: {0}, {1}, {2}", 4321, 1234, "3rd Param");
+            Assert.AreEqual(cExpLogTest2, DebugResult);
+        }
+
+        [TestMethod()]
+        public void LogTest2_1()
+        {
+            SLogging.xTriggerError = false;
+            SLogging.xAppIsStarting = false;
+            SLogging.Log(ELogTopic.Debug, "LogTest2: {0}, {1}, {2}", 4321, 1234, "3rd Param");
+            Assert.AreEqual(cExpLogTest2_1, DebugResult);
+        }
+
+        [TestMethod()]
+        public void LogTest2_2()
+        {
+            SLogging.xTriggerError = false;
+            SLogging.xAppIsStarting = false;
+            SLogging.Log(ELogTopic.Always, "LogTest2: {0}, {1}, {2}", 4321, 1234, "3rd Param");
+            Assert.AreEqual(cExpLogTest2_2, DebugResult);
+        }
+
+        [TestMethod()]
+        public void LogTest3()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ JC-AMS/Core.Tests/Core/SSimpleCipherTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JCAMS.Core.Tests
+{
+    [TestClass()]
+    public class SSimpleCipherTests
+    {
+        [DataTestMethod()]
+        [DataRow("Null", "", null)]
+        [DataRow("empty", "", "")]
+        [DataRow("Hello World", "Hello World", "9uWdWUpsF3YqFx3Br5HehA==")]
+        [DataRow("Ratzopaltuff", "Ratzopaltuff", "um7lW5sPeEbPqyrSFrTQYg==")]
+        [DataRow("Lorem ipsum ...", TestData.cLoremIpsum, "tkSKmV0ot0HdTxKIAyBPxa0F3FNR7bRVvwY4iqDOt9nOXOKLM5GyIs5A1TlLh52MZ+OV4PczZt69mjoACanw1GCfEPkvWvOvlXTB1UmXcxV42YyHxujuk45391ufLqgJrGeJCzw+M5JyYq4WXK4qcWp9PyGhwzr9waHUi3ZNQVj2otsnnDoJkGKxw1yUE1z1m6p/KiWUGg73Un4iarI3cB/KWi4CpIsK+/TaUejzSHIV4l7xDxrL/Mt/5/FYkzy+DMDjQEstjqooCkULknQzE/5vuan2VLTRRzXEsObecXdsXhkFZV1qaoQPtEgcVj6YrL0Fc7jvyF7eY81BvC8WEuyAP/nUqKuHeE1ly0da0pQSM4BDjMdZIj8r8RxtlPZHWfcwMyzYCifMMFNT/BlLi0srvPAMMStVK37MqKDmvNmQurKmQVzaJinu7OOOk2ayePy+0uVzfoGdxgGA8ISlPJHYm3dxoMxRv3Ldhlthip7sDJWaYplBH5LsOsCOXCPR6+DV4ncc/uYgEyRpuJ8tX1CW/+e28xycp0UVPF7M0XqGxDcytpki9ZUOLGb2J/Ypi9Ly+ZX+LYs=")]
+        public void SimpelDecryptTest(string name, string sExp, string sCypher)
+        {
+            Assert.AreEqual(sExp,SSimpleCipher.SimpleDecrypt(sCypher),$"Test: {name}");
+        }
+
+        [DataTestMethod()]
+        [DataRow("Null",null,"")]
+        [DataRow("empty", "", "")]
+        [DataRow("Hello World", "Hello World", "9uWdWUpsF3YqFx3Br5HehA==")]
+        [DataRow("Ratzopaltuff", "Ratzopaltuff", "um7lW5sPeEbPqyrSFrTQYg==")]
+
+        [DataRow("Lorem ipsum ...", TestData.cLoremIpsum, "tkSKmV0ot0HdTxKIAyBPxa0F3FNR7bRVvwY4iqDOt9nOXOKLM5GyIs5A1TlLh52MZ+OV4PczZt69mjoACanw1GCfEPkvWvOvlXTB1UmXcxV42YyHxujuk45391ufLqgJrGeJCzw+M5JyYq4WXK4qcWp9PyGhwzr9waHUi3ZNQVj2otsnnDoJkGKxw1yUE1z1m6p/KiWUGg73Un4iarI3cB/KWi4CpIsK+/TaUejzSHIV4l7xDxrL/Mt/5/FYkzy+DMDjQEstjqooCkULknQzE/5vuan2VLTRRzXEsObecXdsXhkFZV1qaoQPtEgcVj6YrL0Fc7jvyF7eY81BvC8WEuyAP/nUqKuHeE1ly0da0pQSM4BDjMdZIj8r8RxtlPZHWfcwMyzYCifMMFNT/BlLi0srvPAMMStVK37MqKDmvNmQurKmQVzaJinu7OOOk2ayePy+0uVzfoGdxgGA8ISlPJHYm3dxoMxRv3Ldhlthip7sDJWaYplBH5LsOsCOXCPR6+DV4ncc/uYgEyRpuJ8tX1CW/+e28xycp0UVPF7M0XqGxDcytpki9ZUOLGb2J/Ypi9Ly+ZX+LYs=")]
+        public void SimpelEncryptTest(string name, string sMessage, string sExp)
+        {
+            Assert.AreEqual(sExp, SSimpleCipher.SimpleEncrypt(sMessage), $"Test: {name}");
+        }
+    }
+}
