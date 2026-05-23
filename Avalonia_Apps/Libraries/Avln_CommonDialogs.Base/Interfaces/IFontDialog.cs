0829934586fb3324944e2dc632d882ab8d3cc5dd
Joe Care <Joe_care_99@yahoo.com>
2026-05-23 09:04:35 +0200

    Avln_CommonDialogs.Base

--- Avalonia_Apps/Libraries/Avln_CommonDialogs.Base/Interfaces/IFontDialog.cs
+++ Avalonia_Apps/Libraries/Avln_CommonDialogs.Base/Interfaces/IFontDialog.cs
@@ -1,8 +1,16 @@
+using Avln_CommonDialogs.Base.Models;
+
 namespace Avln_CommonDialogs.Base.Interfaces;
 
 public interface IFontDialog
 {
     object? Font { get; set; }
+
+    object? Selection { get; set; }
+
+    string? PreviewText { get; set; }
+
+    FontDialogPresentationMode PresentationMode { get; set; }
 
     ValueTask<bool?> ShowAsync(object owner);
     ValueTask<bool?> ShowAsync();
