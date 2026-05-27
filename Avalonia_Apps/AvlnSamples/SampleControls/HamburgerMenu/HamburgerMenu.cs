543769b9240803c58560504604628ca840661094
Joe Care <Joe_care_99@yahoo.com>
2026-05-27 16:19:16 +0200

    SampleControls

--- Avalonia_Apps/AvlnSamples/SampleControls/HamburgerMenu/HamburgerMenu.cs
+++ Avalonia_Apps/AvlnSamples/SampleControls/HamburgerMenu/HamburgerMenu.cs
@@ -3,81 +3,80 @@ using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 
-namespace ControlSamples
+namespace ControlSamples;
+
+public class HamburgerMenu : TabControl
 {
-    public class HamburgerMenu : TabControl
+    private SplitView? _splitView;
+
+    public static readonly StyledProperty<IBrush?> PaneBackgroundProperty =
+        SplitView.PaneBackgroundProperty.AddOwner<HamburgerMenu>();
+
+    public IBrush? PaneBackground
     {
-        private SplitView? _splitView;
+        get => GetValue(PaneBackgroundProperty);
+        set => SetValue(PaneBackgroundProperty, value);
+    }
 
-        public static readonly StyledProperty<IBrush?> PaneBackgroundProperty =
-            SplitView.PaneBackgroundProperty.AddOwner<HamburgerMenu>();
+    public static readonly StyledProperty<IBrush?> ContentBackgroundProperty =
+        AvaloniaProperty.Register<HamburgerMenu, IBrush?>(nameof(ContentBackground));
 
-        public IBrush? PaneBackground
+    public IBrush? ContentBackground
+    {
+        get => GetValue(ContentBackgroundProperty);
+        set => SetValue(ContentBackgroundProperty, value);
+    }
+
+    public static readonly StyledProperty<int> ExpandedModeThresholdWidthProperty =
+        AvaloniaProperty.Register<HamburgerMenu, int>(nameof(ExpandedModeThresholdWidth), 1008);
+
+    public int ExpandedModeThresholdWidth
+    {
+        get => GetValue(ExpandedModeThresholdWidthProperty);
+        set => SetValue(ExpandedModeThresholdWidthProperty, value);
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _splitView = e.NameScope.Find<SplitView>("PART_NavigationPane");
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == BoundsProperty && _splitView is not null)
         {
-            get => GetValue(PaneBackgroundProperty);
-            set => SetValue(PaneBackgroundProperty, value);
+            var (oldBounds, newBounds) = change.GetOldAndNewValue<Rect>();
+            EnsureSplitViewMode(oldBounds, newBounds);
         }
 
-        public static readonly StyledProperty<IBrush?> ContentBackgroundProperty =
-            AvaloniaProperty.Register<HamburgerMenu, IBrush?>(nameof(ContentBackground));
-
-        public IBrush? ContentBackground
+        if (change.Property == SelectedItemProperty)
         {
-            get => GetValue(ContentBackgroundProperty);
-            set => SetValue(ContentBackgroundProperty, value);
-        }
-
-        public static readonly StyledProperty<int> ExpandedModeThresholdWidthProperty =
-            AvaloniaProperty.Register<HamburgerMenu, int>(nameof(ExpandedModeThresholdWidth), 1008);
-
-        public int ExpandedModeThresholdWidth
-        {
-            get => GetValue(ExpandedModeThresholdWidthProperty);
-            set => SetValue(ExpandedModeThresholdWidthProperty, value);
-        }
-
-        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
-        {
-            base.OnApplyTemplate(e);
-
-            _splitView = e.NameScope.Find<SplitView>("PART_NavigationPane");
-        }
-
-        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
-        {
-            base.OnPropertyChanged(change);
-
-            if (change.Property == BoundsProperty && _splitView is not null)
+            if (_splitView is not null && _splitView.DisplayMode == SplitViewDisplayMode.Overlay)
             {
-                var (oldBounds, newBounds) = change.GetOldAndNewValue<Rect>();
-                EnsureSplitViewMode(oldBounds, newBounds);
-            }
-
-            if (change.Property == SelectedItemProperty)
-            {
-                if (_splitView is not null && _splitView.DisplayMode == SplitViewDisplayMode.Overlay)
-                {
-                    _splitView.SetCurrentValue(SplitView.IsPaneOpenProperty, false);
-                }
+                _splitView.SetCurrentValue(SplitView.IsPaneOpenProperty, false);
             }
         }
+    }
 
-        private void EnsureSplitViewMode(Rect oldBounds, Rect newBounds)
+    private void EnsureSplitViewMode(Rect oldBounds, Rect newBounds)
+    {
+        if (_splitView is not null)
         {
-            if (_splitView is not null)
-            {
-                var threshold = ExpandedModeThresholdWidth;
+            var threshold = ExpandedModeThresholdWidth;
 
-                if (newBounds.Width >= threshold)
-                {
-                    _splitView.DisplayMode = SplitViewDisplayMode.Inline;
-                    _splitView.IsPaneOpen = true;
-                }
-                else if (newBounds.Width < threshold)
-                {
-                    _splitView.DisplayMode = SplitViewDisplayMode.Overlay;
-                    _splitView.IsPaneOpen = false;
-                }
+            if (newBounds.Width >= threshold)
+            {
+                _splitView.DisplayMode = SplitViewDisplayMode.Inline;
+                _splitView.IsPaneOpen = true;
+            }
+            else if (newBounds.Width < threshold)
+            {
+                _splitView.DisplayMode = SplitViewDisplayMode.Overlay;
+                _splitView.IsPaneOpen = false;
             }
         }
     }
