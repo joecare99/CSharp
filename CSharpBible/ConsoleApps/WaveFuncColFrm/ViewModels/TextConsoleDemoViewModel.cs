5210aa06f3f39effbed7ae6edbd1c48c3bf1295c
Joe Care <Joe_care_99@yahoo.com>
2026-06-06 00:38:57 +0200

    WaveFuncColFrm

--- CSharpBible/ConsoleApps/WaveFuncColFrm/ViewModels/TextConsoleDemoViewModel.cs
+++ CSharpBible/ConsoleApps/WaveFuncColFrm/ViewModels/TextConsoleDemoViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using BaseLib.Interfaces;
 using BaseLib.Models.Interfaces;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.DependencyInjection;
 using CommunityToolkit.Mvvm.Input;
+using ConsoleDisplay.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,10 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     private IWFCModel _model;
     private IRandom _random;
 
+    public Display display1 { get; set; }
+    public Display display2 { get; set; }
+    public Display display3 { get; set; }
+
     public IConsole? console { get; set; }
 
     public TextConsoleDemoViewModel(IWFCModel model,IRandom random)
@@ -28,6 +34,9 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     [RelayCommand]
     private void DoHello()
     {
+        display1.Clear();
+        display2.Clear();
+        display3.Clear();
         console.Clear();
         console.ForegroundColor = ConsoleColor.White;
         console.SetCursorPosition(20, 12);
@@ -38,6 +47,15 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     private void DoShowImages()
     {
         console.Clear();
+        _model.console = console;
+        _model.D1SBuffer = () => display1.ScreenBuffer;
+        _model.D2SBuffer = () => display2.ScreenBuffer;
+        _model.D3SBuffer = () => display3.ScreenBuffer;
+        _model.D1PutPixel = (x, y, c) => display1.PutPixel(x, y, c);
+        _model.D1PutPixelC = (x, y, r, g, b) => display1.PutPixel(x, y, r, g, b);
+        _model.D2PutPixelC = (x, y, r, g, b) => display2.PutPixel(x, y, r, g, b);
+        _model.DUpdate = () => { display1.Update(); display2.Update(); display3.Update(); };
+        _model.LoadImage = (filename) => Bitmap.FromFile(filename) as Bitmap;
         _model.ShowImage1("Resources\\GreenDot.png");
         _model.ShowImage2("Resources\\Joe_Care_n_h64.jpg");
     }
@@ -46,6 +64,15 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     private void DoAnalyseImage()
     {
         console.Clear();
+        _model.console = console;
+        _model.D1SBuffer = () => display1.ScreenBuffer;
+        _model.D2SBuffer = () => display2.ScreenBuffer;
+        _model.D3SBuffer = () => display3.ScreenBuffer;
+        _model.D1PutPixel = (x, y, c) => display1.PutPixel(x, y, c);
+        _model.D1PutPixelC = (x, y, r, g, b) => display1.PutPixel(x, y, r, g, b);
+        _model.D2PutPixelC = (x, y, r, g, b) => display2.PutPixel(x, y, r, g, b);
+        _model.DUpdate = () => { display1.Update(); display2.Update(); display3.Update(); };
+        _model.LoadImage = (filename) => Bitmap.FromFile(filename) as Bitmap;
         _model.AnalyseImage("Resources\\GreenDot.png");
     }
 
@@ -53,6 +80,14 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     private void DoDisplayTest()
     {
         _model.console = console;
+        _model.D1SBuffer = () => display1.ScreenBuffer;
+        _model.D2SBuffer = () => display2.ScreenBuffer;
+        _model.D3SBuffer = () => display3.ScreenBuffer;
+        _model.D1PutPixel = (x, y, c) => display1.PutPixel(x, y, c);
+        _model.D1PutPixelC = (x, y, r, g, b) => display1.PutPixel(x, y, r, g, b);
+        _model.D2PutPixelC = (x, y, r, g, b) => display2.PutPixel(x, y, r, g, b);
+        _model.DUpdate = () => { display1.Update(); display2.Update(); display3.Update(); };
+        _model.LoadImage = (filename) => Bitmap.FromFile(filename) as Bitmap;
         _model.DisplayTest1(_random);
         _model.DisplayTest2();
         _model.DisplayTest3(_random);
