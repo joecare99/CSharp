8478a4c2e151cf37dbbe10b532569242bdf238b5
Joe Care <Joe_care_99@yahoo.com>
2026-06-08 00:09:41 +0200

    PluginBase

--- TestStatements/PluginBase/Interfaces/IEnvironment.cs
+++ TestStatements/PluginBase/Interfaces/IEnvironment.cs
@@ -1,10 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PluginBase.Interfaces;
 
