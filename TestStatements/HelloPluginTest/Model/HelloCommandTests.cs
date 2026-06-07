c1644d1c4df96547ff936fe349ee4a9dce71008f
Joe Care <Joe_care_99@yahoo.com>
2026-06-08 00:09:40 +0200

    HelloPluginTest

--- TestStatements/HelloPluginTest/Model/HelloCommandTests.cs
+++ TestStatements/HelloPluginTest/Model/HelloCommandTests.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using HelloPlugin.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NSubstitute;
+﻿using NSubstitute;
 using PluginBase.Interfaces;
 using HelloPlugin.Properties;
 
