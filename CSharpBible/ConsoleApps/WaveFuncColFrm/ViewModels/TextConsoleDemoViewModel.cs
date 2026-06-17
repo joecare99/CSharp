8b1fd629c037ca6ce10db74dd1fb47ee2b021def
Rosewich, Christian <christian.rosewich@sew-eurodrive.de>
2026-06-17 15:43:17 +0200

    Nullable Properties & Multi-Targeting in Projekten
    Properties im ViewModel auf nullable umgestellt und Null-Propagation genutzt, um NullReferenceExceptions zu vermeiden.
    Projektdateien für dynamisches Multi-Targeting (net9.0/net10.0) angepasst und Build-Artefakte aus `obj\**` ausgeschlossen.
    Paketabhängigkeiten aktualisiert/ergänzt.
    Code-Style in `Color32.cs` vereinheitlicht.

--- CSharpBible/ConsoleApps/WaveFuncColFrm/ViewModels/TextConsoleDemoViewModel.cs
+++ CSharpBible/ConsoleApps/WaveFuncColFrm/ViewModels/TextConsoleDemoViewModel.cs
@@ -19,9 +19,9 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     private IWFCModel _model;
     private IRandom _random;
 
-    public Display display1 { get; set; }
-    public Display display2 { get; set; }
-    public Display display3 { get; set; }
+    public Display? display1 { get; set; }
+    public Display? display2 { get; set; }
+    public Display? display3 { get; set; }
 
     public IConsole? console { get; set; }
 
@@ -34,27 +34,27 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     [RelayCommand]
     private void DoHello()
     {
-        display1.Clear();
-        display2.Clear();
-        display3.Clear();
-        console.Clear();
-        console.ForegroundColor = ConsoleColor.White;
-        console.SetCursorPosition(20, 12);
-        console.WriteLine("Hello World");
+        display1?.Clear();
+        display2?.Clear();
+        display3?.Clear();
+        console?.Clear();
+        console?.ForegroundColor = ConsoleColor.White;
+        console?.SetCursorPosition(20, 12);
+        console?.WriteLine("Hello World");
     }
 
     [RelayCommand]
     private void DoShowImages()
     {
-        console.Clear();
+        console?.Clear();
         _model.console = console;
-        _model.D1SBuffer = () => display1.ScreenBuffer;
-        _model.D2SBuffer = () => display2.ScreenBuffer;
-        _model.D3SBuffer = () => display3.ScreenBuffer;
-        _model.D1PutPixel = (x, y, c) => display1.PutPixel(x, y, c);
-        _model.D1PutPixelC = (x, y, r, g, b) => display1.PutPixel(x, y, r, g, b);
-        _model.D2PutPixelC = (x, y, r, g, b) => display2.PutPixel(x, y, r, g, b);
-        _model.DUpdate = () => { display1.Update(); display2.Update(); display3.Update(); };
+        _model.D1SBuffer = () => display1?.ScreenBuffer;
+        _model.D2SBuffer = () => display2?.ScreenBuffer;
+        _model.D3SBuffer = () => display3?.ScreenBuffer;
+        _model.D1PutPixel = (x, y, c) => display1?.PutPixel(x, y, c);
+        _model.D1PutPixelC = (x, y, r, g, b) => display1?.PutPixel(x, y, r, g, b);
+        _model.D2PutPixelC = (x, y, r, g, b) => display2?.PutPixel(x, y, r, g, b);
+        _model.DUpdate = () => { display1?.Update(); display2?.Update(); display3?.Update(); };
         _model.LoadImage = (filename) => Bitmap.FromFile(filename) as Bitmap;
         _model.ShowImage1("Resources\\GreenDot.png");
         _model.ShowImage2("Resources\\Joe_Care_n_h64.jpg");
@@ -63,15 +63,15 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     [RelayCommand]
     private void DoAnalyseImage()
     {
-        console.Clear();
+        console?.Clear();
         _model.console = console;
-        _model.D1SBuffer = () => display1.ScreenBuffer;
-        _model.D2SBuffer = () => display2.ScreenBuffer;
-        _model.D3SBuffer = () => display3.ScreenBuffer;
-        _model.D1PutPixel = (x, y, c) => display1.PutPixel(x, y, c);
-        _model.D1PutPixelC = (x, y, r, g, b) => display1.PutPixel(x, y, r, g, b);
-        _model.D2PutPixelC = (x, y, r, g, b) => display2.PutPixel(x, y, r, g, b);
-        _model.DUpdate = () => { display1.Update(); display2.Update(); display3.Update(); };
+        _model.D1SBuffer = () => display1?.ScreenBuffer;
+        _model.D2SBuffer = () => display2?.ScreenBuffer;
+        _model.D3SBuffer = () => display3?.ScreenBuffer;
+        _model.D1PutPixel = (x, y, c) => display1?.PutPixel(x, y, c);
+        _model.D1PutPixelC = (x, y, r, g, b) => display1?.PutPixel(x, y, r, g, b);
+        _model.D2PutPixelC = (x, y, r, g, b) => display2?.PutPixel(x, y, r, g, b);
+        _model.DUpdate = () => { display1?.Update(); display2?.Update(); display3?.Update(); };
         _model.LoadImage = (filename) => Bitmap.FromFile(filename) as Bitmap;
         _model.AnalyseImage("Resources\\GreenDot.png");
     }
@@ -80,13 +80,13 @@ public partial class TextConsoleDemoViewModel : ObservableObject, ITextConsoleDe
     private void DoDisplayTest()
     {
         _model.console = console;
-        _model.D1SBuffer = () => display1.ScreenBuffer;
-        _model.D2SBuffer = () => display2.ScreenBuffer;
-        _model.D3SBuffer = () => display3.ScreenBuffer;
-        _model.D1PutPixel = (x, y, c) => display1.PutPixel(x, y, c);
-        _model.D1PutPixelC = (x, y, r, g, b) => display1.PutPixel(x, y, r, g, b);
-        _model.D2PutPixelC = (x, y, r, g, b) => display2.PutPixel(x, y, r, g, b);
-        _model.DUpdate = () => { display1.Update(); display2.Update(); display3.Update(); };
+        _model.D1SBuffer = () => display1?.ScreenBuffer;
+        _model.D2SBuffer = () => display2?.ScreenBuffer;
+        _model.D3SBuffer = () => display3?.ScreenBuffer;
+        _model.D1PutPixel = (x, y, c) => display1?.PutPixel(x, y, c);
+        _model.D1PutPixelC = (x, y, r, g, b) => display1?.PutPixel(x, y, r, g, b);
+        _model.D2PutPixelC = (x, y, r, g, b) => display2?.PutPixel(x, y, r, g, b);
+        _model.DUpdate = () => { display1?.Update(); display2?.Update(); display3?.Update(); };
         _model.LoadImage = (filename) => Bitmap.FromFile(filename) as Bitmap;
         _model.DisplayTest1(_random);
         _model.DisplayTest2();
