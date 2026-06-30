346ce43f052dc046e10f4e84ca5d3aaebb0f9533
Joe Care <Joe_care_99@yahoo.com>
2026-07-01 00:21:20 +0200

    GenFreeBrowser

--- GenFreeWin/GenFreeBrowser/Map/Interfaces/ITileCache.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace GenFreeBrowser.Map.Interfaces;
-
-public interface ITileCache
-{
-    Task<byte[]?> TryGetAsync(TileId id, string pId);
-    Task StoreAsync(TileId id, string pId, byte[] data);
-}
--- GenFreeWin/GenFreeBrowser/Map/TileId.cs
+++
@@ -1,9 +0,0 @@
-namespace GenFreeBrowser.Map;
-
-/// <summary>
-/// Tile identification in a WebMercator XYZ tiling scheme
-/// </summary>
-public readonly record struct TileId(long X, long Y, int Z)
-{
-    public override string ToString() => $"{Z}/{X}/{Y}";
-}
