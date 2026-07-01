25d3b28cb4e882225b91fc621b1e4f93c4af401c
Joe Care <Joe_care_99@yahoo.com>
2026-07-01 21:57:06 +0200

    WinAhnenNew.Model

--- WinAhnenNew/WinAhnenNew.Model/ViewModels/TextPageViewModel.cs
+++ WinAhnenNew/WinAhnenNew.Model/ViewModels/TextPageViewModel.cs
@@ -1,9 +1,21 @@
+using CommunityToolkit.Mvvm.Messaging;
+using WinAhnenNew.Services;
+
 namespace WinAhnenNew.ViewModels
 {
     /// <summary>
     /// View model for the text tab page.
     /// </summary>
-    public sealed partial class TextPageViewModel : ViewModelBase
+    public sealed partial class TextPageViewModel : PersonHeaderViewModelBase
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextPageViewModel"/> class.
+        /// </summary>
+        /// <param name="personSelectionService">The shared person selection service.</param>
+        /// <param name="messenger">The shared application messenger.</param>
+        public TextPageViewModel(IPersonSelectionService personSelectionService, IMessenger messenger)
+            : base(personSelectionService, messenger)
+        {
+        }
     }
 }
