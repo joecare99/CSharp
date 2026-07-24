9974ea1d14a8c85eb19fb7d1c07597aaa7ef47bb
Joe Care <Joe_care_99@yahoo.com>
2026-07-24 21:28:19 +0200

    GenFreeBase

--- GenFreeWin/GenFreeBase/Interfaces/UI/IApplUserTexts.cs
+++ GenFreeWin/GenFreeBase/Interfaces/UI/IApplUserTexts.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GenFree.Interfaces.UI;
 
 public interface IApplUserTexts : IList<string>
 {
     string this[object Idx] { get; set; }
+
+    event EventHandler? TextsChanged;
 }
