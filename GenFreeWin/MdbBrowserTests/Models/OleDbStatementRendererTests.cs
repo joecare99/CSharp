d194f89d19ecca6eda69f3ee44671fecc3ce2066
Joe Care <Joe_care_99@yahoo.com>
2026-08-02 13:09:28 +0200

    MdbBrowserTests

--- GenFreeWin/MdbBrowserTests/Models/OleDbStatementRendererTests.cs
+++ GenFreeWin/MdbBrowserTests/Models/OleDbStatementRendererTests.cs
@@ -11,15 +11,4 @@ namespace MdbBrowser.Models.Tests;
 [TestClass]
 public class OleDbStatementRendererTests
 {
-    [TestMethod]
-    public void RenderSelect_QuotesIdentifiers()
-    {
-        var dbc = Substitute.For<IDbConnection>();
-        var sut = new OleDbStatementRenderer(dbc);
-        var statement = new DbSelectStatement("Users", new[] { "Id", "Name" }, Array.Empty<IDbFilterClause>());
-
-        var result = sut.RenderSelect(statement);
-
-        Assert.AreEqual("SELECT [Id],[Name] FROM [Users]", result);
-    }
 }
