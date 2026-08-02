8009835dc0cde30bb1c28be1f28b7d2411c8ae69
Joe Care <Joe_care_99@yahoo.com>
2026-08-02 13:09:29 +0200

    MSQBrowserTests

--- GenFreeWin/MSQBrowserTests/Models/MySqlStatementRendererTests.cs
+++ GenFreeWin/MSQBrowserTests/Models/MySqlStatementRendererTests.cs
@@ -11,16 +11,5 @@ namespace MSQBrowser.Models.Tests
     [TestClass]
     public class MySqlStatementRendererTests
     {
-        [TestMethod]
-        public void RenderSelect_QuotesIdentifiersAndLimit()
-        {
-            var _dbconn = Substitute.For<IDbConnection>();
-            var sut = new MySqlStatementRenderer(_dbconn);
-            var statement = new DbSelectStatement("Users", new[] { "Id", "Name" }, Array.Empty<IDbFilterClause>(), 10);
-
-            var result = sut.RenderSelect(statement);
-
-            Assert.AreEqual("SELECT `Id`,`Name` FROM `Users` limit 10", result);
-        }
     }
 }
