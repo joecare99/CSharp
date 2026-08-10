899699f3b82216ab7aaa92b26642a229e08d31d7
Joe Care <Joe_care_99@yahoo.com>
2026-08-11 00:17:27 +0200

    RnzTrauer.Persistence.MySql.Tests

--- Avalonia_Apps/RnzTrauer/RnzTrauer.Persistence.MySql.Tests/MySqlNoticeSqlTests.cs
+++ Avalonia_Apps/RnzTrauer/RnzTrauer.Persistence.MySql.Tests/MySqlNoticeSqlTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RnzTrauer.Core.Domain;
+using RnzTrauer.Places;
 
 namespace RnzTrauer.Persistence.MySql.Tests;
 
@@ -48,5 +49,61 @@ public sealed class MySqlNoticeSqlTests
 
         StringAssert.Contains(statement.CommandText, "vPossibleLink1");
         Assert.AreEqual(42L, statement.Parameters["@id"]);
+    }
+
+    [TestMethod]
+    public void BuildPlaceCoordinateGet_UsesNormalizedPlaceAndLegacyCoordinateColumns()
+    {
+        var statement = MySqlPlaceCoordinateSql.BuildGet("  Heidelberg ");
+
+        StringAssert.Contains(statement.CommandText, "`Ortname`,`Latitude`,`Longitude`");
+        StringAssert.Contains(statement.CommandText, "WHERE `Ortname`=@place");
+        Assert.AreEqual("Heidelberg", statement.Parameters["@place"]);
+    }
+
+    [TestMethod]
+    public void BuildPlaceCoordinateSave_BindsCoordinatesAndPlace()
+    {
+        var statement = MySqlPlaceCoordinateSql.BuildSave(
+            new PlaceCoordinate(" Heidelberg ", 49.3988, 8.6724, "fixture", false));
+
+        StringAssert.Contains(statement.CommandText, "UPDATE `Orte`");
+        Assert.AreEqual("Heidelberg", statement.Parameters["@place"]);
+        Assert.AreEqual(49.3988, statement.Parameters["@latitude"]);
+        Assert.AreEqual(8.6724, statement.Parameters["@longitude"]);
+    }
+
+    [TestMethod]
+    public void BuildPlaceCoordinateProbe_OnlyChecksOptionalCoordinateColumns()
+    {
+        var statement = MySqlPlaceCoordinateSql.BuildProbe();
+
+        StringAssert.Contains(
+            statement.CommandText,
+            "SELECT `Latitude`,`Longitude` FROM `Orte` LIMIT 0");
+        Assert.AreEqual(0, statement.Parameters.Count);
+    }
+
+    [TestMethod]
+    public void BuildCoordinateMigration_AddsOptionalCoordinateColumnsWithoutValues()
+    {
+        var statement = MySqlPlaceCoordinateSql.BuildCoordinateMigration();
+
+        StringAssert.Contains(statement.CommandText, "ALTER TABLE `Orte`");
+        StringAssert.Contains(statement.CommandText, "DECIMAL(10,7) NULL");
+        Assert.AreEqual(0, statement.Parameters.Count);
+    }
+
+    [TestMethod]
+    public void CoordinateSchemaStatus_MissingDisablesPersistence()
+    {
+        var report = new CoordinateSchemaReport(
+            CoordinateSchemaStatus.Missing,
+            ["Orte.Latitude", "Orte.Longitude"],
+            "unknown column",
+            "schema.missing_columns");
+
+        Assert.IsFalse(report.CanPersist);
+        Assert.AreEqual(CoordinateSchemaStatus.Missing, report.Status);
     }
 }
