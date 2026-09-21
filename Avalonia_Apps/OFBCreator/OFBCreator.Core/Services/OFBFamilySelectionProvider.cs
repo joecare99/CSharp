ff3fb04f86eb04f3c41f165bb777e1f1c25decc4
Joe Care <Joe_care_99@yahoo.com>
2026-09-21 06:16:17 +0200

    OFBCreator.Core

--- Avalonia_Apps/OFBCreator/OFBCreator.Core/Services/OFBFamilySelectionProvider.cs
+++ Avalonia_Apps/OFBCreator/OFBCreator.Core/Services/OFBFamilySelectionProvider.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 using GenInterfaces.Interfaces.Genealogic;
 using OFBCreator.Abstractions.Interfaces;
 using OFBCreator.Abstractions.Models;
--- Avalonia_Apps/OFBCreator/OFBCreator.Core/Services/PropertyIndexGenerator.cs
+++ Avalonia_Apps/OFBCreator/OFBCreator.Core/Services/PropertyIndexGenerator.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
 using Microsoft.Extensions.Logging;
 using GenInterfaces.Data;
 using GenInterfaces.Interfaces.Genealogic;
