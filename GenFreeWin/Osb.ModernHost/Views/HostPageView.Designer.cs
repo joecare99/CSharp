c946c0b0759cc1b0ac74ede07c8b39676671f294
Joe Care <Joe_care_99@yahoo.com>
2026-09-26 02:56:02 +0200

    Osb.ModernHost

--- GenFreeWin/Osb.ModernHost/Views/HostPageView.Designer.cs
+++ GenFreeWin/Osb.ModernHost/Views/HostPageView.Designer.cs
@@ -16,20 +16,35 @@ internal sealed partial class HostPageView
 
     private void InitializeComponent()
     {
-        components = new Container();
         _titleLabel = new Label();
         _descriptionLabel = new Label();
-        _titleLabel.AutoSize = false;
+        SuspendLayout();
+        // 
+        // _titleLabel
+        // 
         _titleLabel.Dock = DockStyle.Top;
-        _titleLabel.Font = new Font(Font, FontStyle.Bold);
-        _titleLabel.Height = 36;
-        _descriptionLabel.AutoSize = false;
+        _titleLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+        _titleLabel.Location = new Point(24, 24);
+        _titleLabel.Name = "_titleLabel";
+        _titleLabel.Size = new Size(2379, 36);
+        _titleLabel.TabIndex = 1;
+        // 
+        // _descriptionLabel
+        // 
         _descriptionLabel.Dock = DockStyle.Fill;
+        _descriptionLabel.Location = new Point(24, 60);
+        _descriptionLabel.Name = "_descriptionLabel";
         _descriptionLabel.Padding = new Padding(0, 16, 0, 0);
+        _descriptionLabel.Size = new Size(2379, 1230);
+        _descriptionLabel.TabIndex = 0;
+        // 
+        // HostPageView
+        // 
         Controls.Add(_descriptionLabel);
         Controls.Add(_titleLabel);
-        Dock = DockStyle.Fill;
+        Name = "HostPageView";
         Padding = new Padding(24);
+        Size = new Size(2427, 1314);
         ResumeLayout(false);
     }
 
